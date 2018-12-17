168891ce9aaba6246dbe0b833171de186644fdaa
Yipeng Sun <syp@umd.edu>
2018-12-17 02:14:15 -0500

    Copy interactive tools from beginner's guide for dst exploration.

---
+++ utils/DstInteractive.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# Author: Yipeng Sun <syp at umd dot edu>
+# Last Change: Mon Dec 17, 2018 at 02:14 AM -0500
+
+import sys
+
+import GaudiPython as GP
+from GaudiConf import IOHelper
+from Configurables import DaVinci
+
+dv = DaVinci()
+dv.DataType = '2016'
+dv.Simulation = True
+
+# Pass file to open as first command line argument
+inputFiles = [sys.argv[-1]]
+IOHelper('ROOT').inputFiles(inputFiles)
+
+appMgr = GP.AppMgr()
+evt = appMgr.evtsvc()
+
+appMgr.run(1)
+evt.dump()
+
+
+def nodes(evt, node=None):
+    """List all nodes in `evt`"""
+    nodenames = []
+
+    if node is None:
+        root = evt.retrieveObject('')
+        node = root.registry()
+
+    if node.object():
+        nodenames.append(node.identifier())
+        for l in evt.leaves(node):
+            # skip a location that takes forever to load
+            # XXX How to detect these automatically??
+            if 'Swum' in l.identifier():
+                continue
+
+            nodenames += nodes(evt, l)
+
+    else:
+        nodenames.append(node.identifier())
+
+    return nodenames
+
+
+def advance(decision):
+    """Advance until stripping decision is true, returns
+    number of events by which we advanced"""
+    n = 0
+    while True:
+        appMgr.run(1)
+
+        if not evt['/Event/Rec/Header']:
+            print('Reached end of input files')
+            break
+
+        n += 1
+        dec = evt['/Event/Strip/Phys/DecReports']
+        if dec.hasDecisionName('Stripping{0}Decision'.format(decision)):
+            break
+
+    return n
