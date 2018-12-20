5f37366f0259e0fe0e9cd1869ad8d7ca83058ff2
Yipeng Sun <syp@umd.edu>
2018-12-20 15:34:29 -0500

    WIP: DaVinci script cleanup.

--- 2012-b2D0MuXB2DMuNuForTauMuLine/ntuple_options.py
+++ 2012-b2D0MuXB2DMuNuForTauMuLine/ntuple_options.py
@@ -1,5 +1,33 @@
 # License: BSD 2-clause
-# Last Change: Thu Dec 20, 2018 at 02:54 PM -0500
+# Last Change: Thu Dec 20, 2018 at 03:09 PM -0500
+
+#####################
+# Configure DaVinci #
+#####################
+
+from Configurables import DaVinci
+
+DaVinci().InputType = 'DST'
+DaVinci().DataType = '2012'
+DaVinci().EvtMax = -1
+DaVinci().SkipEvents = 0
+DaVinci().Simulation = False
+
+DaVinci().PrintFreq = 1000
+
+# Output filename
+DaVinci().TupleFile = 'DVntuple.root'
+
+# Only ask for luminosity information when not using simulated data
+DaVinci().Lumi = not DaVinci().Simulation
+
+
+##########################
+# Define stripping lines #
+##########################
+
+line_data = 'Strippingb2D0MuXB2DMuNuForTauMuLine'
+
 
 ############################################
 # Stream and stripping line we want to use #
@@ -9,32 +37,22 @@ from Configurables import DecayTreeTuple
 from DecayTreeTuple.Configuration import *
 
 stream = 'AllStreams'
-line = 'Strippingb2D0MuXB2DMuNuForTauMuLine'
 
 # Create an ntuple to capture semileptonic B decays from the stripping line
 dtt = DecayTreeTuple('TupleDstToD*Mu+Nu(NuNu)')
 dtt.Inputs = ['/Event/{0}/Phys/{1}/Particles'.format(stream, line)]
 dtt.Decay = '[D*(2010)+ -> (D0 -> K- K+) pi+]CC'
 
+DaVinci().UserAlgorithms += [dtt]
 
-#####################
-# Configure DaVinci #
-#####################
 
-from Configurables import DaVinci
+##################
+# Define filters #
+##################
+
 from PhysConf.Filters import LoKi_Filters
 
-DaVinci().UserAlgorithms += [dtt]
-DaVinci().InputType = 'DST'
-DaVinci().TupleFile = 'DVntuple.root'
-DaVinci().PrintFreq = 1000
-DaVinci().DataType = '2012'
-DaVinci().Simulation = False
 
-# Only ask for luminosity information when not using simulated data
-DaVinci().Lumi = not DaVinci().Simulation
-
-# DaVinci().EvtMax = 10
 
 # fltrs = LoKi_Filters(
     # STRIP_Code="HLT_PASS_RE('StrippingD2hhPromptDst2D2KKLineDecision')"
