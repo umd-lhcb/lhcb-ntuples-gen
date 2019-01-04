80b3c4e8cc4e01f2d2aa5d5e95b0faabb8779a54
Yipeng Sun <syp@umd.edu>
2019-01-04 13:09:29 -0500

    Add another pre-filter to see if the event is triggered.

--- 2012-b2D0MuXB2DMuNuForTauMuLine/ntuple_options.py
+++ 2012-b2D0MuXB2DMuNuForTauMuLine/ntuple_options.py
@@ -1,5 +1,5 @@
 # License: BSD 2-clause
-# Last Change: Tue Jan 01, 2019 at 03:46 AM -0500
+# Last Change: Fri Jan 04, 2019 at 01:09 PM -0500
 
 #####################
 # Configure DaVinci #
@@ -23,6 +23,14 @@ DaVinci().HistogramFile = './gen/DVHisto.root'
 DaVinci().Lumi = not DaVinci().Simulation
 
 
+###############################
+# Define stripping lines, etc #
+###############################
+
+line_strip = 'b2D0MuXB2DMuNuForTauMuLine'
+line_hlt = 'Hlt2CharmHadD0HH_D02KPi'
+
+
 ###################################
 # Customize DaVinci main sequence #
 ###################################
@@ -40,13 +48,6 @@ veloprotos.Output = ['Rec/ProtoP/myProtoPMaker/ProtoParticles']  # This TES loca
 DaVinci().appendToMainSequence([veloprotos])
 
 
-##########################
-# Define stripping lines #
-##########################
-
-line_data = 'b2D0MuXB2DMuNuForTauMuLine'
-
-
 ######################
 # Define pre-filters #
 ######################
@@ -55,13 +56,20 @@ line_data = 'b2D0MuXB2DMuNuForTauMuLine'
 #
 # This should speed up the execution time.
 
-from PhysConf.Filters import LoKi_Filters
+from Configurables import LoKi__HDRFilter as HDRFilter
 
-fltrs = LoKi_Filters(
-    STRIP_Code="HLT_PASS_RE('Stripping{0}Decision')".format(line_data)
-)
+# Differences between 'HLT_PASS' and 'HLT_PASS_RE':
+#   'HLT_PASS' matches the line *exactly*
+#   'HLT_PASS_RE' (which was used in the starter kit) use regular expression to
+#   check if line given is a part of the lines of the events.
+fltr_strip = HDRFilter('StrippedBCands',
+                       Code="HLT_PASS('Stripping{0}Decision')".format(
+                           line_strip))
 
-DaVinci().EventPreFilters = fltrs.filters('Filters')
+fltr_trig = HDRFilter('TriggeredD0',
+                      Code="HLT_PASS('{0}Decision')".format(line_hlt))
+
+# DaVinci().EventPreFilters = fltrs.filters('Filters')
 
 
 #####################
@@ -75,7 +83,7 @@ stream = 'Semileptonic'
 
 # Create an ntuple to capture semileptonic B decays from the stripping line
 dtt = DecayTreeTuple('LFUv')
-dtt.Inputs = ['/Event/{0}/Phys/{1}/Particles'.format(stream, line_data)]
+dtt.Inputs = ['/Event/{0}/Phys/{1}/Particles'.format(stream, line_strip)]
 # dtt.Decay = '[B~0 -> ^(D*(2010)+ -> ^(D0 -> ^K- ^pi+) ^pi+) ^mu-]CC'  # Decay from Phoebe's script
 dtt.Decay = '[B+ ->  ^(D~0 -> ^K+ ^pi-) ^mu+]CC'  # The D* is not reconstructed by the stripping line
 
