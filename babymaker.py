f54dcdbaf39bf227fa159288f0f1a8920218f17b
Yipeng Sun <syp@umd.edu>
2019-06-30 17:24:46 -0400

    Add a function to time stamp generated file.
    Dummy output test.

--- babymaker.py
+++ babymaker.py
@@ -2,10 +2,12 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Sun Jun 30, 2019 at 01:29 PM -0400
+# Last Change: Sun Jun 30, 2019 at 05:24 PM -0400
 
 import abc
 import yaml
+
+from datetime import datetime
 
 
 ################################
@@ -19,17 +21,18 @@ import yaml
 
 class CppWriter(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def parse_conf(yaml_conf):
+    def parse_conf(self, yaml_conf):
         '''
         Parse configuration file for the writer.
         '''
 
     @abc.abstractmethod
-    def write(cpp_file):
+    def write(self, cpp_file):
         '''
         Write generated C++ file to 'cpp_file'.
         '''
 
+    @staticmethod
     def read_ntuple_structure(yaml_ntuple):
         '''
         Read ntuple data structure.
@@ -38,8 +41,9 @@ class CppWriter(metaclass=abc.ABCMeta):
             return yaml.safe_load(f)
 
     @staticmethod
-    def cpp_gen_date(strpformat):
-        return 0
+    def cpp_gen_date(time_format='%Y-%m-%d %H:%M:%S.%f'):
+        return '// Generated on: {}'.format(
+            datetime.now().strftime(time_format))
 
     @staticmethod
     def cpp_header(header):
@@ -70,3 +74,37 @@ class PostProcess(CppWriter):
                  branch_rename=[]
                  ):
         self.include = include
+
+    def parse_conf(self, yaml_conf):
+        pass
+
+    def write(self, cpp_file):
+        filecontent = ('\n').join([self.cpp_header(h) for h in self.include])
+        filecontent += '\n'
+
+        main = self.cpp_main(self.cpp_tfiles(''))
+        filecontent += main
+
+        with open(cpp_file, 'w') as f:
+            f.write(filecontent)
+
+    @staticmethod
+    def cpp_tfiles(loops):
+        return '''
+TFile *input_file = new TFile(argv[1]);
+TFile *output_file = new TFile(argv[2]);
+
+{}
+
+delete input_file;
+delete input_file;
+'''.format(loops)
+
+
+########
+# Main #
+########
+
+if __name__ == '__main__':
+    generator = PostProcess()
+    generator.write('test.cpp')
