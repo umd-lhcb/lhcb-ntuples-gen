6b2e9a30bf47b25e325fe5853a55c2e6ab35d414
Yipeng Sun <syp@umd.edu>
2019-06-30 13:21:40 -0400

    New babymaker structure.

--- babymaker.py
+++ babymaker.py
@@ -2,6 +2,70 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Fri Jun 28, 2019 at 04:39 PM -0400
+# Last Change: Sun Jun 30, 2019 at 01:21 PM -0400
 
+import abc
 import yaml
+
+
+################################
+# Comand line arguments parser #
+################################
+
+
+##########################
+# C++ generator template #
+##########################
+
+class CppWriter(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def parse_conf(yaml_conf):
+        '''
+        Parse configuration file for the writer.
+        '''
+
+    @abc.abstractmethod
+    def write(cpp_file):
+        '''
+        Write generated C++ file to 'cpp_file'.
+        '''
+
+    @abc.abstractmethod
+    def parse_ntuple_structure(yaml_ntuple):
+        '''
+        Parse ntuple data structure.
+        '''
+
+    @staticmethod
+    def cpp_gen_date(strpformat):
+        return 0
+
+    @staticmethod
+    def cpp_header(header):
+        return '#include <{}>'.format(header)
+
+    @staticmethod
+    def cpp_main(body):
+        return '''
+int main(int, char** argv) {{
+  {}
+  return 0;
+}}
+'''.format(body)
+
+
+#################################
+# C++ generator implementations #
+#################################
+
+class PostProcess(CppWriter):
+    def __init__(self,
+                 include=['TFile.h', 'TTreeReader.h'],
+                 drop=[],
+                 keep=['*'],
+                 rename=[]
+                 ):
+        self.include = include
+
+    def template(self):
+        return ''
