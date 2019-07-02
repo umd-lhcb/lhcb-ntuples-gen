f8bec092e8af8765e74c0901d6d5c23225c88140
Yipeng Sun <syp@umd.edu>
2019-07-02 17:15:12 -0400

    Remove unnecessary intermediate variable.

--- include/functor/basic.h
+++ include/functor/basic.h
@@ -1,14 +1,13 @@
 // Author: Yipeng Sun
 // License: BSD 2-clause
-// Last Change: Tue Jul 02, 2019 at 03:28 AM -0400
+// Last Change: Tue Jul 02, 2019 at 05:14 PM -0400
 
 #ifndef _LNG_FUNCTOR_BASIC_H_
 #define _LNG_FUNCTOR_BASIC_H_
 
 template <typename T1, typename T2>
 T1 SUM(T1 x, T2 y) {
-  auto y1 = static_cast<T1>(y);
-  return x + y1;
+  return x + static_cast<T1>(y);
 }
 
 #endif
