38cd1dfd0969eace76594155241e263afb00f13e
Yipeng Sun <syp@umd.edu>
2019-07-10 03:39:15 -0400

    Specify sim08a DB tags.

--- 2012-b2D0MuXB2DMuNuForTauMuLine/ntuple_options_mc_conf.py
+++ 2012-b2D0MuXB2DMuNuForTauMuLine/ntuple_options_mc_conf.py
@@ -6,3 +6,9 @@ DaVinci().Simulation = True
 from Configurables import CondDB
 
 CondDB().useLatestTags("2012")
+
+
+from Configurables import LHCbApp
+
+LHCbApp().CondDBtag = "sim-20130503-1-vc-md100"
+LHCbApp().DDDBtag = "dddb-20130503-1"
