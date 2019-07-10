227e2fe5caeface7bafdeda8c498eb94a9918092
Yipeng Sun <syp@umd.edu>
2019-07-10 03:43:27 -0400

    Now run.sh takes a single argument to produce data or MC ntuples.

--- 2012-b2D0MuXB2DMuNuForTauMuLine/run.sh
+++ 2012-b2D0MuXB2DMuNuForTauMuLine/run.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-run gaudirun.py ./ntuple_options.py
+case $1 in
+    'data')
+        run gaudirun.py ./ntuple_options.py
+        ;;
+    'mc')
+        run gaudirun.py ./ntuple_options_mc_conf.py ./ntuple_options_base.py
+        ;;
+    *)
+        echo "Unknown argument: ${1}"
+        ;;
+esac
