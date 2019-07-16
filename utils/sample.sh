9d2ddcac936036a8c94e1b252a0cf2d5a26cf476
Yipeng Sun <syp@umd.edu>
2019-07-15 23:25:26 -0400

    Update sample script to show how to use shell variables.

--- utils/sample.sh
+++ utils/sample.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-root -q -l 'extract_uid.cxx(
-    "../2012-b2D0MuXB2DMuNuForTauMuLine/data/sample/BCands_Dst-data-2012-mag_down-davinci_v42r8p1-subset.root",
-    "../2012-b2D0MuXB2DMuNuForTauMuLine/data/sample/BCands_Dst-data-2012-mag_down.root",
+FILE1="../2012-b2D0MuXB2DMuNuForTauMuLine/sample/BCands_Dst-data-2012-mag_down-davinci_v42r8p1-subset.root"
+FILE2="../2012-b2D0MuXB2DMuNuForTauMuLine/sample/BCands_Dst-data-2012-mag_down.root"
+
+root -q -l 'extract_uid.cxx("'"${FILE1}"'", "'"${FILE2}"'",
     "uid.root", "TupleY/DecayTree", "YCands/DecayTree")'
