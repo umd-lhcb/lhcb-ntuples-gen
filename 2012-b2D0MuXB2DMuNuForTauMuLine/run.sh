5d7c26f7563c306ebe27ee1e4553e28851b5d56d
Yipeng Sun <syp@umd.edu>
2019-08-06 19:17:17 -0400

    Edit path.

--- 2012-b2D0MuXB2DMuNuForTauMuLine/run.sh
+++ 2012-b2D0MuXB2DMuNuForTauMuLine/run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-case $1 in
-    'Dst')
-        run gaudirun.py ./conds/cond_$1-$2.py ./reco_$1.py
-        ;;
-    *)
-        echo "Unknown argument: ${1}"
-        ;;
-esac
+run gaudirun.py ./conds/cond-$1.py ./reco_$1.py
