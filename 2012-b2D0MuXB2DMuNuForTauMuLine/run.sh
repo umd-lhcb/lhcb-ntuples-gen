153f6a09569c5e793b01e4e32268f5d8098c255f
Yipeng Sun <syp@umd.edu>
2019-08-23 02:31:01 -0400

    [skip ci] Enable an optional debug command line flag.

--- 2012-b2D0MuXB2DMuNuForTauMuLine/run.sh
+++ 2012-b2D0MuXB2DMuNuForTauMuLine/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-run gaudirun.py ./conds/cond-$2.py ./reco_$1.py
+case $3 in
+    debug)
+        run gaudirun.py ./conds/cond-$2.py ./reco_$1.py \
+            --option="from Configurables import DaVinci; DaVinci().EvtMax = 10"
+        ;;
+
+    *)
+        run gaudirun.py ./conds/cond-$2.py ./reco_$1.py
+        ;;
+esac
