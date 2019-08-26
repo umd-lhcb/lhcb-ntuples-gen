d33e305c53ded2e94c4eababa3b9e6a97957ea5f
Yipeng Sun <syp@umd.edu>
2019-08-25 23:49:21 -0400

    Plot isolation track types from ntuples generated by 2 versions of DaVinci.

--- utils/plot_iso_type.sh
+++ utils/plot_iso_type.sh
@@ -2,8 +2,22 @@
 
 OUTPUT_DIR="../gen/"
 NTP_DV36="../2012-b2D0MuXB2DMuNuForTauMuLine/sample/BCands_Dst-data-2012-mag_down-davinci_v36r1p2-subset.root"
+NTP_DV42="../2012-b2D0MuXB2DMuNuForTauMuLine/sample/BCands_Dst-data-2012-mag_down-davinci_v42r8p1-subset.root"
 
-root -q -l 'make_histo_float.cxx(
-    "'"${NTP_DV36}"'", "'"${OUTPUT_DIR}"'",
-    "_dv36r1",
-    "TupleY/DecayTree", "Y_ISOLATION_Type", 40, 0, 5)'
+BRANCHES=(
+    "Y_ISOLATION_Type"
+    "Y_ISOLATION_Type2"
+    "Y_ISOLATION_Type3"
+    "Y_ISOLATION_Type4"
+)
+
+for branch in "${BRANCHES[@]}"; do
+    root -q -l 'make_histo_float.cxx(
+        "'"${NTP_DV36}"'", "'"${OUTPUT_DIR}"'",
+        "_dv36r1p2",
+        "TupleY/DecayTree", "'"${branch}"'", 40, 0, 5)'
+    root -q -l 'make_histo_float.cxx(
+        "'"${NTP_DV42}"'", "'"${OUTPUT_DIR}"'",
+        "_dv42r8p1",
+        "TupleY/DecayTree", "'"${branch}"'", 40, 0, 5)'
+done
