739bef0503e320dad7a6f8c8e3105cdadbed6766
Yipeng Sun <syp@umd.edu>
2019-09-05 03:10:52 -0400

    Correct conversion to GeV and GeV^2.

--- include/functor/basic.h
+++ include/functor/basic.h
@@ -1,6 +1,6 @@
 // Author: Yipeng Sun
 // License: BSD 2-clause
-// Last Change: Tue Jul 02, 2019 at 05:14 PM -0400
+// Last Change: Thu Sep 05, 2019 at 03:10 AM -0400
 
 #ifndef _LNG_FUNCTOR_BASIC_H_
 #define _LNG_FUNCTOR_BASIC_H_
@@ -8,6 +8,16 @@
 template <typename T1, typename T2>
 T1 SUM(T1 x, T2 y) {
   return x + static_cast<T1>(y);
+}
+
+template <typename T>
+T GEV(T x) {
+  return x / 1000;
+}
+
+template <typename T>
+T GEV2(T x) {
+  return x / (1000*1000);
 }
 
 #endif
