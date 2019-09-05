450625050fefd5dde9dd7f25c7c6440744db7001
Yipeng Sun <syp@umd.edu>
2019-09-05 16:06:31 -0400

    OCD: Remove useless functor 'SUM'.

--- include/functor/basic.h
+++ include/functor/basic.h
@@ -1,14 +1,9 @@
 // Author: Yipeng Sun
 // License: BSD 2-clause
-// Last Change: Thu Sep 05, 2019 at 03:39 AM -0400
+// Last Change: Thu Sep 05, 2019 at 04:06 PM -0400
 
 #ifndef _LNG_FUNCTOR_BASIC_H_
 #define _LNG_FUNCTOR_BASIC_H_
-
-template <typename T1, typename T2>
-T1 SUM(T1 x, T2 y) {
-  return x + static_cast<T1>(y);
-}
 
 template <typename T>
 T GEV(T x) {
