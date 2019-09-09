7bd4a0a02b5f40129ff92437c848c8caaad487a4
Yipeng Sun <syp@umd.edu>
2019-09-09 02:49:48 -0400

    Update basic functors.

--- include/functor/basic.h
+++ include/functor/basic.h
@@ -1,9 +1,23 @@
 // Author: Yipeng Sun
 // License: BSD 2-clause
-// Last Change: Sat Sep 07, 2019 at 01:24 PM -0400
+// Last Change: Mon Sep 09, 2019 at 02:49 AM -0400
 
 #ifndef _LNG_FUNCTOR_BASIC_H_
 #define _LNG_FUNCTOR_BASIC_H_
+
+// Arithmetic //////////////////////////////////////////////////////////////////
+
+template <typename T>
+T SUB(T x, T y) {
+  return x - y;
+}
+
+template <typename T>
+T PROD(T x, T y) {
+  return x * y;
+}
+
+// Units ///////////////////////////////////////////////////////////////////////
 
 template <typename T>
 T GEV(T x) {
@@ -14,11 +28,5 @@ template <typename T>
 T GEV2(T x) {
   return GEV(GEV(x));
 }
-
-template <typename T>
-T PROD(T x, T y) {
-  return x*y;
-}
-
 
 #endif
