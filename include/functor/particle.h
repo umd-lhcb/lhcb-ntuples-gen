047bff9a9133abd5201d39920e549f54e82d221c
Yipeng Sun <syp@umd.edu>
2019-09-09 13:11:54 -0400

    Calculate momentum transfer q. Currently incorrect.

--- include/functor/particle.h
+++ include/functor/particle.h
@@ -1,6 +1,6 @@
 // Author: Yipeng Sun
 // License: BSD 2-clause
-// Last Change: Mon Sep 09, 2019 at 12:49 PM -0400
+// Last Change: Mon Sep 09, 2019 at 01:03 PM -0400
 
 #ifndef _LNG_FUNCTOR_PARTICLE_H_
 #define _LNG_FUNCTOR_PARTICLE_H_
@@ -19,6 +19,21 @@ Bool_t ISDATA(ULong64_t time) {
     return false;
 }
 
+TVector3 VEC_DELTA(Double_t Xf, Double_t Yf, Double_t Zf, Double_t Xi,
+                   Double_t Yi, Double_t Zi) {
+  TVector3 v;
+  v.SetXYZ(Xf - Xi, Yf - Yi, Zf - Zi);
+  return v;
+}
+
+TLorentzVector FOUR_VEC(Double_t X, Double_t Y, Double_t Z, Double_t T) {
+  TLorentzVector v;
+  v.SetXYZT(X, Y, Z, T);
+  return v;
+}
+
+Double_t M(TLorentzVector v) { return v.M2(); }
+
 // Muon-related ////////////////////////////////////////////////////////////////
 
 Bool_t MU_PID(Bool_t id_correctness, Double_t PID) {
@@ -27,28 +42,18 @@ Bool_t MU_PID(Bool_t id_correctness, Double_t PID) {
 
 // Rest frame approximation ////////////////////////////////////////////////////
 
-TVector3 B0_FLIGHT_VECTOR(Double_t Xf, Double_t Yf, Double_t Zf, Double_t Xi,
-                          Double_t Yi, Double_t Zi) {
-  TVector3 v;
-  v.SetXYZ(Xf - Xi, Yf - Yi, Zf - Zi);
-  return v;
-}
-
-Double_t M2_MISS(Double_t E, Double_t PX, Double_t PY, Double_t PZ, Double_t m,
-                 TVector3 flight) {
+TLorentzVector P_MISS(Double_t E, Double_t PX, Double_t PY, Double_t PZ,
+                      Double_t m, TVector3 flight) {
   const Double_t B_M = 5279.61;
 
-  TLorentzVector P_reco;
-  P_reco.SetXYZT(PX, PY, PZ, E);
-
-  Double_t Tan_theta = flight.Unit().Perp() / flight.Unit().Z();
-  Double_t PT        = TMath::Power((B_M / m), 1) * Tan_theta * PZ;
+  TLorentzVector P_reco    = FOUR_VEC(PX, PY, PZ, E);
+  Double_t       Tan_theta = flight.Unit().Perp() / flight.Unit().Z();
+  Double_t       PT        = TMath::Power((B_M / m), 1) * Tan_theta * PZ;
 
   TLorentzVector P;
   P.SetPtEtaPhiM(PT, flight.Unit().Eta(), flight.Unit().Phi(), B_M);
 
-  TLorentzVector P_miss = P - P_reco;
-  return P_miss.M2();
+  return P - P_reco;
 }
 
 #endif
