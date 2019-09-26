82dc36e4b8649a86e3415fa6809ff3ceeab3ecac
Yipeng Sun <syp@umd.edu>
2019-09-25 23:14:20 -0400

    Return index to unique events; cleanup.

--- utils/extract_uid.py
+++ utils/extract_uid.py
@@ -2,14 +2,14 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Wed Sep 25, 2019 at 05:28 PM -0400
+# Last Change: Wed Sep 25, 2019 at 11:14 PM -0400
 
 import uproot
 import numpy as np
 
 
-def get_unique_id(filename, tree,
-                  runBranch='runNumber', eventBranch='eventNumber'):
+def extract_uid(filename, tree,
+                runBranch='runNumber', eventBranch='eventNumber'):
     ntp = uproot.open(filename)
     run = np.char.mod('%d', ntp[tree].array(runBranch))
     event = np.char.mod('%d', ntp[tree].array(eventBranch))
@@ -17,14 +17,14 @@ def get_unique_id(filename, tree,
     run = np.char.add(run, '-')
     mid = np.char.add(run, event)
 
-    _, idx, count = np.unique(mid, return_index=True, return_counts=True)
-    uniq = idx[count == 1]
+    uid, idx, count = np.unique(mid, return_index=True, return_counts=True)
+    uid_idx = idx[count == 1]
 
     total_size = mid.size
-    uniq_size = uniq.size
-    dup_size = total_size - uniq_size
+    uniq_size = uid.size
+    dupl_size = total_size - uniq_size
 
-    return (uniq, total_size, uniq_size, dup_size)
+    return (uid, uid_idx, total_size, uniq_size, dupl_size)
 
 
 if __name__ == '__main__':
@@ -32,5 +32,5 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     tree = sys.argv[2]
 
-    _, total_size, _, dup_size = get_unique_id(filename, tree)
-    print('Total # of event: {}, duplicate: {}'.format(total_size, dup_size))
+    _, _, total_size, _, dupl_size = extract_uid(filename, tree)
+    print('Total # of event: {}, duplicate: {}'.format(total_size, dupl_size))
