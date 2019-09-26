e82ac92bb0d4a4a793e7b25a3b9a927031aaf173
Yipeng Sun <syp@umd.edu>
2019-09-26 15:08:12 -0400

    Reorganize generated comparison figures.

---
+++ utils/comparison_run1_Dst/data-dv36_vs_dv42-no_velo_pions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OUTPUT_DIR="../../docs/data/comparison_run1_Dst/data-dv36_vs_dv42-no_velo_pions"
+NTP_REF="../../run1-b2D0MuXB2DMuNuForTauMuLine/samples/BCands_Dst-phoebe-data-2012-mag_down-davinci_v36r1p2-subset-no_velo_pions.root"
+NTP_COM="../../run1-b2D0MuXB2DMuNuForTauMuLine/samples/BCands_Dst-yipeng-data-2012-mag_down-davinci_v42r8p1-subset-no_velo_pions.root"
+
+BRANCHES=(
+    "Y_ISOLATION_Type"
+    "Y_ISOLATION_Type2"
+    "Y_ISOLATION_Type3"
+    "Y_ISOLATION_Type4"
+)
+
+for branch in "${BRANCHES[@]}"; do
+    ../plot_single_branch.py "${NTP_REF}" "TupleY/DecayTree" "${branch}" \
+        -o "${OUTPUT_DIR}/${branch}_dv36r1p2-no_velo_pions.png"
+    ../plot_single_branch.py "${NTP_COM}" "TupleB0/DecayTree" "${branch}" \
+        -o "${OUTPUT_DIR}/${branch}_dv42r8p1-no_velo_pions.png"
+done
---
+++ utils/comparison_run1_Dst/data-dv36_vs_dv42.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+OUTPUT_DIR="../../docs/data/comparison_run1_Dst/data-dv36_vs_dv42"
+NTP_REF="../../run1-b2D0MuXB2DMuNuForTauMuLine/samples/BCands_Dst-phoebe-data-2012-mag_down-davinci_v36r1p2-subset.root"
+NTP_COM="../../run1-b2D0MuXB2DMuNuForTauMuLine/samples/BCands_Dst-yipeng-data-2012-mag_down-davinci_v42r8p1-subset.root"
+
+# Diff
+BRANCHES="D0_P,Dst_2010_minus_P,"
+BRANCHES+="Kplus_P,Kplus_PX,Kplus_PY,Kplus_PZ,"
+BRANCHES+="muplus_P,muplus_PX,muplus_PY,muplus_PZ,"
+BRANCHES+="Y_ISOLATION_BDT,Y_ISOLATION_BDT2,Y_ISOLATION_BDT3"
+
+../plot_diff_two_ntuples.py -n "${NTP_REF}" -N "${NTP_COM}" \
+    -t "TupleY/DecayTree" -T "TupleB0/DecayTree" \
+    -b "${BRANCHES}" -B ${BRANCHES} \
+    -o "${OUTPUT_DIR}"
+
+# Individual branches
+BRANCHES=(
+    "Y_ISOLATION_Type"
+    "Y_ISOLATION_Type2"
+    "Y_ISOLATION_Type3"
+    "Y_ISOLATION_Type4"
+)
+
+for branch in "${BRANCHES[@]}"; do
+    ../plot_single_branch.py "${NTP_REF}" "TupleY/DecayTree" "${branch}" \
+        -o "${OUTPUT_DIR}/${branch}_dv36r1p2.png"
+    ../plot_single_branch.py "${NTP_COM}" "TupleB0/DecayTree" "${branch}" \
+        -o "${OUTPUT_DIR}/${branch}_dv42r8p1.png"
+done
