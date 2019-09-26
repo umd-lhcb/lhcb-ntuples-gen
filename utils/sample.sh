09247a9340d2f97951e750ac04f432e9d79357e8
Yipeng Sun <syp@umd.edu>
2019-09-26 12:57:47 -0400

    Remove old C++ marco. They are archived in 0.6.2.

--- utils/sample.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILE1="../2012-b2D0MuXB2DMuNuForTauMuLine/sample/BCands_Dst-data-2012-mag_down-davinci_v42r8p1-subset.root"
-FILE2="../2012-b2D0MuXB2DMuNuForTauMuLine/sample/BCands_Dst-data-2012-mag_down.root"
-
-root -q -l 'extract_uid.cxx("'"${FILE1}"'", "'"${FILE2}"'",
-    "uid.root", "TupleY/DecayTree", "YCands/DecayTree")'
--- utils/sample_validate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-root -q -l 'ntuple_validate.cxx(
-	"../2012-b2D0MuXB2DMuNuForTauMuLine/data/sample/BCands_Dst-data-2012-mag_down-davinci_v42r8p1-subset.root",
-	"../2012-b2D0MuXB2DMuNuForTauMuLine/data/sample/BCands_Dst-data-2012-mag_down-davinci_v36r1p2-subset.root",
-	"TupleY/DecayTree", "TupleY/DecayTree", "D0_P", "D0_P",
-	"davinci_compare")'
