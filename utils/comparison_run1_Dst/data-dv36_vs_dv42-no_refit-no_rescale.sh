f8933f7dc5c85ff27fa5d59cb9969a89eedef617
Yipeng Sun <syp@umd.edu>
2019-10-11 13:40:34 -0400

    Add comparison plots without refitting or rescaling.

---
+++ utils/comparison_run1_Dst/data-dv36_vs_dv42-no_refit-no_rescale.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OUTPUT_DIR="../../docs/data/comparison_run1_Dst/data-dv36_vs_dv42-no_refit-no_rescale"
+NTP_REF="../../run1-b2D0MuXB2DMuNuForTauMuLine/samples/BCands_Dst-phoebe-data-2012-mag_down-dv36-subset-no_refit-no_rescale.root"
+NTP_COM="../../run1-b2D0MuXB2DMuNuForTauMuLine/samples/BCands_Dst-yipeng-data-2012-mag_down-dv42-subset-no_refit-no_rescale.root"
+
+# Individual branches
+BRANCHES=(
+    "Y_ISOLATION_Type"
+    "Y_ISOLATION_Type2"
+    "Y_ISOLATION_Type3"
+    "Y_ISOLATION_Type4"
+)
+
+for branch in "${BRANCHES[@]}"; do
+    ../plot_single_branch.py "${NTP_REF}" "TupleB0/DecayTree" "${branch}" \
+        -o "${OUTPUT_DIR}/${branch}_dv36.png"
+    ../plot_single_branch.py "${NTP_COM}" "TupleB0/DecayTree" "${branch}" \
+        -o "${OUTPUT_DIR}/${branch}_dv42.png"
+done
+
+# Comparison between track types
+../plot_match_iso_track.py -n ${NTP_REF} -N ${NTP_COM} \
+    -t 'TupleB0/DecayTree' -T 'TupleB0/DecayTree' -s '_dv42,_dv36' \
+    -o ${OUTPUT_DIR}
+
+# Draw 2D histogram between track type difference and BDT score difference
+../plot_match_iso_track_hexbin.py -n ${NTP_REF} -N ${NTP_COM} \
+    -t 'TupleB0/DecayTree' -T 'TupleB0/DecayTree' -s '_dv42,_dv36' \
+    -o ${OUTPUT_DIR} --bins 30
