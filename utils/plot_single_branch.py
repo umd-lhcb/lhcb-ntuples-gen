c6077f1aae8bc7a6e2ae10af1baeef4319b457b2
Yipeng Sun <syp@umd.edu>
2019-10-21 03:06:33 -0400

    [skip ci] Experimental conversion of the basic plot script.

--- utils/plot_single_branch.py
+++ utils/plot_single_branch.py
@@ -1,127 +1,35 @@
 #!/usr/bin/env python3
 #
 # Author: Yipeng Sun
-# Last Change: Thu Oct 03, 2019 at 04:22 PM -0400
+# Last Change: Mon Oct 21, 2019 at 03:05 AM -0400
 
 import uproot
-import numpy as np
-import matplotlib as mp
 
-from argparse import ArgumentParser
-from matplotlib import pyplot as plt
-
-
-################
-# Configurable #
-################
-
-BINS = 200
-
-FONT_FAMILY = 'monospace'
-FONT_SIZE = '12'
-PLT_STYLE = 'classic'
+from pyTuplingUtils.parse import single_ntuple_parser
+from pyTuplingUtils.io import read_branch
+from pyTuplingUtils.utils import gen_histo
+from pyTuplingUtils.plot import plot_style, plot_histo, ax_add_args_default
 
 
 #################################
 # Command line arguments parser #
 #################################
 
-def parse_input():
-    parser = ArgumentParser(description='''
-generate a plot from a branch of tree contained in a n-tuple.'''
-                            )
+DESCR = '''
+generate a plot from a branch of tree contained in a n-tuple.
+'''
 
-    parser.add_argument('ntuple',
-                        help='''
-path to n-tuple.''')
 
-    parser.add_argument('tree',
-                        help='''
-tree name in n-tuple.''')
+def parse_input(descr=DESCR):
+    parser = single_ntuple_parser(descr)
 
-    parser.add_argument('branch',
-                        help='''
-branch name in tree.''')
-
-    parser.add_argument('-o', '--output',
-                        nargs='?',
-                        required=True,
-                        help='''
-path to output file.''')
-
-    parser.add_argument('--bins',
-                        nargs='?',
-                        type=int,
-                        default=BINS,
-                        help='''
-number of bins. default to {}.'''.format(BINS))
-
-    parser.add_argument('--yAxisScale',
+    parser.add_argument('--y-axis-scale',
                         nargs='?',
                         default='linear',
                         help='''
 y axis scale (linear or log).''')
 
-    return parser.parse_args()
-
-
-######
-# IO #
-######
-
-def read_branch(ntuple, tree=None, branch=None):
-    ntp = uproot.open(ntuple)
-    return ntp[tree].array(branch)
-
-
-def gen_histo(array, bins=BINS, scale=1.05):
-    min = array.min()
-    max = array.max()
-
-    min = min*scale if min < 0 else min/scale
-    max = max/scale if max < 0 else max*scale
-
-    return np.histogram(array, bins, (min, max))
-
-
-########
-# Plot #
-########
-
-def tick_formatter(x, p):
-    X = str(x)
-    if len(X) > 4:
-        return '{:2g}'.format(x)
-    else:
-        return x
-
-
-def plot_single_branch(histo, bins, output, title, num, mean, std,
-                       yAxisScale='linear'):
-    plt.style.use(PLT_STYLE)
-    plt.rcParams.update({'font.family': FONT_FAMILY})
-    plt.rcParams.update({'font.size': FONT_SIZE})
-
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    ax.set_yscale(yAxisScale)
-
-    ax.bar(bins[:-1], histo, width=np.diff(bins),
-           align='edge', color='blue', edgecolor='blue',
-           label='tot: {:.4g} mean: {:.2g} std: {:.2g}'.format(num, mean, std))
-    ax.legend()
-    ax.set_title(title)
-    # Reformat x-ticks
-    ax.get_xaxis().set_major_formatter(
-        mp.ticker.FuncFormatter(tick_formatter)
-    )
-
-    plt.tight_layout(pad=0.1)  # Remove all paddings
-    fig.savefig(output)
-
-    # Close figure
-    fig.clf()
-    plt.close(fig)
+    return parser
 
 
 ########
@@ -129,13 +37,15 @@ def plot_single_branch(histo, bins, output, title, num, mean, std,
 ########
 
 if __name__ == '__main__':
-    args = parse_input()
+    args = parse_input().parse_args()
 
-    branch = read_branch(args.ntuple, args.tree, args.branch)
+    plot_style(True, style='ggplot')
 
-    mean = branch.mean()
-    std = branch.std()
-    histo, bins = gen_histo(branch, args.bins)
+    ntp = uproot.open(args.ref)
+    branch = read_branch(ntp, args.ref_tree, args.ref_branch)
+    histo, bins = gen_histo(branch)
 
-    plot_single_branch(histo, bins, args.output, args.branch, branch.size, mean,
-                       std, args.yAxisScale)
+    plot_add_args = ax_add_args_default(
+        branch.size, branch.mean(), branch.std())
+    plot_histo(histo, bins, plot_add_args, args.output,
+               title=args.ref_branch, yscale=args.y_axis_scale)
