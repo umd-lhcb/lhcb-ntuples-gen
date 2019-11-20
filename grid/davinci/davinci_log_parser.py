c96ec4c2cbec3dc7ef73b1d7ec6b5dc363888c5d
Yipeng Sun <syp@umd.edu>
2019-11-20 02:43:47 -0500

    WIP: Implement barebone state machine for parsing DaVinci log files.

--- grid/davinci/davinci_log_parser.py
+++ grid/davinci/davinci_log_parser.py
@@ -2,25 +2,25 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Tue Nov 19, 2019 at 05:28 PM -0500
+# Last Change: Wed Nov 20, 2019 at 02:43 AM -0500
 
 import re
 import sys
 
+from collections import OrderedDict as odict
+from abc import ABC, abstractmethod
 
-###########
-# Parsing #
-###########
 
-READ_HEADER = -1
-TERMINATE = 0
-
+##############
+# Data store #
+##############
+# Store info (total num. of input, num. of events after each cut, etc.) for each
+# selection.
 
 class Selection(object):
     def __init__(self, selection):
         self.selection = selection
 
-        self.counter = READ_HEADER
         self.headers = None
         self.result = dict()
 
@@ -39,10 +39,49 @@ class Selection(object):
         return [i for i in map(lambda x: x.strip(), entries) if i != '']
 
 
-class DaVinciLogParser(object):
-    def __init__(self, file):
-        self.file = file
-        self.result = dict()
+#################
+# State machine #
+#################
+# For parsing.
 
-    def parse(self):
-        pass
+class State(ABC):
+    @abstractmethod
+    def run(self, data):
+        '''
+        Action on current data.
+        '''
+
+    @abstractmethod
+    def next(self, data):
+        '''
+        Determine next state
+        '''
+
+
+class StateMachine(ABC):
+    def __init__(self, filename, init_state):
+        self.filename = filename
+        self.init_state = init_state
+        self.current_state = init_state
+
+    def run_all(self):
+        with open(self.filename) as f:
+            for line in f:
+                data = self.data(line)
+                self.current_state = self.current_state.next(data)
+                self.current_state.run(data)
+
+    @abstractmethod
+    def data(self, raw_data):
+        '''
+        Pack data for each state to consume.
+        '''
+
+
+##########
+# Parser #
+##########
+
+class DaVinciLogParser(StateMachine):
+    def __init__(self, *args):
+        super().__init__(*args)
