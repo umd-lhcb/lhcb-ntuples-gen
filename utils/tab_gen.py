c24ddc69e54df315c4b63a04cc7b7b9b154ffe6a
Yipeng Sun <syp@umd.edu>
2020-03-10 22:43:18 +0800

    Simple table generator.

---
+++ utils/tab_gen.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# Author: Yipeng Sun
+# License: BSD 2-clause
+# Last Change: Tue Mar 10, 2020 at 10:42 PM +0800
+
+import fileinput
+
+from argparse import ArgumentParser
+from tabulate import tabulate
+
+
+################################
+# Command line argument parser #
+################################
+
+def parse_input(descr='table generator taking stdin as input.'):
+    parser = ArgumentParser(description=descr)
+
+    parser.add_argument('-f', '--format',
+                        nargs='?',
+                        choices=['latex', 'simple', 'github'],
+                        default='latex_booktab',
+                        help='specify the output table format.'
+                        )
+
+    parser.add_argument('-n', '--no-math-env',
+                        action='store_false',
+                        help="Don't wrap content of each cell with $ sign."
+                        )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_input()
+
+    output = []
+    for l in fileinput.input():
+        output.append(l.rstrip().split(','))
+
+    print(tabulate(output, tablefmt=args.format, headers='firstrow'))
