f5c6d03a5ef2b28898d62cfd9d156f90c8461d1f
Yipeng Sun <syp@umd.edu>
2020-03-10 22:57:06 +0800

    Don't escape LaTeX characters.

--- utils/tab_gen.py
+++ utils/tab_gen.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Tue Mar 10, 2020 at 10:49 PM +0800
+# Last Change: Tue Mar 10, 2020 at 10:57 PM +0800
 
 import fileinput
 
@@ -19,14 +19,13 @@ def parse_input(descr='table generator taking stdin as input.'):
 
     parser.add_argument('-f', '--format',
                         nargs='?',
-                        choices=['latex', 'simple', 'github', 'latex_booktabs'],
-                        default='latex_booktabs',
+                        choices=['latex',
+                                 'simple',
+                                 'github',
+                                 'latex_booktabs',
+                                 'latex_raw'],
+                        default='latex_raw',
                         help='specify the output table format.'
-                        )
-
-    parser.add_argument('-n', '--no-math-env',
-                        action='store_false',
-                        help="Don't wrap content of each cell with $ sign."
                         )
 
     return parser.parse_args()
@@ -37,6 +36,9 @@ if __name__ == '__main__':
 
     output = []
     for l in fileinput.input(files=('-',)):
-        output.append(l.rstrip().split(','))
+        row = l.rstrip().split(',')
+        # Strip out head and tail whitespace unconditionally
+        row = [x.strip() for x in row]
+        output.append(row)
 
     print(tabulate(output, headers='firstrow', tablefmt=args.format))
