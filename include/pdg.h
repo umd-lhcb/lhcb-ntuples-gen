d63286ffb098c43703571c91a9976e298492d426
Yipeng Sun <syp@umd.edu>
2020-05-28 01:21:00 +0800

    WIP #34: Include common units and D0 mass PDG value.

---
+++ include/pdg.h
@@ -0,0 +1,12 @@
+// Author: Yipeng Sun
+// License: BSD 2-clause
+// Last Change: Thu May 28, 2020 at 12:38 AM +0800
+
+#ifndef _LNG_PDG_H_
+#define _LNG_PDG_H_
+
+#include "unit.h"
+
+#define PDG_M_D0 1864.84*MeV
+
+#endif
---
+++ include/unit.h
@@ -0,0 +1,11 @@
+// Author: Yipeng Sun
+// License: BSD 2-clause
+// Last Change: Wed May 27, 2020 at 04:07 AM +0800
+
+#ifndef _LNG_UNIT_H_
+#define _LNG_UNIT_H_
+
+#define MeV 1
+#define GeV 1000
+
+#endif
