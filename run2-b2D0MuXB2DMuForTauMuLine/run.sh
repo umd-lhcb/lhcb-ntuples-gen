f79b1725ca143c112de5ac738212705bcf226a3b
Yipeng Sun <syp@umd.edu>
2020-06-04 01:36:22 +0800

    Run over 5000 events when debugging, for run 2.

--- run2-b2D0MuXB2DMuForTauMuLine/run.sh
+++ run2-b2D0MuXB2DMuForTauMuLine/run.sh
@@ -3,7 +3,7 @@
 case $3 in
     debug)
         run gaudirun.py $2 $1 \
-            --option="from Configurables import DaVinci; DaVinci().EvtMax = 10"
+            --option="from Configurables import DaVinci; DaVinci().EvtMax = 5000"
         ;;
 
     *)
