56417ac2352858919cb294dcf3137d23107be4d6
Yipeng Sun <syp@umd.edu>
2020-06-07 14:12:13 +0800

    Update particle mass and the source.

--- include/pdg.h
+++ include/pdg.h
@@ -1,14 +1,17 @@
 // Author: Yipeng Sun
 // License: BSD 2-clause
-// Last Change: Thu May 28, 2020 at 02:52 AM +0800
+// Last Change: Sun Jun 07, 2020 at 02:12 PM +0800
+//
+// NOTE: The particle masses are obtained from here:
+//   https://gitlab.cern.ch/lhcb-conddb/DDDB/-/blob/master/param/ParticleTable.txt
 
 #ifndef _LNG_PDG_H_
 #define _LNG_PDG_H_
 
 #include "unit.h"
 
-#define PDG_M_B0  5279.61*MeV
-#define PDG_M_Dst 2010.27*MeV
-#define PDG_M_D0  1864.84*MeV
+#define PDG_M_B0  5279.64*MeV
+#define PDG_M_Dst 2010.26*MeV
+#define PDG_M_D0  1864.83*MeV
 
 #endif
