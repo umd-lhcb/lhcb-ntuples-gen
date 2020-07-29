38200896332f164c6f76b3c8ee4067387746a32e
Yipeng Sun <syp@umd.edu>
2020-07-30 03:45:09 +0800

    Cleanup script to get MC size.

--- scripts/size_mc_samples.py
+++ scripts/size_mc_samples.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
+#
+# Description: Script to count
 
-###### Script to count 
-import os, sys, subprocess
-import glob
-import string
-import argparse
+from argparse import ArgumentParser
+from subprocess import check_output
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument("-i", "--input", help="MC ID of sample", default="14543010")
-args = parser.parse_args()
 
 class bcolors:
     BOLD = '\033[1m'
     ENDC = '\033[0m'
 
-output = subprocess.check_output(['lb-run','lhcbdirac', 'dirac-bookkeeping-decays-path', args.input])
 
-nrun1 = 0
-nrun2 = 0
-lines = output.decode().replace('\n','').replace(',', '').replace('(', '').replace("'", '').split(')')
-for line in lines:
-    splits = line.split(' ')
-    if(len(splits) > 2):
-        nevents = int(splits[-2])
-        sample = splits[0]
-        print(f'{nevents:12,d}' + '   ' + sample)
-        if('2015' in sample or '2016' in sample or '2017' in sample or '2018' in sample): nrun2 += nevents
-        else: nrun1 += nevents
+def parse_input():
+    parser = ArgumentParser(description='Process some integers.')
+    parser.add_argument("-i", "--input", help="MC ID of sample", default="14543010")
+    return parser.parse_args()
 
 
-print('\n Total number of events for MC ID '+args.input+' in Run 1 is '+bcolors.BOLD+f'{nrun1:,d}'+bcolors.ENDC+' and in Run 2 is '+bcolors.BOLD+f'{nrun2:,d}'+bcolors.ENDC+'\n')
+def decode_dirac_output(output):
+    nrun1 = 0
+    nrun2 = 0
+    lines = output.decode().replace('\n', '').replace(',', '').replace('(', '').replace("'", '').split(')')
+
+    for line in lines:
+        splits = line.split(' ')
+        if(len(splits) > 2):
+            nevents = int(splits[-2])
+            sample = splits[0]
+            print(f'{nevents:12,d}' + '   ' + sample)
+
+            if ('2015' in sample or '2016' in sample or '2017' in sample or '2018' in sample):
+                nrun2 += nevents
+            else:
+                nrun1 += nevents
+
+    print('\n Total number of events for MC ID '+args.input+' in Run 1 is '+bcolors.BOLD+f'{nrun1:,d}'+bcolors.ENDC+' and in Run 2 is '+bcolors.BOLD+f'{nrun2:,d}'+bcolors.ENDC+'\n')
 
 
-
+if __name__ == '__main__':
+    args = parse_input()
+    dirac_output = check_output(
+        ['lb-run', 'lhcbdirac', 'dirac-bookkeeping-decays-path', args.input])
+    decode_dirac_output(dirac_output)
