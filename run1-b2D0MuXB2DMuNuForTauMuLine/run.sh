e53195a07e92c981fed1bd9a2eba6a1ddd729593
Yipeng Sun <syp@umd.edu>
2020-08-06 17:54:05 +0800

    WIP #49: Update the wrapper script to use DaVinci/v36r1p2.

--- run1-b2D0MuXB2DMuNuForTauMuLine/run.sh
+++ run1-b2D0MuXB2DMuNuForTauMuLine/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+export CMTCONFIG=x86_64-slc6-gcc48-opt
+
 case $3 in
     debug)
-        run gaudirun.py $2 $1 \
+        lb-run DaVinci/v36r1p2 gaudirun.py $2 $1 \
             --option="from Configurables import DaVinci; DaVinci().EvtMax = 5000"
         ;;
 
     *)
-        run gaudirun.py $2 $1
+        lb-run DaVinci/v36r1p2 gaudirun.py $2 $1
         ;;
 esac
