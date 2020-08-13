4ff23e406c80c63f355025f3f811e119caa82242
Yipeng Sun <syp@umd.edu>
2020-08-14 02:06:58 +0800

    Add cond files for refit D* only.

---
+++ run2-b2D0MuXB2DMuForTauMuLine/conds/cond-std-2016-refit_dst_only.py
@@ -0,0 +1,17 @@
+from Configurables import DaVinci
+
+DaVinci().DataType = '2016'
+DaVinci().Simulation = False
+
+DaVinci().TupleFile = 'std-refit_dst_only.root'
+# DaVinci().HistogramFile = 'std-histo.root'
+
+DaVinci().MoniSequence += ['REFIT_DST_ONLY']
+
+
+from GaudiConf import IOHelper
+
+IOHelper().inputFiles([
+    './data/data-2016-md/00069529_00017556_1.semileptonic.dst',  # 1.2 GB
+    './data/data-2016-md/00069527_00003141_1.semileptonic.dst',  # 3.5 GB
+], clear=True)
