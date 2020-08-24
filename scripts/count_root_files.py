abf465623b23baa92204845fb12e2c8ef3ec6178
Yipeng Sun <syp@umd.edu>
2020-08-25 00:27:34 +0800

    WIP #46: Naive output, need for tweaks on formatting.

--- scripts/count_root_files.py
+++ scripts/count_root_files.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Tue Aug 25, 2020 at 12:00 AM +0800
+# Last Change: Tue Aug 25, 2020 at 12:27 AM +0800
 
 import sys
 import json
@@ -21,6 +21,33 @@ def find_annex_info(dirs):
         for d in dirs]
 
 
+def file_size_prettifier(size):
+    size = int(size)
+    units = ['KiB', 'MiB', 'GiB']
+
+    for unit in units:
+        size = size / 1024
+        if size <= 1 or len(str(int(size))) <= 3:
+            break
+
+    return (size, unit)
+
+
+def print_output(annex_info):
+    for i in annex_info:
+        tot_size, tot_unit = file_size_prettifier(
+            i['size of annexed files in working tree'])
+        loc_size, loc_unit = file_size_prettifier(i['local annex size'])
+
+        print('{:4} .root   total: {:5.2f} {}   local: {:5.2f} {}   {}'.format(
+            i['annexed files in working tree'],
+            tot_size, tot_unit,
+            loc_size, loc_unit,
+            i['directory']
+        ))
+
+
 if __name__ == '__main__':
     dirs = find_all_subdirs(sys.argv[1])
     dirs_annex_info = find_annex_info(dirs)
+    print_output(dirs_annex_info)
