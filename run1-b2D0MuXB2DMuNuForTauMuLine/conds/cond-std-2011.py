8f7172d05d1ae491c8547ba801d878294eb3a703
Yipeng Sun <syp@umd.edu>
2020-09-21 02:00:49 +0800

    Add a 2011 cond file.

---
+++ run1-b2D0MuXB2DMuNuForTauMuLine/conds/cond-std-2011.py
@@ -0,0 +1,11 @@
+from Configurables import DaVinci
+
+DaVinci().DataType = '2011'
+DaVinci().Simulation = False
+
+DaVinci().TupleFile = 'std.root'
+# DaVinci().HistogramFile = 'std-histo.root'
+
+# Specify tags
+# DaVinci().DDDBtag = 'dddb-20130929-1'
+# DaVinci().CondDBtag = 'cond-20141007'
