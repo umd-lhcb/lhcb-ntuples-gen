036b66552cf6a1e4221acdbdde85923d4d46034d
Yipeng Sun <syp@umd.edu>
2020-10-12 15:47:24 +0800

    Rename cutflow target names.

--- postprocess/make/rdx.mk
+++ postprocess/make/rdx.mk
@@ -1,6 +1,6 @@
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Mon Oct 12, 2020 at 03:19 PM +0800
+# Last Change: Mon Oct 12, 2020 at 03:42 PM +0800
 # Description: Targets for R(D(*))
 
 VPATH := run1-rdx/samples:run2-rdx/samples:$(VPATH)
@@ -19,13 +19,13 @@ VPATH := gen/run1-Dst-step2:gen/run2-Dst-step2:$(VPATH)
 ###########################
 
 # Cutflow output YAML for D*, MC, bare.
-gen/cutflow/output-run1-bare.yml: \
+gen/cutflow/output-rdst-run1-bare.yml: \
 	Dst--20_06_05--cutflow_mc--bare--MC_2011_Beam3500GeV-2011-MagDown-Nu2-Pythia8_Sim08h_Digi13_Trig0x40760037_Reco14c_Stripping20r1NoPrescalingFlagged_11874091_ALLSTREAMS.DST.root \
 	input-run1-bare.yml \
 	cutflow_output_yml_gen.py
 	@$(word 3, $^) $< $(word 2, $^) $@ run1-bare -t 'TupleB0/DecayTree'
 
-gen/cutflow/output-run2-bare.yml: \
+gen/cutflow/output-rdst-run2-bare.yml: \
 	Dst--20_06_05--cutflow_mc--bare--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09b_Trig0x6138160F_Reco16_Turbo03_Stripping26NoPrescalingFlagged_11874091_ALLSTREAMS.DST.root \
 	input-run2-bare.yml \
 	cutflow_output_yml_gen.py
@@ -33,13 +33,13 @@ gen/cutflow/output-run2-bare.yml: \
 
 
 # Cutflow output YAML for D*, MC.
-gen/cutflow/output-run1.yml: \
+gen/cutflow/output-rdst-run1.yml: \
 	Dst--20_03_18--cutflow_mc--cocktail--2011--md.root \
 	input-run1.yml \
 	cutflow_output_yml_gen_pre_0.9.0.py
 	@$(word 3, $^) $< $(word 2, $^) $@ run1 -t 'TupleB0/DecayTree'
 
-gen/cutflow/output-run2.yml: \
+gen/cutflow/output-rdst-run2.yml: \
 	Dst--20_03_18--cutflow_mc--cocktail--2016--md.root \
 	input-run2.yml \
 	cutflow_output_yml_gen_pre_0.9.0.py
@@ -47,13 +47,13 @@ gen/cutflow/output-run2.yml: \
 
 
 # Cutflow output YAML for D*, data.
-gen/cutflow/output-run1-data.yml: \
+gen/cutflow/output-rdst-run1-data.yml: \
 	Dst--20_04_03--cutflow_data--data--2012--md.root \
 	input-run1-data.yml \
 	cutflow_output_yml_gen_pre_0.9.0.py
 	@$(word 3, $^) $< $(word 2, $^) $@ run1 -t 'TupleB0/DecayTree'
 
-gen/cutflow/output-run2-data.yml: \
+gen/cutflow/output-rdst-run2-data.yml: \
 	Dst--20_04_03--cutflow_data--data--2016--md.root \
 	input-run2-data.yml \
 	cutflow_output_yml_gen_pre_0.9.0.py
@@ -61,38 +61,38 @@ gen/cutflow/output-run2-data.yml: \
 
 
 # Cutflow output YAML for signal, normalization, and D** yield, run 1
-gen/cutflow/output-run1-%.yml: \
+gen/cutflow/output-rdst-run1-%.yml: \
 	Dst--20_06_05--cutflow_mc--bare--MC_2011_Beam3500GeV-2011-MagDown-Nu2-Pythia8_Sim08h_Digi13_Trig0x40760037_Reco14c_Stripping20r1NoPrescalingFlagged_11874091_ALLSTREAMS.DST.root \
 	cutflow_components.py
 	@$(word 2, $^) $< $@ run1-$* -t 'TupleB0/DecayTree'
 
 
 # Cutflow output YAML for signal, normalization, and D** yield, run 2
-gen/cutflow/output-run2-%.yml: \
+gen/cutflow/output-rdst-run2-%.yml: \
 	Dst--20_06_05--cutflow_mc--bare--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09b_Trig0x6138160F_Reco16_Turbo03_Stripping26NoPrescalingFlagged_11874091_ALLSTREAMS.DST.root \
 	cutflow_components.py
 	@$(word 2, $^) $< $@ run2-$* -t 'TupleB0/DecayTree'
 
 
 # UNUSED: Cutflow for D*, detail: individual
-gen/cutflow/output-run1-individual.yml: \
+gen/cutflow/output-rdst-run1-individual.yml: \
 	Dst--20_03_18--cutflow_mc--cocktail--2011--md.root \
 	cutflow_detail.py
 	@$(word 2, $^) $< $@ run1
 
-gen/cutflow/output-run2-individual.yml: \
+gen/cutflow/output-rdst-run2-individual.yml: \
 	Dst--20_03_18--cutflow_mc--cocktail--2016--md.root \
 	cutflow_detail.py
 	@$(word 2, $^) $< $@ run2
 
 
 # Study the effect of refit D* only
-gen/cutflow/output-run2-cocktail-refit_dst_only-%.yml: \
+gen/cutflow/output-rdst-run2-cocktail-refit_dst_only-%.yml: \
 	Dst_D0--20_08_18--cutflow_mc--refit_dst_only--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09b_Trig0x6138160F_Reco16_Turbo03_Stripping26NoPrescalingFlagged_11874091_ALLSTREAMS.DST.root \
 	cutflow_components.py
 	@$(word 2, $^) $< $@ run2-$* -t 'TupleB0/DecayTree'
 
-gen/cutflow/output-run2-cocktail-full_refit-%.yml: \
+gen/cutflow/output-rdst-run2-cocktail-full_refit-%.yml: \
 	Dst_D0--20_08_18--cutflow_mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09b_Trig0x6138160F_Reco16_Turbo03_Stripping26NoPrescalingFlagged_11874091_ALLSTREAMS.DST.root \
 	cutflow_components.py
 	@$(word 2, $^) $< $@ run2-$* -t 'TupleB0/DecayTree'
@@ -102,99 +102,101 @@ gen/cutflow/output-run2-cocktail-full_refit-%.yml: \
 # Cutflow #
 ###########
 
-.PHONY: cutflow-Dst-bare cutflow-Dst-bare-web \
-	cutflow-Dst-data cutflow-Dst-data-web \
-	cutflow-Dst cutflow-Dst-web \
-	cutflow-sig-nor-dss-run1 cutflow-sig-nor-dss-run2 \
-	cutflow-all
-
-
 # All cutflow studies:
-cutflow-all: \
-	cutflow-Dst cutflow-Dst-bare cutflow-Dst-data \
-	cutflow-sig-nor-dss-run1 cutflow-sig-nor-dss-run2
+.PHONY: cutflow-rdst-all
+cutflow-rdst-all: \
+	cutflow-rdst cutflow-rdst-bare cutflow-rdst-data \
+	cutflow-rdst-run1-sig-nor-dss cutflow-rdst-run2-sig-nor-dss
 
 
 # Cutflow for D*, MC, bare.
-cutflow-Dst-bare: \
-	gen/cutflow/output-run1-bare.yml \
-	gen/cutflow/output-run2-bare.yml \
+.PHONY: cutflow-rdst-bare cutflow-rdst-bare-web
+cutflow-rdst-bare: \
+	gen/cutflow/output-rdst-run1-bare.yml \
+	gen/cutflow/output-rdst-run2-bare.yml \
 	cutflow_gen.py
 	@$(word 3, $^) -o $< -t $(word 2, $^) -n | tabgen.py -f latex_booktabs_raw
 
-cutflow-Dst-bare-web: \
-	gen/cutflow/output-run1-bare.yml \
-	gen/cutflow/output-run2-bare.yml \
+cutflow-rdst-bare-web: \
+	gen/cutflow/output-rdst-run1-bare.yml \
+	gen/cutflow/output-rdst-run2-bare.yml \
 	cutflow_gen.py
 	@$(word 3, $^) -o $< -t $(word 2, $^) -n | tabgen.py -f github
 
 
 # Cutflow for D*, MC.
-cutflow-Dst: \
-	gen/cutflow/output-run1.yml \
-	gen/cutflow/output-run2.yml \
+.PHONY: cutflow-rdst cutflow-rdst-web
+cutflow-rdst: \
+	gen/cutflow/output-rdst-run1.yml \
+	gen/cutflow/output-rdst-run2.yml \
 	cutflow_gen.py
 	@$(word 3, $^) -o $< -t $(word 2, $^) -n | tabgen.py -f latex_booktabs_raw
 
-cutflow-Dst-web: \
-	gen/cutflow/output-run1.yml \
-	gen/cutflow/output-run2.yml \
+cutflow-rdst-web: \
+	gen/cutflow/output-rdst-run1.yml \
+	gen/cutflow/output-rdst-run2.yml \
 	cutflow_gen.py
 	@$(word 3, $^) -o $< -t $(word 2, $^) -n | tabgen.py -f github
 
 
 # Cutflow for D*, data.
-cutflow-Dst-data: \
-	gen/cutflow/output-run1-data.yml \
-	gen/cutflow/output-run2-data.yml \
+.PHONY: cutflow-rdst-data cutflow-rdst-data-web
+cutflow-rdst-data: \
+	gen/cutflow/output-rdst-run1-data.yml \
+	gen/cutflow/output-rdst-run2-data.yml \
 	cutflow_gen.py
 	@$(word 3, $^) -o $< -t $(word 2, $^) -n | tabgen.py -f latex_booktabs_raw
 
-cutflow-Dst-data-web: \
-	gen/cutflow/output-run1-data.yml \
-	gen/cutflow/output-run2-data.yml \
+cutflow-rdst-data-web: \
+	gen/cutflow/output-rdst-run1-data.yml \
+	gen/cutflow/output-rdst-run2-data.yml \
 	cutflow_gen.py
 	@$(word 3, $^) -o $< -t $(word 2, $^) -n | tabgen.py -f github
 
 
 # Cutflow for signal, normalization, and D** yield, run 1
-cutflow-sig-nor-dss-run1: \
-	gen/cutflow/output-run1-sig.yml \
-	gen/cutflow/output-run1-nor.yml \
-	gen/cutflow/output-run1-dss.yml \
+.PHONY: cutflow-rdst-run1-sig-nor-dss
+cutflow-rdst-run1-sig-nor-dss: \
+	gen/cutflow/output-rdst-run1-sig.yml \
+	gen/cutflow/output-rdst-run1-nor.yml \
+	gen/cutflow/output-rdst-run1-dss.yml \
 	table_cutflow_components.py
 	@$(word 4, $^) -s $(word 1, $^) -n $(word 2, $^) -d $(word 3, $^) | tabgen.py -f latex_booktabs_raw
 
 
 # Cutflow for signal, normalization, and D** yield, run 2
-cutflow-sig-nor-dss-run2: \
-	gen/cutflow/output-run2-sig.yml \
-	gen/cutflow/output-run2-nor.yml \
-	gen/cutflow/output-run2-dss.yml \
+.PHONY: cutflow-rdst-run2-sig-nor-dss
+cutflow-rdst-run2-sig-nor-dss: \
+	gen/cutflow/output-rdst-run2-sig.yml \
+	gen/cutflow/output-rdst-run2-nor.yml \
+	gen/cutflow/output-rdst-run2-dss.yml \
 	table_cutflow_components.py
 	@$(word 4, $^) -s $(word 1, $^) -n $(word 2, $^) -d $(word 3, $^) | tabgen.py -f latex_booktabs_raw
 
 
 # UNUSED: Cutflow for D*, detail: individual
-cutflow-Dst-detail-individual: \
-	gen/cutflow/output-run1-individual.yml \
-	gen/cutflow/output-run2-individual.yml \
+.PHONY: cutflow-rdst-detail-individual
+cutflow-rdst-detail-individual: \
+	gen/cutflow/output-rdst-run1-individual.yml \
+	gen/cutflow/output-rdst-run2-individual.yml \
 	cutflow_gen.py
 	@$(word 3, $^) -o $(word 1, $^) -t $(word 2, $^) -n | tabgen.py -f latex_booktabs_raw
 
 
 # Study the effect of refit D* only
-cutflow-sig-nor-dss-run2-cocktail-refit_dst_only: \
-	gen/cutflow/output-run2-cocktail-refit_dst_only-sig.yml \
-	gen/cutflow/output-run2-cocktail-refit_dst_only-nor.yml \
-	gen/cutflow/output-run2-cocktail-refit_dst_only-dss.yml \
+.PHONY: cutflow-rdst-run2-sig-nor-dss-cocktail-refit_dst_only \
+	cutflow-rdst-run2-sig-nor-dss-cocktail-full_refit
+cutflow-rdst-run2-sig-nor-dss-cocktail-refit_dst_only: \
+	gen/cutflow/output-rdst-run2-cocktail-refit_dst_only-sig.yml \
+	gen/cutflow/output-rdst-run2-cocktail-refit_dst_only-nor.yml \
+	gen/cutflow/output-rdst-run2-cocktail-refit_dst_only-dss.yml \
 	table_cutflow_components.py
 	@$(word 4, $^) -s $(word 1, $^) -n $(word 2, $^) -d $(word 3, $^) | tabgen.py -f github
 
-cutflow-sig-nor-dss-run2-cocktail-full_refit: \
-	gen/cutflow/output-run2-cocktail-full_refit-sig.yml \
-	gen/cutflow/output-run2-cocktail-full_refit-nor.yml \
-	gen/cutflow/output-run2-cocktail-full_refit-dss.yml \
+cutflow-rdst-run2-sig-nor-dss-cocktail-full_refit: \
+	gen/cutflow/output-rdst-run2-cocktail-full_refit-sig.yml \
+	gen/cutflow/output-rdst-run2-cocktail-full_refit-nor.yml \
+	gen/cutflow/output-rdst-run2-cocktail-full_refit-dss.yml \
 	table_cutflow_components.py
 	@$(word 4, $^) -s $(word 1, $^) -n $(word 2, $^) -d $(word 3, $^) | tabgen.py -f github
 
