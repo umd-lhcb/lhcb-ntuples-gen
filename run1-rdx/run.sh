af55aa1cea8455be52ed47b6ab9a3561df836da5
Yipeng Sun <syp@umd.edu>
2020-12-21 23:20:14 +0100

    Update helper scripts for DaVinci reconstruction.

--- run1-rdx/run.sh
+++ run1-rdx/run.sh
@@ -2,11 +2,11 @@
 
 case $3 in
     debug)
-        run gaudirun.py $2 $1 \
+        run gaudirun.py $1 ./reco_Dst_D0.py \
             --option="from Configurables import DaVinci; DaVinci().EvtMax = 5000"
         ;;
 
     *)
-        run gaudirun.py $2 $1
+        run gaudirun.py $1 ./reco_Dst_D0.py
         ;;
 esac
