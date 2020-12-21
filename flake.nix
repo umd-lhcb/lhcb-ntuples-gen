c411d96f6af5308b0d047cf2d8d7909c88b3360c
Yipeng Sun <syp@umd.edu>
2020-12-21 03:24:18 +0100

    Switch to flake.

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "ntuple generation with DaVinci and babymaker.";
+
+  inputs = rec {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          # overlays = [ self.overlay ];
+        };
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
+        root = pkgs.root;
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = "lhcb-ntuples-gen";
+          buildInputs = with pythonPackages; [
+            pkgs.clang-tools # For clang-format
+            root
+
+            # Auto completion
+            jedi
+
+            # Linters
+            flake8
+            pylint
+
+            # Python requirements (enough to get a virtualenv going).
+            virtualenvwrapper
+          ];
+
+          shellHook = ''
+            # Allow the use of wheels.
+            SOURCE_DATE_EPOCH=$(date +%s)
+            VENV=./.virtualenv
+
+            if test ! -d $VENV; then
+              virtualenv $VENV
+            fi
+            source $VENV/bin/activate
+
+            # allow for the environment to pick up packages installed with virtualenv
+            export PYTHONPATH=$VENV/${python.sitePackages}/:$PYTHONPATH
+
+            # fix libstdc++.so not found error
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+          '';
+        };
+      });
+}
