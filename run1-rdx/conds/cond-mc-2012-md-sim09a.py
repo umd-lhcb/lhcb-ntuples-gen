a4d244d9c00ac9a0b62bcd26cd239a42750b4cb4
alexfernez <afernez@umd.edu>
2021-01-04 01:43:25 -0500

    update 2012 MC cond file and add dst files for local testing

--- run1-rdx/conds/cond-mc-2012-md-sim09a.py
+++ run1-rdx/conds/cond-mc-2012-md-sim09a.py
@@ -10,3 +10,10 @@ from Configurables import LHCbApp
 
 LHCbApp().CondDBtag = "sim-20160321-2-vc-md100"
 LHCbApp().DDDBtag = "dddb-20150928"
+
+from GaudiConf import IOHelper
+
+IOHelper().inputFiles([
+        './data/Bd2DstMuNu-2012-md-py8-sim09a/00054936_00000051_1.dsttaunu.safestriptrig.dst',
+        './data/Bd2DstMuNu-2012-md-py8-sim09a/00054936_00000076_1.dsttaunu.safestriptrig.dst',
+], clear=True)
