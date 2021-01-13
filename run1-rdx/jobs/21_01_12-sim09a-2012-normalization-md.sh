8e3c3e6e8af6a52151e35301a1115f867551517c
Yipeng Sun <syp@umd.edu>
2021-01-13 13:08:30 +0100

    Small cleanups.

--- run1-rdx/jobs/21_01_12-sim09a-2012-normalization-md.sh
+++ run1-rdx/jobs/21_01_12-sim09a-2012-normalization-md.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-../scripts/ganga/ganga_jobs.py reco_Dst_D0.py conds/cond-mc-2012-md-sim09a.py -p md -d Bd2DstMuNu
+../scripts/ganga/ganga_jobs.py \
+    reco_Dst_D0.py \
+    conds/cond-mc-2012-md-sim09a.py \
+    -p md -d Bd2DstMuNu
