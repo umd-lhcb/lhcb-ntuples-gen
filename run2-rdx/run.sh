132217551c6ef6baa2226181dc4ea6808d9ac940
Yipeng Sun <syp@umd.edu>
2021-03-03 00:59:53 +0100

    Fix: Various inconsistencies in helper scripts.

--- run2-rdx/run.sh
+++ run2-rdx/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-case $3 in
+case $2 in
     debug)
-        run gaudirun.py $1 ./reco_Dst_D0.py \
+        ../DaVinciDev_v45r6/run gaudirun.py $1 ./reco_Dst_D0.py \
             --option="from Configurables import DaVinci; DaVinci().EvtMax = 5000"
         ;;
 
