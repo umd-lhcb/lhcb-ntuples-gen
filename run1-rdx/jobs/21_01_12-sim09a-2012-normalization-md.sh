bab35e742c9aa4448be0c15161d5fb88ec015aa1
Yipeng Sun <syp@umd.edu>
2021-03-15 00:33:20 +0100

    Other ganga-related changes.

--- run1-rdx/jobs/21_01_12-sim09a-2012-normalization-md.sh
+++ run1-rdx/jobs/21_01_12-sim09a-2012-normalization-md.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-../scripts/ganga/ganga_jobs.py \
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+export PATH=$DIR/../../ganga:$PATH
+
+ganga_jobs.py \
     reco_Dst_D0.py \
     conds/cond-mc-2012-md-sim09a.py \
     -p md -d Bd2DstMuNu
