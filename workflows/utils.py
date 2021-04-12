3459d36c946efa7b2249922255606b501ae37b4a
Yipeng Sun <syp@umd.edu>
2021-04-12 22:39:28 +0200

    WIP #61: Remove unneeded abstraction.

--- workflows/utils.py
+++ workflows/utils.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Sat Apr 10, 2021 at 02:30 AM +0200
+# Last Change: Mon Apr 12, 2021 at 10:38 PM +0200
 
 import os.path as os_path
 import shlex
@@ -31,9 +31,13 @@ def gen_date(fmt='%y_%m_%d'):
 ###############
 
 def ensure_dir(path, delete_if_exist=True):
-    if os_path.isdir(path) and delete_if_exist:
-        rmtree(path)
-    makedirs(path)
+    if os_path.isdir(path):
+        if delete_if_exist:
+            rmtree(path)
+            makedirs(path)
+
+    else:
+        makedirs(path)
 
 
 def abs_path(path, base_path=__file__):
@@ -58,84 +62,20 @@ def pipe_executor(cmd, **kwargs):
     return operation
 
 
-@dataclass
-class Executor:
-    op: List
-    filters: Dict[str, Any] = field(default_factory=dict)
-    generic_filters: Dict[str, Any] = field(default_factory=dict)
-    inputs: Dict[str, str] = field(default_factory=dict)
-    keep: Dict[str, str] = field(default_factory=dict)
+def aggragate_output(workdir, output_dir, keep):
+    # Symbolic link generated files that match 'keep' patterns in separate
+    # folders so that it's easier to find them.
+    chdir(os_path.abspath(workdir))
+    output_dir = os_path.abspath(output_dir)
 
+    for d, patterns in keep.items():
+        ensure_dir(d, False)
+        chdir(d)
+        relpath = os_path.relpath(output_dir, os_path.abspath('.'))
 
-class Processor:
-    def __init__(self, inputs, workdir, keep=None, debug=False,
-                 additional_path=[
-                     abs_path('../lib/python/TrackerOnlyEmu/scripts')
-                 ]):
-        self.workdir = workdir
-        self.debug = debug
-        self.inputs = [os_path.abspath(f) for f in inputs]
-        self.keep = dict() if keep is None else keep
-        self.outputs = {-1: self.inputs}
-
-        # Additional PATH
-        if additional_path:
-            environ['PATH'] += pathsep + pathsep.join(additional_path)
-
-    def process(self, executors):
-        print('{}Processing {}...{}'.format(
-            TC.BOLD+TC.GREEN, os_path.basename(self.workdir), TC.END))
-
-        for idx, exe in enumerate(executors):
-            self.keep.update(exe.keep)
-            workdir = os_path.join(os_path.abspath(self.workdir), str(idx))
-            ensure_dir(workdir)
-
-            keys = self.gen_keys(exe)
-            chdir(workdir)
-
-            if self.debug:
-                print('{}DEBUG: Dir: {}{}'.format(TC.YELLOW, TC.END, getcwd()))
-
-            # Now execute all operations
-            for op in exe.op:
-                op(keys, self.debug)
-
-            # Take a snapshot of the current working dir
-            self.outputs[idx] = [os_path.join(workdir, f)
-                                 for f in listdir(workdir)]
-
-    def link_keep(self):
-        # Symbolic link generated files that match 'keep' patterns in separate
-        # folders so that it's easier to find them.
-        chdir(self.workdir)
-        for d, pattern in self.keep.items():
-            ensure_dir(d)
-            chdir(d)
-            for idx in range(len(self.outputs)):
-                for f in glob(os_path.join('..', str(idx), pattern)):
-                    symlink(f, os_path.join('.', os_path.basename(f)))
-
-    def gen_keys(self, exe):
-        keys_filters = {k: f(list(self.outputs.values())[-1])
-                        for k, f in exe.filters.items()}
-        keys_generic_filters = {k: f(self.outputs)
-                                for k, f in exe.generic_filters.items()}
-        keys_filters.update(keys_generic_filters)
-
-        # Always flatten list
-        for k, v in keys_filters.items():
-            if isinstance(v, list):
-                keys_filters[k] = ' '.join(v)
-
-        # Now merge the inputs defined in the executor
-        keys_filters.update(exe.inputs)
-
-        if self.debug:
-            for k, v in keys_filters.items():
-                print('{}DEBUG: Key: {}{} : {}'.format(TC.YELLOW, TC.END, k, v))
-
-        return keys_filters
+        for p in patterns:
+            for f in glob(os_path.join(relpath, p)):
+                symlink(f, os_path.join('.', os_path.basename(f)))
 
 
 ################################
