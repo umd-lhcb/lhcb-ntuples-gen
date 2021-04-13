0467ca76d0578435da0125327ef9a6a7729529b6
Yipeng Sun <syp@umd.edu>
2021-04-13 03:00:49 +0200

    WIP #61: Add Hlt1 emulation.

--- workflows/rdx/mc.sh
+++ workflows/rdx/mc.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 #Author: Yipeng Sun
 #License: GPLv2
-#Last Change: Mon Apr 12, 2021 at 11:10 PM +0200
+#Last Change: Tue Apr 13, 2021 at 02:48 AM +0200
 
 INPUT_NTP=$1
 
-echo $1
+# Hlt1 emulation
+run2-rdx-hlt1.py ${INPUT_NTP} emu_hlt1_b0.root -t TupleB0/DecayTree -B b0
+run2-rdx-hlt1.py ${INPUT_NTP} emu_hlt1_b.root -t TupleBminus/DecayTree -B b
