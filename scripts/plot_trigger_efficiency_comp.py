d5c90e4b680d2ef7ee82afbe2d32442df29cc24a
Yipeng Sun <syp@umd.edu>
2021-04-13 19:29:53 +0200

    WIP #64: Naive comparison plot that works.

--- scripts/plot_trigger_efficiency_comp.py
+++ scripts/plot_trigger_efficiency_comp.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
 #
 # Author: Yipeng Sun
-# Last Change: Tue Apr 13, 2021 at 06:30 PM +0200
+# Last Change: Tue Apr 13, 2021 at 07:17 PM +0200
 
 import uproot
 
+from argparse import Action
 from numpy import logical_and as AND
+from numpy import nan_to_num
 
 from pyTuplingUtils.parse import single_ntuple_parser_no_output
 from pyTuplingUtils.utils import gen_histo
 from pyTuplingUtils.io import read_branches, read_branch
-from pyTuplingUtils.plot import plot_style, plot_histo, ax_add_args_simple
+from pyTuplingUtils.plot import plot_style, plot_two_histos, ax_add_args_simple
 
 
 #################################
 # Command line arguments parser #
 #################################
+
+class DataRangeAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if len(values) % 2 != 0:
+            raise ValueError('Odd number of min, max pairs!')
+
+        values = [float(v) for v in values]
+        min_max_pairs = self.divide_list_in_chunk(values)
+        setattr(namespace, self.dest, min_max_pairs)
+
+    @staticmethod
+    def divide_list_in_chunk(lst, chunk_size=2):
+        return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
+
 
 DESCR = '''
 generate trigger efficiency comparison plots
@@ -52,6 +68,14 @@ specify trigger branches that will be used for efficiency comparison.
 specify triggers to be required True before evaluating efficiency.
 ''')
 
+    parser.add_argument('-D', '--data-range',
+                        nargs='+',
+                        default=[(-10, 10), (-10, 8), (0, 3)],
+                        action=DataRangeAction,
+                        help='''
+specify plotting range for the kinematic variables.
+''')
+
     return parser
 
 
@@ -81,22 +105,25 @@ if __name__ == '__main__':
         eff_branches.append(filtered)
 
     # Now generate efficiency plots regarding some kinematic variables
-    for br in args.kinematic_vars:
+    for br, data_range in zip(args.kinematic_vars, args.data_range):
         raw = read_branch(ntp, args.ref_tree, br)
         filtered = raw[cut]
+        histos = []
+        legends = []
 
         for tr_br in eff_branches:
-            data_range = (-1e7, 1e7)
             histo_orig, bins = gen_histo(
                 filtered, bins=25, data_range=data_range)
             histo_weighted, bins = gen_histo(
                 filtered, bins=25, data_range=data_range,
                 weights=tr_br.astype(int))
-            histo = histo_weighted / histo_orig
 
-            plot_add_args = ax_add_args_simple('test')
-            plot_histo(histo, bins, plot_add_args, 'test.png')
-                       # title=args.ref_branch, yscale=args.y_axis_scale)
-            break  # Let's stop immediately.
+            # Replace NaN with 0
+            histos.append(nan_to_num(histo_weighted / histo_orig))
+            legends.append(dict())
 
-        break
+        filename = '_'.join([args.output_prefix, br])
+
+        plot_two_histos(
+            histos[0], bins, histos[1], bins,
+            dict(), dict(), output=filename)
