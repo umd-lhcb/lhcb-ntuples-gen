c5cd35867fa7a6f016f71f72bea081831c7f8fc2
Yipeng Sun <syp@umd.edu>
2021-04-21 03:14:37 +0200

    Submitter for trigger emulation verification.

---
+++ run2-rdx/jobs/21_04_21-trigger_emu_verify.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+export PATH=$DIR/../../ganga:$PATH
+
+ganga_jobs.py \
+    ../reco_Dst_D0.py \
+    ../conds/cond-mc-2016-md-sim09j.py \
+    -p md -d 11574021
+
+ganga_jobs.py \
+    ../reco_Dst_D0.py \
+    ../conds/cond-mc-2016-md-sim09j-tracker_only.py \
+    -p md -d 11574021
