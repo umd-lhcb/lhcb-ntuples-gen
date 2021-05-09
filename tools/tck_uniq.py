37c0edcfedcf9e3729f4e16e12a4cf24d5baf847
Yipeng Sun <syp@umd.edu>
2021-05-09 03:09:56 +0200

    Add a tool to print unique TCKs of a given tree.

---
+++ tools/tck_uniq.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Author: Yipeng Sun
+# License: BSD 2-clause
+# Last Change: Sun May 09, 2021 at 03:08 AM +0200
+# NOTE: For tracker-only, the TCK branches are identically 0!
+
+import uproot
+import numpy as np
+
+from argparse import ArgumentParser
+from pyTuplingUtils.io import read_branches
+
+
+################################
+# Command line argument parser #
+################################
+
+def parse_input():
+    parser = ArgumentParser(
+        description='Find all unique L0-HLT1-HLT2 TCK combo in a given tree.')
+
+    parser.add_argument('ntp', help='specify input ntuple.')
+
+    parser.add_argument('tree', help='specify ntuple tree.')
+
+    parser.add_argument('--l0', default='L0DUTCK',
+                        help='specify L0 TCK branch.')
+
+    parser.add_argument('--hlt1', default='HLT1TCK',
+                        help='specify HLT1 TCK branch.')
+
+    parser.add_argument('--hlt2', default='HLT2TCK',
+                        help='specify HLT2 TCK branch.')
+
+    return parser.parse_args()
+
+
+########
+# Main #
+########
+
+if __name__ == '__main__':
+    args = parse_input()
+    ntp = uproot.open(args.ntp)
+
+    tck_raw = read_branches(ntp, args.tree, (args.l0, args.hlt1, args.hlt2),
+                            transpose=True)
+    tck = list(map(lambda x: '-'.join([hex(i) for i in x]), tck_raw))
+    tck_uniq = np.unique(tck)
+
+    print('Unique TCKs (L0-HLT1-HLT2):')
+    for t in tck_uniq:
+        print(t)
