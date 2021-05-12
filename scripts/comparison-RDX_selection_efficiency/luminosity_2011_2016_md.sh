60015397ee3d867bce151a7f7ec7d8d77679c78b
Yipeng Sun <syp@umd.edu>
2021-05-12 02:32:40 +0200

    Make luminosity query work.

--- scripts/comparison-RDX_selection_efficiency/luminosity_2011_2016_md.sh
+++ scripts/comparison-RDX_selection_efficiency/luminosity_2011_2016_md.sh
@@ -2,7 +2,7 @@
 # NOTE: Don't forget to run lhcb-proxy-init
 
 # 2011 MD
-dirac-bookkeeping-get-stats --BK /LHCb/Collision11/Beam3500GeV-VeloClosed-MagDown/Real Data/Reco14/Stripping21r1/90000000/SEMILEPTONIC.DST
+lb-dirac dirac-bookkeeping-get-stats --BK "/LHCb/Collision11/Beam3500GeV-VeloClosed-MagDown/Real Data/Reco14/Stripping21r1/90000000/SEMILEPTONIC.DST"
 
-# 2012 MD
-dirac-bookkeeping-get-stats --BK /LHCb/Collision16/Beam6500GeV-VeloClosed-MagDown/Real Data/Reco16/Stripping28r1/90000000/SEMILEPTONIC.DST
+# 2016 MD
+lb-dirac dirac-bookkeeping-get-stats --BK "/LHCb/Collision16/Beam6500GeV-VeloClosed-MagDown/Real Data/Reco16/Stripping28r1/90000000/SEMILEPTONIC.DST"
