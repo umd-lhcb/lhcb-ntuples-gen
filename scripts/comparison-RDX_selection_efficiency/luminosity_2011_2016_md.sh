e292faa3dd786de0fa5f2f7e5bca53eb86bc146f
Yipeng Sun <syp@umd.edu>
2021-05-12 02:19:55 +0200

    WIP #71: Get 2011 and 2016 MD luminosity w/ DIRAC tool.

---
+++ scripts/comparison-RDX_selection_efficiency/luminosity_2011_2016_md.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# NOTE: Don't forget to run lhcb-proxy-init
+
+# 2011 MD
+dirac-bookkeeping-get-stats --BK /LHCb/Collision11/Beam3500GeV-VeloClosed-MagDown/Real Data/Reco14/Stripping21r1/90000000/SEMILEPTONIC.DST
+
+# 2012 MD
+dirac-bookkeeping-get-stats --BK /LHCb/Collision16/Beam6500GeV-VeloClosed-MagDown/Real Data/Reco16/Stripping28r1/90000000/SEMILEPTONIC.DST
