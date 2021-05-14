2580a87817ab627350486016e76b9e6ff9ebdb74
Yipeng Sun <syp@umd.edu>
2021-05-14 04:21:45 +0200

    Update ganga job GRID submission scripts.

--- run1-rdx/jobs/21_01_12-sim09a-2012-normalization-md.sh
+++ run1-rdx/jobs/21_01_12-sim09a-2012-normalization-md.sh
@@ -4,6 +4,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 export PATH=$DIR/../../ganga:$PATH
 
 ganga_jobs.py \
-    reco_Dst_D0.py \
-    conds/cond-mc-2012-md-sim09a.py \
+    ../reco_Dst_D0.py \
+    ../conds/cond-mc-2012-sim09a.py \
     -p md -d Bd2DstMuNu
--- run2-rdx/jobs/21_04_21-trigger_emu_verify.sh
+++ run2-rdx/jobs/21_04_21-trigger_emu_verify.sh
@@ -5,10 +5,10 @@ export PATH=$DIR/../../ganga:$PATH
 
 ganga_jobs.py \
     ../reco_Dst_D0.py \
-    ../conds/cond-mc-2016-md-sim09j.py \
+    ../conds/cond-mc-2016-sim09j.py \
     -p md -d 11574021
 
 ganga_jobs.py \
     ../reco_Dst_D0.py \
-    ../conds/cond-mc-2016-md-sim09j-tracker_only.py \
+    ../conds/cond-mc-2016-sim09j-tracker_only.py \
     -p md -d 11574021
