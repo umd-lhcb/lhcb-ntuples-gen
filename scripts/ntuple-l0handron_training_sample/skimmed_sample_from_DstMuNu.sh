b098bde52d2f125612da7dc839e40e597a72db97
Yipeng Sun <syp@umd.edu>
2021-05-16 15:59:22 +0200

    Generate L0Hadron BDT training sample.

---
+++ scripts/ntuple-l0handron_training_sample/skimmed_sample_from_DstMuNu.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+haddcut.py bdt_sample.root \
+    ../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09j_Trig0x6139160F_Reco16_Turbo03a_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST.root \
+    -c ../../postprocess/skims/tracker_only_train_sample.yml
