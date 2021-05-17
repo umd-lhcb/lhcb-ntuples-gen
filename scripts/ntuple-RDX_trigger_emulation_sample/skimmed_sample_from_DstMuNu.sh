4dc1d04ffc7565e1e86ac4dcaf52a4d133025cb0
Yipeng Sun <syp@umd.edu>
2021-05-17 03:38:56 +0200

    Use the right input ntuple.

--- scripts/ntuple-RDX_trigger_emulation_sample/skimmed_sample_from_DstMuNu.sh
+++ scripts/ntuple-RDX_trigger_emulation_sample/skimmed_sample_from_DstMuNu.sh
@@ -3,5 +3,5 @@
 # Generate the base ntuple
 ../../tools/haddcut.py \
     rdx-tracker_only.root \
-    ../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09j_Trig0x6139160F_Reco16_Turbo03a_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST.root \
+    ../../run2-rdx/samples/Dst_D0--21_04_12--mc--Bd2DstMuNu--2016--md--py8-sim09j-dv45-subset.root \
     -c ./trigger_emulation_sample.yml
