86478c65de815ff22262fe38e1cb526d0438f3c5
Yipeng Sun <syp@umd.edu>
2021-05-17 03:37:31 +0200

    OCD: Rename.

--- scripts/ntuple-RDX_l0handron_training_sample/bdt_train_sample_from_DstMuNu.sh
+++ scripts/ntuple-RDX_l0handron_training_sample/bdt_train_sample_from_DstMuNu.sh
@@ -4,7 +4,7 @@
 ../../tools/haddcut.py \
     bdt_sample_raw.root \
     ../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09j_Trig0x6139160F_Reco16_Turbo03a_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST.root \
-    -c ./l0_hadron_bdt_train_sample.yml
+    -c ./l0hadron_bdt_train_sample.yml
 
 # Generate the emulated HCAL ET
 ../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py \
@@ -17,10 +17,10 @@ rm -rf ./bdt_sample_tmp.root
 ../../tools/haddcut.py \
     bdt_sample_tmp.root \
     ./emu_l0_hadron.root ./bdt_sample_raw.root \
-    -c ./l0_hadron_bdt_train_sample.yml \
+    -c ./l0hadron_bdt_train_sample.yml \
     -m friend
 
 # Skim final output file
 ../../tools/haddcut.py \
     rdx-bdt_train_sample.root ./bdt_sample_tmp.root \
-    -c ./l0_hadron_bdt_train_sample.yml
+    -c ./l0hadron_bdt_train_sample.yml
