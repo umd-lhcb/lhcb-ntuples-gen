b9c720aca9413fdfd607fb9e2dc78a7ef53ee655
Yipeng Sun <syp@umd.edu>
2021-05-24 20:37:45 +0200

    WIP #71: Apply offline D0 cuts.

--- scripts/cutflow_output_yml_gen.py
+++ scripts/cutflow_output_yml_gen.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Mon May 24, 2021 at 05:51 PM +0200
+# Last Change: Mon May 24, 2021 at 08:37 PM +0200
 
 import pathlib
 import os
@@ -45,14 +45,36 @@ CUTFLOW = {
         Rule('k_Hlt1TrackAllL0Decision_TOS | pi_Hlt1TrackAllL0Decision_TOS', key='Hlt1'),
         Rule('d0_Hlt2CharmHadD02HH_D02KPiDecision_TOS', key='Hlt2'),
         # Stripping
-        Rule('flag_sel_run1_strip(mu_IPCHI2_OWNPV, mu_TRACK_GhostProb, mu_PIDmu, mu_P, mu_TRACK_CHI2NDOF, k_PIDK, k_IPCHI2_OWNPV, k_P, k_PT, k_TRACK_GhostProb, pi_PIDK, pi_IPCHI2_OWNPV, pi_P, pi_PT, pi_TRACK_GhostProb, d0_MM, d0_ENDVERTEX_CHI2, d0_ENDVERTEX_NDOF, d0_FDCHI2_OWNPV, d0_DIRA_OWNPV)',
+        Rule('''flag_sel_run1_strip(mu_IPCHI2_OWNPV, mu_TRACK_GhostProb,
+                                    mu_PIDmu, mu_P, mu_TRACK_CHI2NDOF,
+                                    k_PIDK, k_IPCHI2_OWNPV, k_P, k_PT,
+                                    k_TRACK_GhostProb,
+                                    pi_PIDK, pi_IPCHI2_OWNPV, pi_P, pi_PT,
+                                    pi_TRACK_GhostProb,
+                                    d0_MM, d0_ENDVERTEX_CHI2,
+                                    d0_ENDVERTEX_NDOF, d0_FDCHI2_OWNPV,
+                                    d0_DIRA_OWNPV)''',
              key='Stripping'),
-        Rule('flag_sel_run1_dv(spi_IPCHI2_OWNPV, spi_TRACK_GhostProb, spi_TRACK_CHI2NDOF, d0_M, dst_MM, dst_M, dst_ENDVERTEX_CHI2, dst_ENDVERTEX_NDOF, b0_MM, b0_ENDVERTEX_CHI2, b0_ENDVERTEX_NDOF, b0_DIRA_OWNPV)',
-             key='DaVinci cuts for $D^*$'),
-        # Newer step 2 cuts
-        # Rule('k_PT > 800.0*MeV & !k_isMuon & k_IPCHI2_OWNPV > 45', r'Kaon'),
-        # Rule('pi_PT > 800.0*MeV & !pi_isMuon & pi_IPCHI2_OWNPV > 45', r'Pion'),
-        # Rule('d0_P > 2.0*GeV & d0_FDCHI2_OWNPV > 250 & abs(d0_MM - PDG_M_D0) < 23.4 & ((k_PT > 1.7*GeV & k_Hlt1TrackAllL0Decision_TOS) | (pi_PT > 1.7*GeV & pi_Hlt1TrackAllL0Decision_TOS)) & log(d0_IP_OWNPV) > -3.5 & d0_IPCHI2_OWNPV > 9', r'$D^0 \\rightarrow K \\pi$'),
+        Rule('''flag_sel_run1_dv(spi_IPCHI2_OWNPV, spi_TRACK_GhostProb,
+                                 spi_TRACK_CHI2NDOF,
+                                 d0_M,
+                                 dst_MM, dst_M, dst_ENDVERTEX_CHI2,
+                                 dst_ENDVERTEX_NDOF,
+                                 b0_MM, b0_ENDVERTEX_CHI2, b0_ENDVERTEX_NDOF,
+                                 b0_DIRA_OWNPV)''',
+             key=r'DaVinci cuts for $D^*$'),
+        # Step 2 cuts
+        Rule('''flag_sel_d0_run1(k_PIDK, pi_PIDK, k_isMuon, pi_isMuon,
+                                 k_PT, pi_PT, k_P, pi_P,
+                                 k_Hlt1TrackAllL0Decision_TOS,
+                                 pi_Hlt1TrackAllL0Decision_TOS,
+                                 k_IPCHI2_OWNPV, pi_IPCHI2_OWNPV,
+                                 k_TRACK_GhostProb, pi_TRACK_GhostProb,
+                                 d0_PT, d0_Hlt2CharmHadD02HH_D02KPiDecision_TOS,
+                                 d0_ENDVERTEX_NDOF, d0_ENDVERTEX_CHI2,
+                                 d0_IP_OWNPV, d0_IPCHI2_OWNPV,
+                                 d0_DIRA_OWNPV, d0_FDCHI2_OWNPV, d0_M)''',
+             key=r'$D^0$'),
         # Rule('mu_isMuon & mu_PIDmu > 2 & mu_PIDe < 1 & mu_P < 100.0*GeV & ETA(mu_P,mu_PZ)>1.7 & ETA(mu_P,mu_PZ)<5 & LOG10pp(mu_PX, mu_PY, mu_PZ, k_PX, k_PY, k_PZ)>-6.5 & LOG10pp(mu_PX, mu_PY, mu_PZ, pi_PX, pi_PY, pi_PZ)>-6.5 & LOG10pp(mu_PX, mu_PY, mu_PZ, pi_PX, pi_PY, pi_PZ)>-6.5', r'$\mu$'),
         # Rule('spi_TRACK_GhostProb < 0.25 & dst_ENDVERTEX_CHI2 / dst_ENDVERTEX_NDOF < 10 & abs(dst_MM - d0_MM - 145.43) < 2', r'$D^{*+} \rightarrow D^0 \pi$'),
         # Rule('b0_ISOLATION_BDT < 0.15 & (b0_ENDVERTEX_CHI2/b0_ENDVERTEX_NDOF) < 6 & b0_MM<5280 & b0_DIRA_OWNPV>0.9995 & sin(b0_FlightDir_Zangle)*b0_FD_OWNPV < 7', r'$B^0 \rightarrow D^{*+} \mu$')
@@ -128,8 +150,34 @@ def yaml_gen(data, indent='', indent_increment=' '*4):
 
 load_cpp(header_path + '/functor/rdx/cut.h')
 
+flag_sel_d0_pid_ok_run1 = vectorize(ROOT.FLAG_SEL_D0_PID_OK_RUN1)
+flag_sel_d0_run1_raw = vectorize(ROOT.FLAG_SEL_D0_RUN1)
+
+
+def flag_sel_d0_run1(k_pid_k, pi_pid_k, k_is_mu, pi_is_mu,
+                     k_pt, pi_pt,
+                     k_p, pi_p,
+                     k_hlt1_tos, pi_hlt1_tos,
+                     k_ip_chi2, pi_ip_chi2,
+                     k_gh_prob, pi_gh_prob,
+                     d0_pt,
+                     d0_hlt2,
+                     d0_endvtx_chi2, d0_endvtx_ndof,
+                     d0_ip, d0_ip_chi2,
+                     d0_dira, d0_fd_chi2, d0_m):
+    flag_d0_pid_ok = flag_sel_d0_pid_ok_run1(k_pid_k, pi_pid_k, k_is_mu,
+                                             pi_is_mu)
+    return flag_sel_d0_run1_raw(flag_d0_pid_ok, k_pt, pi_pt, k_p, pi_p,
+                                k_hlt1_tos, pi_hlt1_tos,
+                                k_ip_chi2, pi_ip_chi2, k_gh_prob, pi_gh_prob,
+                                d0_pt, d0_hlt2,
+                                d0_endvtx_chi2, d0_endvtx_ndof,
+                                d0_ip, d0_ip_chi2, d0_dira, d0_fd_chi2, d0_m)
+
+
 KNOWN_FUNC['flag_sel_run1_strip'] = vectorize(ROOT.FLAG_SEL_RUN1_STRIP)
 KNOWN_FUNC['flag_sel_run1_dv'] = vectorize(ROOT.FLAG_SEL_RUN1_DV)
+KNOWN_FUNC['flag_sel_d0_run1'] = vectorize(flag_sel_d0_run1)
 
 
 if __name__ == '__main__':
