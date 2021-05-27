5abb96f20387d6185b436e8104e49701a99681a5
Yipeng Sun <syp@umd.edu>
2021-05-27 15:41:31 +0200

    Add a script to generate debug plots for L0Hadron TOS emulation.

---
+++ scripts/trigger_emulation-l0_hadron_cut_var/l0_hadron_cut_var.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+INPUT_NTP=../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09j_Trig0x6139160F_Reco16_Turbo03a_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST.root
+
+# Emulate L0 Hadron
+../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP} \
+    emu_l0_hadron_debug.root --debug
+
+# Generate comparison plots
+../plot_trigger_efficiency_comp.py \
+    -n ./emu_l0_hadron_debug.root -o b0 -t TupleB0/DecayTree -c \
+    --title "L0Hadron TOS" \
+    --ext png \
+    --triggers d0_l0_hadron_tos d0_l0_hadron_tos_emu_no_bdt \
+    -k k_trg_et k_trg_hcal_et \
+       pi_trg_et pi_trg_hcal_et \
+       k_pi_trg_et_sum k_pi_trg_hcal_et_sum \
+       d0_et_emu_no_bdt \
+    -D 0 6100 0 6100 \
+       0 6100 0 6100 \
+       0 6100 0 6100 \
+       0 6100 \
+    --xlabel "\$K$ trigger \$E_T$" \
+             "\$K$ trigger HCAL \$E_T$" \
+             "\$\\pi$ trigger \$E_T$" \
+             "\$\\pi$ trigger HCAL \$E_T$" \
+             "\$K+\\pi$ trigger \$E_T$" \
+             "\$K+\\pi$ trigger HCAL \$E_T$" \
+             "\$D^0$ emulated \$E_T$ (no BDT)"
