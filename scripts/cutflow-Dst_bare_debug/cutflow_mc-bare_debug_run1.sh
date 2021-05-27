f98512d4c7d39c9577fa0a190a70e4a449400acf
Yipeng Sun <syp@umd.edu>
2021-05-28 00:10:31 +0200

    Update path to sample cutflow ntuple.

--- scripts/cutflow-Dst_bare_debug/cutflow_mc-bare_debug_run1.sh
+++ scripts/cutflow-Dst_bare_debug/cutflow_mc-bare_debug_run1.sh
@@ -2,7 +2,7 @@
 # NOTE: This is just an example! To generate all bare component cutflows, go to
 #       project root, then 'make rdx-cutflow'
 
-RUN1_NTP=../../run1-rdx/cutflow_mc-bare.root
+RUN1_NTP=../../run1-rdx/samples/Dst_D0--21_05_26--cutflow_mc--cocktail--2011--md--dv45-subset-bare.root
 RUN1_LOG=../../run1-rdx/logs/Dst_D0-21_05_26-cutflow_mc-bare.log
 
 # Log
