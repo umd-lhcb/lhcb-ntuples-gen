6a8ce104dda9adf2b8be8e3122d6be6c8e97f00d
Yipeng Sun <syp@umd.edu>
2021-06-02 04:05:37 +0200

    Edit typo in path; small cleanups.

---
+++ scripts/ntuple-RDX_l0hadron_training_sample/bdt_train_sample_from_DstMuNu.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+INPUT_NTP=../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09j_Trig0x6139160F_Reco16_Turbo03a_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST.root
+
+# Generate the base ntuple
+../../tools/haddcut.py \
+    bdt_sample_raw.root \
+    ${INPUT_NTP}
+    -c ./l0hadron_bdt_train_sample.yml
+
+# Generate the emulated HCAL ET
+../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP}
+    emu_l0_hadron.root -t TupleB0/DecayTree
+
+# Merge the two
+rm -rf ./bdt_sample_tmp.root
+../../tools/haddcut.py \
+    bdt_sample_tmp.root \
+    ./emu_l0_hadron.root ./bdt_sample_raw.root \
+    -c ./l0hadron_bdt_train_sample.yml \
+    -m friend
+
+# Skim final output file
+../../tools/haddcut.py \
+    rdx-bdt_train_sample.root ./bdt_sample_tmp.root \
+    -c ./l0hadron_bdt_train_sample.yml
