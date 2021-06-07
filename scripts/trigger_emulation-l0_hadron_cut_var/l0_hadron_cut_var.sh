98cda6262489c8e06b80f8ad3e1c9b9e661fb5ae
Yipeng Sun <syp@umd.edu>
2021-06-07 03:31:16 +0200

    Add more debug plots for L0Hadron TOS.

--- scripts/trigger_emulation-l0_hadron_cut_var/l0_hadron_cut_var.sh
+++ scripts/trigger_emulation-l0_hadron_cut_var/l0_hadron_cut_var.sh
@@ -3,8 +3,10 @@
 INPUT_NTP=../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09j_Trig0x6139160F_Reco16_Turbo03a_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST.root
 
 # Emulate L0 Hadron
-../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP} \
-    emu_l0_hadron_debug.root --debug
+#../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP} \
+    #emu_l0_hadron_debug.root --debug
+#../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP} \
+    #emu_l0_hadron_no_debug.root
 
 # Generate comparison plots
 ../plot_trigger_efficiency_comp.py \
@@ -17,11 +19,11 @@ INPUT_NTP=../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--
        k_pi_trg_et_sum k_pi_trg_hcal_et_sum \
        d0_et_emu_no_bdt \
        k_pi_trg_et_cap \
-    -D 3244 4244 3244 4244 \
-       3244 4244 3244 4244 \
-       3244 4244 3244 4244 \
-       3244 4244 \
-       3244 4244 \
+    -D 744 5744 744 5744 \
+       744 5744 744 5744 \
+       744 5744 744 5744 \
+       744 5744 \
+       744 5744 \
     --xlabel "\$K$ trigger \$E_T$" \
              "\$K$ trigger HCAL \$E_T$" \
              "\$\\pi$ trigger \$E_T$" \
@@ -30,7 +32,7 @@ INPUT_NTP=../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--
              "\$K+\\pi$ trigger HCAL \$E_T$ (capped)" \
              "\$D^0$ emulated \$E_T$ (no BDT)" \
              "\$Max(K, \\pi)$ trigger \$E_T$ (capped)" \
-    --bins 50
+    --bins 25
 
 # Plot differences between Trigger ET variables
 ../plot_two_branches.py -n ./emu_l0_hadron_debug.root -t TupleB0/DecayTree \
@@ -39,9 +41,42 @@ INPUT_NTP=../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--
     -o k_pi_trg_et_comparison.png \
     -XD -10 6200
 
+# Plot the difference between D0 PT and ET
+../plot_two_branches.py -n ./emu_l0_hadron_debug.root -t TupleB0/DecayTree \
+    -b d0_pt_raw -B k_pi_trg_et_cap \
+    -l "\$D^0$ \$p_T$" -L "\$D^0$ HCAL \$E_T$" \
+    -o d0_pt_et_comparison.png \
+    -XD 0 8000 -YD 0 9e4 \
+    --xlabel "\$D^0$ \$E_T$ or \$p_T$"
+
+../plot_two_branches.py -n ./emu_l0_hadron_debug.root -t TupleB0/DecayTree \
+    -b d0_pt_raw -B d0_et_emu_no_bdt \
+    -l "\$D^0$ \$p_T$" -L "\$D^0$ emulated HCAL \$E_T$" \
+    -o d0_pt_et_emu_comparison.png \
+    -XD 0 8000 -YD 0 9e4 \
+    --xlabel "\$D^0$ \$E_T$ or \$p_T$"
+
+../plot_two_branches.py -n ./emu_l0_hadron_debug.root -t TupleB0/DecayTree \
+    -b k_pi_trg_et_cap -B d0_et_emu_no_bdt \
+    -l "\$D^0$ HCAL \$E_T$" -L "\$D^0$ emulated HCAL \$E_T$" \
+    -o d0_et_et_emu_comparison.png \
+    -XD 0 6200 -YD 0 9e4 \
+    --xlabel "\$D^0$ \$E_T$"
+
 # Plot the radial differences
 ../plot_single_branch.py -n ./emu_l0_hadron_debug.root -t TupleB0/DecayTree \
+    -l "\$K$ \$\\pi$ radial distance" \
     -b rdiff_k_pi -o rdiff_k_pi.png
 
 ../plot_single_branch.py -n ./emu_l0_hadron_debug.root -t TupleB0/DecayTree \
+    -l "\$K$ \$\\pi$ radial distance (wrong)" \
     -b rdiff_k_pi_wrong -o rdiff_k_pi_wrong.png
+
+# Plot the effect of nSPDhits < 450 for radial differences
+../plot_single_branch_two_ntuples.py \
+    -n ./emu_l0_hadron_no_debug.root -t TupleB0/DecayTree -b rdiff_k_pi \
+    -N ./emu_l0_hadron_debug.root -T TupleB0/DecayTree -B rdiff_k_pi \
+    -l "no nSPDHits \$< 450$ cut" -L "with nSPDhits cut" \
+    -XD 0 5000 -YD 0 1e5 \
+    --xlabel "\$K$ \$\\pi$ radial distance" \
+    -o rdiff_k_pi_nspd_cut_comp.png
