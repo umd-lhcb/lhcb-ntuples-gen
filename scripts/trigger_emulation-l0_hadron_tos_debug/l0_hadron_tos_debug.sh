c0fe442c62a297a85abd8d03af63648f724cfde5
Yipeng Sun <syp@umd.edu>
2021-06-14 15:37:46 +0200

    Update to latest plotters for L0Hadron TOS debug plots.

--- scripts/trigger_emulation-l0_hadron_tos_debug/l0_hadron_tos_debug.sh
+++ scripts/trigger_emulation-l0_hadron_tos_debug/l0_hadron_tos_debug.sh
@@ -3,36 +3,34 @@
 INPUT_NTP=../../ntuples/0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09j_Trig0x6139160F_Reco16_Turbo03a_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST.root
 
 # Emulate L0 Hadron
-#../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP} \
-    #emu_l0_hadron_debug.root --debug
-#../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP} \
-    #emu_l0_hadron_no_debug.root
+../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP} \
+    emu_l0_hadron_debug.root --debug
+../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron.py ${INPUT_NTP} \
+    emu_l0_hadron_no_debug.root
 
 # Generate comparison plots
-#../plot_trigger_efficiency_comp.py \
-    #-n ./emu_l0_hadron_debug.root -o b0 -t TupleB0/DecayTree -c \
-    #--title "L0Hadron TOS" \
-    #--ext png \
-    #--triggers d0_l0_hadron_tos d0_l0_hadron_tos_emu_no_bdt \
-    #-k k_trg_et k_trg_hcal_et \
-       #pi_trg_et pi_trg_hcal_et \
-       #k_pi_trg_et_sum k_pi_trg_hcal_et_sum \
-       #d0_et_emu_no_bdt \
-       #k_pi_trg_et_cap \
-    #-D 744 5744 744 5744 \
-       #744 5744 744 5744 \
-       #744 5744 744 5744 \
-       #744 5744 \
-       #744 5744 \
-    #--xlabel "\$K$ trigger \$E_T$" \
-             #"\$K$ trigger HCAL \$E_T$" \
-             #"\$\\pi$ trigger \$E_T$" \
-             #"\$\\pi$ trigger HCAL \$E_T$" \
-             #"\$K+\\pi$ trigger \$E_T$ (capped)" \
-             #"\$K+\\pi$ trigger HCAL \$E_T$ (capped)" \
-             #"\$D^0$ emulated \$E_T$ (no BDT)" \
-             #"\$Max(K, \\pi)$ trigger \$E_T$ (capped)" \
-    #--bins 25
+../plot_trigger_efficiencies.py \
+    -n ./emu_l0_hadron_debug.root/TupleB0/DecayTree  \
+    -b d0_l0_hadron_tos d0_l0_hadron_tos_emu_no_bdt \
+    -k k_trg_et k_trg_hcal_et \
+       pi_trg_et pi_trg_hcal_et \
+       k_pi_trg_et_sum k_pi_trg_hcal_et_sum \
+       d0_et_emu_no_bdt \
+       k_pi_trg_et_cap \
+    -D 744 5744 744 5744 \
+       744 5744 744 5744 \
+       744 5744 744 5744 \
+       744 5744 \
+       744 5744 \
+    --xlabel "\$K$ trigger \$E_T$" \
+             "\$K$ trigger HCAL \$E_T$" \
+             "\$\\pi$ trigger \$E_T$" \
+             "\$\\pi$ trigger HCAL \$E_T$" \
+             "\$K+\\pi$ trigger \$E_T$ (capped)" \
+             "\$K+\\pi$ trigger HCAL \$E_T$ (capped)" \
+             "\$D^0$ emulated \$E_T$ (no BDT)" \
+             "\$Max(K, \\pi)$ trigger \$E_T$ (capped)" \
+    -o b0 --bins 25 --title "L0Hadron TOS" --ext png --ratio-plot
 
 # Plot differences between Trigger ET variables
 plotbr -n ./emu_l0_hadron_debug.root/TupleB0/DecayTree \
@@ -101,11 +99,12 @@ plotbr -n ./emu_l0_hadron_debug.root/TupleB0/DecayTree \
 
 # Plot the radial differences vs radial differences (wrong)
 plotbr -n ./emu_l0_hadron_debug.root/TupleB0/DecayTree \
-    -b rdiff_k_pi "rdiff_k_pi_wrong*10" \
+    -b rdiff_k_pi "rdiff_k_pi_wrong*8" \
     -l "\$K$ \$\\pi$ radial distance" \
-       "\$K$ \$\\pi$ radial distance (wrong) \$\\times 10$" \
+       "\$K$ \$\\pi$ radial distance (wrong) \$\\times 8$" \
     --normalize \
     --ylabel "Normalized" \
+    -XD 0 5000 \
     -o rdiff_k_pi_vs_rdiff_k_pi_wrong.png
 
 # Plot the effect of nSPDhits < 450 for radial differences
