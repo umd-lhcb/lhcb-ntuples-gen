8195001035879f9eb7f5055739e3f8e4b1789a80
Yipeng Sun <syp@umd.edu>
2021-07-01 04:04:52 +0200

    WIP umd-lhcb/TrackerOnlyEmu#3: Generate BDT debug plots.
    This is with varying max_depth parameter of the BDT.

---
+++ scripts/trigger_emulation-l0_hadron_tos_bdt_debug/l0_hadron_tos_bdt_debug.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Need to generate a ntuple for BDT training use this script:
+#   ../ntuple-RDX_l0_hadron_tos_training_sample/bdt_train_sample_from_DstMuNu.sh
+
+INPUT_NTP=../ntuple-RDX_l0_hadron_tos_training_sample/rdx-bdt_train_sample.root
+
+# Train BDT w/ different max_depth
+for d in 3
+do
+    echo "========"
+    echo "BDT max_depth is set to ${d}"
+
+    # Generate debug ntuples w/ different BDT max_depth
+    ../../lib/python/TrackerOnlyEmu/scripts/run2-rdx-l0_hadron_train_bdt.py \
+        ${INPUT_NTP} fake_bdt_output \
+        --debug-ntuple "bdt_max_depth_${d}.root" \
+        --debug --no-bdt-export --max-depth $d
+
+    # Debug plots
+    plotbr -n "./bdt_max_depth_${d}.root/TupleB0/DecayTree" \
+        -b d0_et_diff d0_et_diff_pred \
+        -l "\$D^0$ trigger \$-$ emulated \$E_T$ (no BDT)" \
+           "\$D^0$ BDT prediction \$-$ emulated \$E_T$ (no BDT)" \
+        --xlabel "Difference in \$E_T$ [MeV]" \
+        -o "d0_et_diff_max_depth_${d}.png"
+    plotbr -n "./bdt_max_depth_${d}.root/TupleB0/DecayTree" \
+        -b d0_et_trg_pred_diff \
+        -l '' \
+        --xlabel \
+            "\$D^0$ trigger \$-$ BDT prediction \$E_T$ (Resolution) [MeV]" \
+        -o "d0_et_emu_resolution_max_depth_${d}.png"
+done
