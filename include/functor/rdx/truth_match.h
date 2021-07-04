f8fea61c4381560c9935d262c232947ea2297afc
Yipeng Sun <syp@umd.edu>
2021-07-05 01:50:24 +0200

    WIP #78: Add a stub header for truth-matching.

---
+++ include/functor/rdx/truth_match.h
@@ -0,0 +1,15 @@
+// Author: Yipeng Sun
+// License: BSD 2-clause
+// Last Change: Mon Jul 05, 2021 at 01:50 AM +0200
+
+#ifndef _LNG_FUNCTOR_RDX_TRUTH_MATCH_H_
+#define _LNG_FUNCTOR_RDX_TRUTH_MATCH_H_
+
+#include <TMath.h>
+#include <TROOT.h>
+
+Bool_t MC_TRUTH_MATCH_DST(int decay_id) { return true; }
+
+Bool_t MC_TRUTH_MATCH_D0(int decay_id) { return true; }
+
+#endif
