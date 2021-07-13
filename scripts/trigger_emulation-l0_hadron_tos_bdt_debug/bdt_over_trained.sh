ea6e4d0bcebe7e05048b2d4367934ac70ca343f7
Yipeng Sun <syp@umd.edu>
2021-07-13 19:29:38 +0200

    Plot the emulation resolution to show the over-training effect.

--- scripts/trigger_emulation-l0_hadron_tos_bdt_debug/bdt_over_trained.sh
+++ scripts/trigger_emulation-l0_hadron_tos_bdt_debug/bdt_over_trained.sh
@@ -7,3 +7,12 @@ INPUT_NTP=../ntuple-RDX_l0_hadron_tos_training_sample/rdx-bdt_train_sample.root
     --max-depth 40 \
     --debug-ntuple bdt_train_sample.root \
     --test-ntuple bdt_test_sample.root
+
+plotbr \
+    -n bdt_train_sample.root/TupleB0/DecayTree -b d0_et_trg_pred_diff \
+    -n bdt_test_sample.root/TupleB0/DecayTree -b d0_et_trg_pred_diff \
+    --normalize \
+    -XD -2000 2000 \
+    -l "Train sample" -l "Test sample" \
+    -XL "Emulation resolution" -YL "Normalized" \
+    -o d0_et_emu_resolution_over_train.png
