942930f54cf865304fe83b0617e5feb8bdd14c03
Yipeng Sun <syp@umd.edu>
2021-08-16 18:22:39 +0200

    The splitter now works.

--- scripts/split_train_vali_test.py
+++ scripts/split_train_vali_test.py
@@ -2,14 +2,16 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Mon Aug 16, 2021 at 05:27 PM +0200
+# Last Change: Mon Aug 16, 2021 at 06:04 PM +0200
 
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True  # Don't hijack argparse!
 ROOT.PyConfig.DisableRootLogon = True  # Don't read .rootlogon.py
 
 from argparse import ArgumentParser
-from os.path import basename, splitext
+from os.path import basename, splitext, join
+
+from ROOT import gInterpreter, RDataFrame
 
 
 ################################
@@ -60,22 +62,14 @@ def get_filename(path):
 
 
 def get_cuts(train, validation, br='rand_split'):
-    result = dict()
-
-    result['train'] = (
-        '0 <= {}'.format(br),
-        '{} < {}'.format(br, train)
-    )
-    result['valid'] = (
-        '{} <= {}'.format(train, br),
-        '{} < {}'.format(br, train+validation)
-    )
-    result['test'] = (
-        '{} <= {}'.format(train+validation, br),
-        '{} <= 100'.format(br)
-    )
-
-    return result
+    return {
+        'train': '{lb} <= {br} && {br} < {ub}'.format(
+            br=br, lb=0, ub=train),
+        'valid': '{lb} <= {br} && {br} < {ub}'.format(
+            br=br, lb=train, ub=train+validation),
+        'test': '{lb} <= {br} && {br} <= {ub}'.format(
+            br=br, lb=train+validation, ub=100)
+    }
 
 
 ########
@@ -85,7 +79,23 @@ def get_cuts(train, validation, br='rand_split'):
 if __name__ == '__main__':
     args = parse_input()
 
-    cuts = get_cuts(args.train_ratio, args.validation_ratio)
+    gInterpreter.Declare('auto rand_gen = TRandom3({});'.format(args.seed))
+
+    init_frame = RDataFrame(args.tree, args.ntp)
+    rand_frame = init_frame.Define('rand_split', 'rand_gen.Uniform(0, 100)')
     if args.debug:
-        for key, val in cuts.items():
-            print('sample: {}, cuts: {}'.format(key, val))
+        print('loaded {} with {} entries'.format(
+            args.ntp, rand_frame.Count().GetValue()))
+
+    cuts = get_cuts(args.train_ratio, args.validation_ratio)
+    for sample, cut in cuts.items():
+        subsample_frame = rand_frame.Filter(cut)
+        output_ntp = join(args.output_dir, '{}_{}.root'.format(
+            get_filename(args.ntp), sample))
+
+        subsample_frame.Snapshot(args.tree, output_ntp)
+
+        if args.debug:
+            print('sample: {}, cuts: {}'.format(sample, cut))
+            print('wrote {} with {} entries'.format(
+                output_ntp, subsample_frame.Count().GetValue()))
