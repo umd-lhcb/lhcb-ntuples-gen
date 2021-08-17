755fdf383fddd40697f160921e8bfec466cf9973
Yipeng Sun <syp@umd.edu>
2021-08-17 15:20:25 +0200

    WIP #83: Add a demo pidcalib2 script on lxplus.

---
+++ run2-rdx/reweight/pid/run2-rdx-demo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Note: Run this on lxplus!
+
+lb-conda pidcalib pidcalib2.make_eff_hists \
+    --output-dir pidcalib_output \
+    --sample Turbo16 --magnet down \
+    --particle K --pid-cut "DLLK > 4.0" \
+    --bin-var P --bin-var PT --bin-var ETA \
+    --max-files 3  # debug only
