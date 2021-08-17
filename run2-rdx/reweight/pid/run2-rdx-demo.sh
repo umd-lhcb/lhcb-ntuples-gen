b1435215bb61893c3e10e38a10ff7d982b9fa26f
Yipeng Sun <syp@umd.edu>
2021-08-17 15:24:11 +0200

    WIP #83: Don't bin on PT.

--- run2-rdx/reweight/pid/run2-rdx-demo.sh
+++ run2-rdx/reweight/pid/run2-rdx-demo.sh
@@ -6,5 +6,5 @@ lb-conda pidcalib pidcalib2.make_eff_hists \
     --output-dir pidcalib_output \
     --sample Turbo16 --magnet down \
     --particle K --pid-cut "DLLK > 4.0" \
-    --bin-var P --bin-var PT --bin-var ETA \
+    --bin-var P --bin-var ETA \
     --max-files 3  # debug only
