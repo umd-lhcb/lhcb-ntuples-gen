1932ae41a40908c2c0d1fb19f2749f961cdc704d
Yipeng Sun <syp@umd.edu>
2021-09-22 20:01:15 +0200

    WIP #83: More realistic PIDCalib demo; add Mu histo w/ empty bins.

--- run2-rdx/reweight/pid/run2-rdx_demo.sh
+++ run2-rdx/reweight/pid/run2-rdx_demo.sh
@@ -2,9 +2,18 @@
 #
 # Note: Run this on lxplus!
 
+#PARTICLE="K"
+#CUTS="DLLK > 4.0 & IsMuon == 0.0"
+
+#PARTICLE="Pi"
+#CUTS="DLLK < 2.0 & IsMuon == 0.0"
+
+PARTICLE="Mu"
+CUTS="DLLmu > 2.0 & DLLe < 1.0 & IsMuon == 1.0"
+
 lb-conda pidcalib pidcalib2.make_eff_hists \
     --output-dir pidcalib_output \
     --sample Turbo16 --magnet down \
-    --particle K --pid-cut "DLLK > 4" \
-    --bin-var P --bin-var ETA \
-    --max-files 3  # debug only
+    --particle ${PARTICLE} --pid-cut "${CUTS}" \
+    --bin-var P --bin-var ETA
+    #--max-files 3  # debug only
