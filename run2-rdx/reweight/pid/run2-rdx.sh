2f07a36e09efb4c83a15233471695b84f29e5f62
Yipeng Sun <syp@umd.edu>
2021-09-22 04:01:54 +0200

    WIP #83: The PID histos are separated by particle.

--- run2-rdx/reweight/pid/run2-rdx.sh
+++ run2-rdx/reweight/pid/run2-rdx.sh
@@ -2,10 +2,14 @@
 #
 # Note: Run this on lxplus!
 
-lb-conda pidcalib pidcalib2.make_eff_hists \
-    --output-dir pidcalib_output \
-    --sample Turbo16 --magnet down \
-    --particle K --pid-cut "DLLK > 4" \
-    --particle Pi --pid-cut "DLLK < 2" \
-    --particle Mu --pid-cut "DLLmu > -200" \
-    --bin-var P --bin-var ETA
+declare -A SAMPLES
+SAMPLES[K]="DLLK > 4"
+SAMPLES[Pi]="DLLK < 2"
+SAMPLES[Mu]="DLLmu > -200"
+
+for part in "${!SAMPLES[@]}"; do
+    lb-conda pidcalib pidcalib2.make_eff_hists \
+        --output-dir pidcalib_output \
+        --sample Turbo16 --magnet down \
+        --particle ${part} --pid-cut ${SAMPLES[${part}]} \
+        --bin-var P --bin-var ETA
