1a6c884c2cde21afcb13e085c074d585f3d41150
Yipeng Sun <syp@umd.edu>
2021-09-23 04:18:21 +0200

    WIP #83: Add K, Pi PID reweight sample histos.

--- run2-rdx/reweight/pid/run2-rdx_demo.sh
+++ run2-rdx/reweight/pid/run2-rdx_demo.sh
@@ -2,14 +2,9 @@
 #
 # Note: Run this on lxplus!
 
-#PARTICLE="K"
-#CUTS="DLLK > 4.0 & IsMuon == 0.0"
+PARTICLE="K"
+CUTS="DLLK > 4.0 & IsMuon == 0.0"
 
-#PARTICLE="Pi"
-#CUTS="DLLK < 2.0 & IsMuon == 0.0"
-
-PARTICLE="Mu"
-CUTS="DLLmu > 2.0 & DLLe < 1.0 & IsMuon == 1.0"
 
 lb-conda pidcalib pidcalib2.make_eff_hists \
     --output-dir pidcalib_output \
