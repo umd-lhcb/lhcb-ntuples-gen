6247fbdf9141647643a0b9afada8107f06b736a5
Yipeng Sun <syp@umd.edu>
2021-10-16 19:55:55 +0200

    WIP #87: Add Sim09k condition.

---
+++ run2-rdx/conds/cond-mc-2016-md-sim09k-tracker_only.py
@@ -0,0 +1,16 @@
+from Configurables import DaVinci
+
+DaVinci().DataType = '2016'
+DaVinci().Simulation = True
+
+DaVinci().TupleFile = 'mc.root'
+# DaVinci().HistogramFile = 'mc-histo.root'
+
+# Additional global flags
+DaVinci().MoniSequence += ['TRACKER_ONLY']
+
+
+from Configurables import LHCbApp
+
+LHCbApp().CondDBtag = "sim-20170721-2-vc-md100"
+LHCbApp().DDDBtag = "dddb-20170721-3"
