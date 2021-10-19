19e10fac87229fa1bd6f2d0f06f9639fdf061365
Yipeng Sun <syp@umd.edu>
2021-10-19 14:24:55 +0200

    Fix: Correct way to use ProbNN variables in pidcalib2.

--- run2-rdx/reweight/pid/run2-rdx_iso_oldcut.sh
+++ run2-rdx/reweight/pid/run2-rdx_iso_oldcut.sh
@@ -3,8 +3,8 @@
 # Note: Run this on lxplus!
 
 declare -a K_CUTS=(
-    "ProbNNk > 0.2"
-    "ProbNNk < 0.2"
+    "MC15TuneV1_ProbNNk > 0.2"
+    "MC15TuneV1_ProbNNk < 0.2"
 )
 
 for year in 15 16 17 18; do
@@ -13,7 +13,7 @@ for year in 15 16 17 18; do
             lb-conda pidcalib pidcalib2.make_eff_hists \
                 --output-dir pidcalib_output \
                 --sample "Turbo${year}" --magnet ${polarity} \
-                --particle K --pid-cut ${cut} \
+                --particle K --pid-cut "${cut}" \
                 --bin-var Brunel_P --bin-var Brunel_ETA --bin-var nTracks_Brunel
         done
     done
