029e7946f5a5ddec23685df50a744e48b8829e33
Yipeng Sun <syp@umd.edu>
2021-10-22 03:49:25 +0200

    WIP #88: Add more cuts and some color.

--- studies/cutflow-sync_with_phoebe/cutflow-sync_with_phoebe.py
+++ studies/cutflow-sync_with_phoebe/cutflow-sync_with_phoebe.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri Oct 22, 2021 at 03:35 AM +0200
+# Last Change: Fri Oct 22, 2021 at 03:48 AM +0200
 # Note: Here we use Phoebe's latest ntuple
 
 import pathlib
@@ -15,6 +15,7 @@ os.environ['ROOT_INCLUDE_PATH'] = header_path
 
 import ROOT
 from ROOT import RDataFrame
+from pyBabyMaker.base import TermColor as TC
 
 # Load C++ headers
 ROOT.gInterpreter.Declare('#include "functor/rdx/skims.h"')
@@ -26,7 +27,11 @@ ROOT.gInterpreter.Declare('#include "functor/rdx/skims.h"')
 
 DST_CUTS = [
     '(selcounter & (4096 * 64 - 1)) == (4096 * 64 - 1)',
+    'isData && DstIDprod > 0 && IDprod > 0',
     '!muVeto',
+    'dxy < 7.0',
+    '(Hlt1TAL0K && K_PT > 1700.0) || (Hlt1TAL0pi && pi_PT > 1700.0)',
+    'muPID && DLLe < 1.0 && BDTmu > 0.25'
 ]
 
 DST_SKIM_CUTS = {
@@ -42,7 +47,8 @@ def apply_skim_cuts(frame, cuts=DST_SKIM_CUTS):
     for name, cut in cuts.items():
         cut_frame = frame.Filter(cut)
         num = cut_frame.Count().GetValue()
-        print('    After applying {} skim cut: {:,}'.format(name, num))
+        print('    After applying {}{}{} skim cut: {}{:,}{}'.format(
+            TC.BOLD+TC.GREEN, name, TC.END, TC.UNDERLINE, num, TC.END))
 
 
 ########
@@ -56,13 +62,15 @@ if __name__ == '__main__':
 
     for c in DST_CUTS:
         if len(frames_dst) == 1:
-            print('Before applying any cut: {:,}'.format(
-                frames_dst[-1].Count().GetValue()))
+            print('{}Before applying any cut: {}{:,}{}'.format(
+                TC.BOLD, TC.UNDERLINE, frames_dst[-1].Count().GetValue(),
+                TC.END))
 
         frm = frames_dst[-1]
         new_frm = frm.Filter(c)
         num = new_frm.Count().GetValue()
-        print('After applying {}: {:,}'.format(c, num))
+        print('{}After applying {}{}{}: {}{:,}{}'.format(
+            TC.BOLD, TC.YELLOW, c, TC.END+TC.BOLD, TC.UNDERLINE, num, TC.END))
         frames_dst.append(new_frm)
 
         apply_skim_cuts(new_frm)
