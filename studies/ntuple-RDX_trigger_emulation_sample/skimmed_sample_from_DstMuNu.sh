a0a55eca30703e6f05af2016ed8fccd24b759ee2
Yipeng Sun <syp@umd.edu>
2021-10-26 15:54:02 +0200

    Small updates.

--- studies/ntuple-RDX_trigger_emulation_sample/skimmed_sample_from_DstMuNu.sh
+++ studies/ntuple-RDX_trigger_emulation_sample/skimmed_sample_from_DstMuNu.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+NTP=../../run2-rdx/samples/Dst_D0--21_04_12--mc--Bd2DstMuNu--2016--md--py8-sim09j-dv45-subset.root
+
 # Generate the base ntuple
 ../../scripts/haddcut.py \
-    rdx-tracker_only.root \
-    ../../run2-rdx/samples/Dst_D0--21_04_12--mc--Bd2DstMuNu--2016--md--py8-sim09j-dv45-subset.root \
+    rdx-fullsim.root $NTP \
     -c ./trigger_emulation_sample.yml
