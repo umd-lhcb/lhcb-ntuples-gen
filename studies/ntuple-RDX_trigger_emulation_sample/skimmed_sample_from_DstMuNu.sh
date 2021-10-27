86e118622f138bf5f36032765cc56c1022786665
Yipeng Sun <syp@umd.edu>
2021-10-27 02:31:34 +0200

    No need to include a separate trigger emulation test sample.

--- studies/ntuple-RDX_trigger_emulation_sample/skimmed_sample_from_DstMuNu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-NTP=../../run2-rdx/samples/Dst_D0--21_04_12--mc--Bd2DstMuNu--2016--md--py8-sim09j-dv45-subset.root
-
-# Generate the base ntuple
-../../scripts/haddcut.py \
-    rdx-fullsim.root $NTP \
-    -c ./trigger_emulation_sample.yml
