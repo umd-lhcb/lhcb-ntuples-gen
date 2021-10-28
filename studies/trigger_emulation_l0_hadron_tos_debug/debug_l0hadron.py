59d0c937d8a42350a58f700772b7914cb1cf6198
Yipeng Sun <syp@umd.edu>
2021-10-29 01:18:22 +0200

    WIP #89: Rename, reorganize, and consolidate studies.

--- studies/trigger_emulation_l0_hadron_tos_debug/debug_l0hadron.py
+++ studies/trigger_emulation_l0_hadron_tos_debug/debug_l0hadron.py
@@ -152,6 +152,11 @@ def plotL0Hadron(ntpIn, triggers,
                      'Real response in FullSim',
                      'Emulated (no BDT)',
                      'Emulated (BDT)'
+                 ],
+                 cuts=[
+                     'nspdhits < 450',
+                     'nspdhits < 450',
+                     'nspdhits < 450'
                  ]):
     exe = '../../scripts/plot_trigger_efficiencies.py'
 
@@ -160,18 +165,20 @@ def plotL0Hadron(ntpIn, triggers,
         --ratio-plot \\
         -k d0_pt -D 0 20 \\
         -l {legends} \\
+        -c {cuts} \\
         --xlabel "\\$D^0$ \\$p_T$ [GeV]"
     '''.format(ntp=ntpIn, tree=tree, trg=' '.join([f'"{i}"' for i in triggers]),
                outPref=outPref, title=title,
-               legends=' '.join(['"{}"'.format(leg) for leg in legends])
+               legends=' '.join(['"{}"'.format(i) for i in legends]),
+               cuts=' '.join(['"{}"'.format(i) for i in cuts])
                )
     runCmd(cmd)
 
 
 bdtTrgsToPlot = [
     'd0_l0_hadron_tos',
-    'd0_l0_hadron_tos_emu_no_bdt & nspdhits < 450',
-    'd0_l0_hadron_tos_emu_bdt & nspdhits < 450',
+    'd0_l0_hadron_tos_emu_no_bdt',
+    'd0_l0_hadron_tos_emu_bdt',
 ]
 
 xgbTrgsToPlot = [
