83512246a9257da99820df2f95ea4d0cc47617bd
Yipeng Sun <syp@umd.edu>
2021-11-22 02:55:33 +0100

    WIP: More work on building histograms.

--- studies/trigger_emulation-l0_global_tis_debug/debug_l0global.py
+++ studies/trigger_emulation-l0_global_tis_debug/debug_l0global.py
@@ -11,7 +11,7 @@ import sys
 from os.path import isfile
 from os.path import splitext
 
-from ROOT import TFile, TH2D, TMath, std, TH1D
+from ROOT import TFile, TH2D, TH1D, TMath, std, TEfficiency
 from TrackerOnlyEmu.loader import load_file
 
 
@@ -79,6 +79,22 @@ def findBinning(ntpInName, histoName, binLbls):
     return result
 
 
+def buildHistoFromHisto(ntpInName, ntpOutName, histoName, name,
+                        writeMode=NTP_WRT_MODE):
+    ntpIn = TFile.Open(ntpInName, 'READ')
+    ntpOut = TFile.Open(ntpOutName, writeMode)
+
+    histo = ntpIn.Get(histoName)
+    histoX = histo.ProjectionX(f'{name}_proj_x', 1, histo.GetNbinsX())
+    histoY = histo.ProjectionY(f'{name}_proj_y', 1, histo.GetNbinsY())
+
+    ntpOut.cd()
+    histoX.Write()
+    histoY.Write()
+
+    return ntpOutName
+
+
 def buildHisto(ntpInName, ntpOutName, bin_spec, name, x='b0_PZ', y='b0_PT',
                particle='b0', treeName='TupleB0/DecayTree'):
     xbins, ybins = bin_spec
@@ -97,8 +113,6 @@ def buildHisto(ntpInName, ntpOutName, bin_spec, name, x='b0_PZ', y='b0_PT',
     histoTistos = TH2D(f'{name}_tistos', f'{name}_tistos',
                        len(xbins)-1, v_xbins.data(),
                        len(ybins)-1, v_ybins.data())
-    histoEff = TH2D(f'{name}_eff', f'{name}_eff',
-                    len(xbins)-1, v_xbins.data(), len(ybins)-1, v_ybins.data())
 
     ntpIn = TFile.Open(ntpInName, 'READ')
     tree = ntpIn.Get(treeName)
@@ -118,21 +132,33 @@ def buildHisto(ntpInName, ntpOutName, bin_spec, name, x='b0_PZ', y='b0_PT',
         if brTis and brTos:
             histoTistos.Fill(brX, brY)
 
-    # Find the TISTOS efficiency of TIS
-    histoEff.Divide(histoTistos, histoTos)
+    histoTosX = histoTos.ProjectionX(f'{name}_tos_proj_x')
+    histoTosY = histoTos.ProjectionY(f'{name}_tos_proj_y')
+    histoTistosX = histoTistos.ProjectionX(f'{name}_tistos_proj_x')
+    histoTtisosY = histoTistos.ProjectionY(f'{name}_tistos_proj_y')
+
+    # Generate efficiency histograms
+    histoEffProjX = TEfficiency(histoTistosX, histoTosX)
+    histoEffProjX.SetName(f'{name}_eff_proj_x')
+
+    histoEffProjY = TEfficiency(histoTtisosY, histoTosY)
+    histoEffProjY.SetName(f'{name}_eff_proj_y')
 
     ntpOut = TFile.Open(ntpOutName, NTP_WRT_MODE)
     ntpOut.cd()
     histoTot.Write()
     histoTos.Write()
     histoTistos.Write()
-    histoEff.Write()
+
+    histoEffProjX.Write()
+    histoEffProjY.Write()
 
 
 # Rename the trigger efficiency from real data & write in a new file
 ntpData = load_file('<triggers/l0/l0_tis_efficiency.root>')
-ntpOut = renameHisto(ntpData, 'debug_l0_tis.root',
-                     'Jpsi_data_eff1', 'data_2016', writeMode='RECREATE')
+ntpOut = buildHistoFromHisto(
+    ntpData, 'debug_l0_tis.root', 'Jpsi_data_eff1', 'data_2016',
+    writeMode='RECREATE')
 
 # Find the binning scheme for the sample
 histoBinSpec = findBinning(ntpData, 'Jpsi_data_eff1', ['x', 'y'])
