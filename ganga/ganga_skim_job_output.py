23b4caaa04fb28f872896c8de8adfb66883f0e50
Yipeng Sun <syp@umd.edu>
2021-12-28 17:06:03 +0100

    WIP #92: Make the debug version work.

--- ganga/ganga_skim_job_output.py
+++ ganga/ganga_skim_job_output.py
@@ -2,11 +2,21 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Tue Dec 28, 2021 at 04:21 PM +0100
+# Last Change: Tue Dec 28, 2021 at 05:05 PM +0100
+
+import os
 
 from argparse import ArgumentParser
 from glob import glob
-from os.path import basename
+from os.path import basename, abspath
+
+
+#################
+# Configurables #
+#################
+
+PROJECT_ROOT = abspath(f'{abspath(__file__)}/../..')
+OUTPUT_NTP_SUFFIX = '{}-dv'
 
 
 #################################
@@ -19,6 +29,7 @@ def parse_input():
     parser.add_argument('input_folder', help='specify ganga job folder.')
     parser.add_argument('output_folder',
                         help='specify output folder for skimmed ntuples.')
+    parser.add_argument('skim_config', help='specify skim config')
 
     parser.add_argument('-d', '--debug', action='store_true',
                         help='enable debug mode.')
@@ -38,9 +49,37 @@ def is_num(val):
         return False
 
 
-def fltr_subjob_folders(job_folder):
+def find_ntp_name(folders):
+    return basename(glob(f'{folders[0]}/*.root')[0])
+
+
+def fltr_subjob_ntps(job_folder):
     raw = glob(f'{job_folder}/*')
-    return [i for i in raw if is_num(basename(i))]
+    folders = [i+'/output' for i in raw if is_num(basename(i))]
+    ntp_name = find_ntp_name(folders)
+    return [i+f'/{ntp_name}' for i in folders], [basename(i) for i in raw]
+
+
+def output_idx_gen(input_ntp_folder):
+    return input_ntp_folder
+
+
+def output_ntp_name_gen(output_folder, input_ntp_folder):
+    suffix = OUTPUT_NTP_SUFFIX.format(output_idx_gen(input_ntp_folder))
+    return f'{output_folder}--{suffix}.root'
+
+
+def run_skim(debug=False):
+    exe = f'{PROJECT_ROOT}/scripts/haddcut.py'
+
+    def executor(input_ntp, output_ntp, config):
+        cmd = f'{exe} {output_ntp} {input_ntp} -c {config}'
+        if debug:
+            print(cmd)
+        else:
+            os.system(cmd)
+
+    return executor
 
 
 ########
@@ -49,3 +88,13 @@ def fltr_subjob_folders(job_folder):
 
 if __name__ == '__main__':
     args = parse_input()
+
+    if args.debug:
+        print(f'lhcb-ntuples-gen project root path is: {PROJECT_ROOT}')
+
+    exe = run_skim(args.debug)
+
+    input_ntps, folder_idx = fltr_subjob_ntps(args.input_folder)
+    for ntp, idx in zip(input_ntps, folder_idx):
+        output_ntp = f'{args.output_folder}/{output_ntp_name_gen(args.output_folder, idx)}'
+        exe(ntp, output_ntp, args.skim_config)
