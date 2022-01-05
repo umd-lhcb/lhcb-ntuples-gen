4900380c77dde3aa4009499960a084c90e398931
Yipeng Sun <syp@umd.edu>
2022-01-05 18:48:45 +0100

    Looks like more files per job is a better choice.

--- ganga/ganga_jobs.py
+++ ganga/ganga_jobs.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Thu Oct 07, 2021 at 12:46 AM +0200
+# Last Change: Wed Jan 05, 2022 at 06:48 PM +0100
 #
 # Description: A demonstration on ganga option file with parser.
 #              This demo runs stand-alone, provided that Python is installed:
@@ -29,8 +29,8 @@ from ganga_sample_jobs_parser import args, reco_type, lfn, job_name, ntuple_name
 
 PLATFORM = 'x86_64-centos7-gcc9-opt'
 WEIGHT_FILE = 'weights_soft.xml'
-FILES_PER_JOB_DATA = 12
-FILES_PER_JOB_MC = 4
+FILES_PER_JOB_DATA = 100
+FILES_PER_JOB_MC = 8
 
 
 ###########
