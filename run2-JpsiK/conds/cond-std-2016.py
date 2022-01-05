b16941980d2b6adc47868e548e56cad798721e4d
Yipeng Sun <syp@umd.edu>
2022-01-05 02:02:13 +0100

    WIP #97: Update condition file.

--- run2-JpsiK/conds/cond-std-2016.py
+++ run2-JpsiK/conds/cond-std-2016.py
@@ -1,1 +1,17 @@
-../../run2-rdx/conds/cond-std-2016.py
+from Configurables import DaVinci
+
+DaVinci().DataType = '2016'
+DaVinci().Simulation = False
+
+DaVinci().TupleFile = 'std.root'
+# DaVinci().HistogramFile = 'std-histo.root'
+
+
+from GaudiConf import IOHelper
+
+# Stripping v28r2p1
+IOHelper().inputFiles([
+    './data/data-2016-md/00151979_00000281_1.dimuon.dst',
+    './data/data-2016-md/00151979_00000290_1.dimuon.dst',
+    './data/data-2016-md/00151979_00000312_1.dimuon.dst',
+], clear=True)
