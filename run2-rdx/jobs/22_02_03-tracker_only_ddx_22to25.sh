c14500293b9a8a5ff09aa03348294769fa9d1444
Manuel Franco Sevilla <manuelf@physics.ucsb.edu>
2022-02-04 04:01:23 +0100

    Script to send partial DDX jobs

---
+++ run2-rdx/jobs/22_02_03-tracker_only_ddx_22to25.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+export PATH=$DIR/../../ganga:$PATH
+#EXE=ganga_sample_jobs_parser.py
+EXE=ganga_jobs.py
+
+for i in 11874050 11874070 12874010 12874030; do
+    for p in md mu; do
+        $EXE \
+            ../reco_Dst_D0.py \
+            ../conds/cond-mc-2016-$p-sim09k-tracker_only.py \
+            -p $p -d $i
+    done
+done
