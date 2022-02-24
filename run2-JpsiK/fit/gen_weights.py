34679c701534d6513b41e3647b412d02f8afdce0
Yipeng Sun <syp@umd.edu>
2022-02-24 00:51:37 -0500

    WIP #97: Generation of efficiencies for J/psi K.

--- run2-JpsiK/fit/gen_weights.py
+++ run2-JpsiK/fit/gen_weights.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Tue Feb 22, 2022 at 11:54 AM -0500
+# Last Change: Thu Feb 24, 2022 at 12:47 AM -0500
 
 import numpy as np
 
@@ -41,7 +41,7 @@ def parse_input():
     parser.add_argument('-d', '--dataNtp', required=True,
                         help='specify data input ntuples, which must contains sWeight branches.')
 
-    parser.add_argument('-m', '--mcNtp', required=True,
+    parser.add_argument('-m', '--mcNtp', required=True, nargs='+',
                         help='specify MC input ntuples.')
 
     parser.add_argument('-o', '--output', required=True,
@@ -53,15 +53,39 @@ def parse_input():
     return parser.parse_args()
 
 
+###########
+# Helpers #
+###########
+
+def get_weights(histo, branches, bin_specs):
+    bin_idx = []
+
+    for br, spec in zip(branches, bin_specs):
+        tmp_idx = np.digitize(br, spec) - 1
+        # handle over/underflow
+        tmp_idx = np.where(tmp_idx == -1, 0, tmp_idx)
+        tmp_idx = np.where(tmp_idx >= spec.size-1, spec.size-2, tmp_idx)
+        bin_idx.append(tmp_idx)
+
+    return histo[tuple(bin_idx)]
+
+
+
 if __name__ == '__main__':
     args = parse_input()
     ntp = recreate(args.output)
 
-    for name, r in REWEIGHT_PROCEDURE.items():
+    histos = dict()
+    brs_mc_stash = dict()
+
+    for idx, (name, r) in enumerate(REWEIGHT_PROCEDURE.items()):
         brs_data = list(
-            concatenate(args.dataNtp, r.vars, library='np').values())
-        brs_mc = list(
-            concatenate(args.mcNtp, r.vars, library='np').values())
+            concatenate(f'{args.dataNtp}:{args.tree}', r.vars, library='np').values())
+        brs_mc = list(concatenate(
+            [f'{i}:{args.tree}' for i in args.mcNtp],
+            r.vars, library='np'
+        ).values())
+        brs_mc_stash[idx] = brs_mc
 
         scales = [1.0]*len(brs_data) if not r.scale else r.scale
         for i, _ in enumerate(scales):
@@ -69,10 +93,23 @@ if __name__ == '__main__':
             brs_mc[i] = scales[i]*brs_mc[i]
 
         h_data_raw = np.histogram2d(*brs_data, r.bins, r.range)
-        h_mc_raw = np.histogram2d(*brs_mc, r.bins, r.range)
 
-        h_ratio = (h_mc_raw[0]/h_data_raw[0], h_data_raw[1], h_data_raw[2])
+        if idx == 0:
+            h_mc_raw = np.histogram2d(*brs_mc, r.bins, r.range)
+        else:
+            brs_mc_prev = brs_mc_stash[idx]
+            mc_wt = get_weights(
+                histos[idx-1][0], brs_mc_prev, histos[idx-1][1:])
+            h_mc_raw = np.histogram2d(*brs_mc, r.bins, r.range)
+
+        h_ratio_histo = h_data_raw[0] / h_mc_raw[0]
+        h_ratio_histo = h_ratio_histo * (brs_mc[0].size / brs_data[0].size)
+        h_ratio_histo = np.nan_to_num(
+            h_ratio_histo, nan=0.0, posinf=0.0, neginf=0.0)
+        h_ratio = (h_ratio_histo, h_data_raw[1], h_data_raw[2])
 
         ntp[f'{name}_data_raw'] = h_data_raw
         ntp[f'{name}_mc_raw'] = h_mc_raw
         ntp[name] = h_ratio
+        histos[idx] = h_ratio
+        old_key = name
