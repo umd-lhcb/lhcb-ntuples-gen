331d820f08eb423f010123b27bc2cc47c1a84862
Yipeng Sun <syp@umd.edu>
2022-02-25 18:05:37 -0500

    WIP #99: Update batch_skim.sh generation script.

--- ganga/ganga.sample.py
+++ ganga/ganga.sample.py
@@ -36,42 +36,54 @@ SKIM_CONFIG=./postprocess/skims/rdx_mc.yml  # NOTE: Make sure you pick the right
 OUTPUT_DIR={}
 MIN_NTUPLE_SIZE={}  # in KiB
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 '''.format(output_dir, min_ntuple_size)
 
     functions = '''
 function check_job () {
   local error=0
   local job_dir=${INPUT_DIR}/$1
+  local num_of_subjobs=$2
 
-  echo "Verifying output for Job $1..."
+  echo "Verifying output for Job $1, which has $2 subjobs..."
+  # might require GNU find
+  local folders=$(find $job_dir -mindepth 1 -maxdepth 1 -type d -regex ".*/[0-9]*" | wc -l)
+
+  if [[ $folders -ne $num_of_subjobs ]]; then
+      echo -e "${RED}Found $folders subjobs, which =/= $num_of_subjobs. Terminate now.${NC}"
+      exit 1
+  fi
 
   for sj in $(ls $job_dir | grep -E "^[0-9].*$"); do
     local file=$(find $job_dir/$sj/output -name '*.root')
 
     if [[ -z $file ]]; then
       let "error++"
-      echo "subjob $sj: ntuple missing!"
+      echo -e "${RED}subjob $sj: ntuple missing!${NC}"
     else
       local size=$(du -b $file | awk '{print int($1 / 1024)}')  # in KiB
       if [ $size -lt ${MIN_NTUPLE_SIZE} ]; then
         let "error++"
-        echo "subjob $sj: ntuple has a size of $size KiB, which is too small!"
+        echo -e "${RED}subjob $sj: ntuple has a size of $size KiB, which is too small!${NC}"
       fi
     fi
   done
 
   if [ $error -gt 0 ]; then
-    echo "Job $1 output verification failed with $error error(s)."
+    echo -e "${RED}Job $1 output verification failed with $error error(s).${NC}"
+    exit $error  # exit early to make errors easy to see
   fi
 
   return $error
 }
 
 function concat_job () {
-  check_job $1
+  check_job $1 $2
 
   if [ $? -eq 0 ]; then
-    ./ganga/ganga_skim_job_output.py ${OUTPUT_DIR}/$2 ${INPUT_DIR}/$1 ${SKIM_CONFIG}
+    ./ganga/ganga_skim_job_output.py ${OUTPUT_DIR}/$3 ${INPUT_DIR}/$1 ${SKIM_CONFIG}
   fi
 }
 
@@ -81,68 +93,8 @@ function concat_job () {
         f.write(header)
         f.write(functions)
 
-        for idx, orig_filename in instructions:
-            f.write(f'concat_job {idx} {orig_filename}\n')
-
-
-def gen_hadd_script_old(instructions, output_script, input_dir, output_dir='$1',
-                    min_ntuple_size=500):
-    from os.path import expanduser
-
-    header = '''#!/usr/bin/env bash
-INPUT_DIR={}
-OUTPUT_DIR={}
-MIN_NTUPLE_SIZE={}  # in KiB
-
-'''.format(input_dir, output_dir, min_ntuple_size)
-
-    functions = '''
-function check_job () {
-  local error=0
-  local job_dir=${INPUT_DIR}/$1
-
-  echo "Verifying output for Job $1..."
-
-  for sj in $(ls $job_dir | grep -E "^[0-9].*$"); do
-    local file=$(find $job_dir/$sj/output -name '*.root')
-
-    if [[ -z $file ]]; then
-      let "error++"
-      echo "subjob $sj: ntuple missing!"
-    else
-      local size=$(du -b $file | awk '{print int($1 / 1024)}')  # in KiB
-      if [ $size -lt ${MIN_NTUPLE_SIZE} ]; then
-        let "error++"
-        echo "subjob $sj: ntuple has a size of $size KiB, which is too small!"
-      fi
-    fi
-  done
-
-  if [ $error -gt 0 ]; then
-    echo "Job $1 output verification failed with $error error(s)."
-  fi
-
-  return $error
-}
-
-function concat_job () {
-  check_job $1
-
-  if [ $? -eq 0 ]; then
-    hadd -fk ${OUTPUT_DIR}/$3 ${INPUT_DIR}/$1/*/output/$2
-  fi
-}
-
-'''
-
-    with open(expanduser(output_script), 'w') as f:
-        f.write(header)
-        f.write(functions)
-
-        for idx, orig_filename, hadd_filename in instructions:
-            f.write('concat_job {idx} {orig_filename} {hadd_filename}\n'.format(
-                hadd_filename=hadd_filename, idx=idx,
-                orig_filename=orig_filename))
+        for idx, size, name in instructions:
+            f.write(f'concat_job {idx} {size} {name}\n')
 
 
 ##################
@@ -213,6 +165,6 @@ def hadd_completed_job_output(
                     print('Skipping...')
                     continue
 
-            instructions.append((j.id, normalize_hadd_filename(j)))
+            instructions.append((j.id, len(j.subjobs), normalize_hadd_filename(j)))
 
     gen_hadd_script(instructions, output_script)
