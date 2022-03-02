782af76bd2c12861f4426b5b2784b9bfad136bbc
Yipeng Sun <syp@umd.edu>
2022-03-02 11:33:49 -0500

    Plot w/ transparent background.

--- studies/plot-JpsiK_kinematic_reweighting/plot_JpsiK_reweighting.py
+++ studies/plot-JpsiK_kinematic_reweighting/plot_JpsiK_reweighting.py
@@ -18,8 +18,8 @@ from pyTuplingUtils.plot import (
 #################
 
 mcNtps = [
-    '../../ntuples/0.9.6-2016_production/JpsiK-mc-step2/JpsiK--22_02_28--mc--12143001--2016--md/*.root:tree',
-    '../../ntuples/0.9.6-2016_production/JpsiK-mc-step2/JpsiK--22_02_28--mc--12143001--2016--mu/*.root:tree',
+    '../../ntuples/0.9.6-2016_production/JpsiK-mc-step2/JpsiK--22_03_02--mc--12143001--2016--md/*.root:tree',
+    '../../ntuples/0.9.6-2016_production/JpsiK-mc-step2/JpsiK--22_03_02--mc--12143001--2016--mu/*.root:tree',
 ]
 
 dataNtps = '../../run2-JpsiK/fit/fit_results/JpsiK-22_02_26_23_52-std-fit/fit.root:tree'
@@ -42,6 +42,7 @@ dataBrs = uproot.concatenate(dataNtps, varsToComp + sweightBrs, library='np')
 mcBrs = uproot.concatenate(mcNtps, varsToComp + weightBrs, library='np')
 
 global_cut = dataBrs['ntracks'] <= 450
+# global_cut = np.full(dataBrs['ntracks'].size, True, dtype=bool)
 
 
 def plot(output, br, xLabel, dataRange, defaultBinning):
@@ -65,8 +66,7 @@ def plot(output, br, xLabel, dataRange, defaultBinning):
         h_bins, h_mc_before, mc_before_style, show_legend=False,
         figure=fig, axis=ax)
 
-    # w_mc_after = mcBrs['w']
-    w_mc_after = mcBrs['wpid']*mcBrs['wtrk']*mcBrs['wjk_kin']
+    w_mc_after = mcBrs['w']
     h_mc_after, _ = gen_histo(
         mcBrs[br], defaultBinning, data_range=dataRange, weights=w_mc_after,
         density=True)
@@ -78,7 +78,7 @@ def plot(output, br, xLabel, dataRange, defaultBinning):
     ax.legend(numpoints=1, loc='best', frameon=True)
 
     fig.set_tight_layout({'pad': 0.0})
-    fig.savefig(output)
+    fig.savefig(output, transparent=True)
 
 
 if __name__ == '__main__':
