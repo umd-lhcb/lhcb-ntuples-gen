3cee35b087c66ab44564e98ed8055fa6bc18734e
Yipeng Sun <syp@umd.edu>
2022-03-05 02:09:58 -0500

    Check that weight application is also identical.

--- studies/test-JpsiK_weight_apply/test_JpsiK_histo.py
+++ studies/test-JpsiK_weight_apply/test_JpsiK_histo.py
@@ -3,9 +3,8 @@
 import numpy as np
 import uproot
 
-from ROOT import RDataFrame
+from ROOT import RDataFrame, gInterpreter
 from ROOT.RDF import TH2DModel
-
 from tabulate import tabulate
 
 
@@ -16,13 +15,30 @@ from tabulate import tabulate
 mcNtpN = '../../ntuples/0.9.6-2016_production/JpsiK-mc-step2/JpsiK--22_03_04--mc--12143001--2016--md/JpsiK--22_03_04--mc--12143001--2016--md--000.root'
 mcTreeN = 'tree'
 
+histoNtpN = '../../run2-JpsiK/reweight/JpsiK/root-run2-JpsiK_oldcut/run2-JpsiK-2016-md-B-ndof_ntracks__pt_eta.root'
+histoN = 'h_occupancy'
+
 
 #################
 # Histo w/ ROOT #
 #################
 
+gInterpreter.Declare('''
+Int_t getBin(Double_t x, Double_t y, TH2D* histo) {
+  return histo->FindFixBin(x, y);
+}
+
+auto getWeight(Double_t x, Double_t y, TH2D* histo) {
+  auto binIdx = getBin(x, y, histo);
+  return histo->GetBinContent(binIdx);
+}
+''')
+
+gInterpreter.Declare(f'auto histoNtp = new TFile("{histoNtpN}", "read");')
+gInterpreter.Declare(f'auto histo = dynamic_cast<TH2D*>(histoNtp->Get("{histoN}"));')
+
 dfInit = RDataFrame(mcTreeN, mcNtpN)
-df = dfInit.Define('wt', 'wpid*wtrk')
+df = dfInit.Define('wjk_alt', 'getWeight(b_ownpv_ndof, ntracks, histo)').Define('wt', 'wpid*wtrk*wjk_alt')
 
 histoRootMdl = TH2DModel(
     'histoRoot', 'histoRoot',
@@ -38,11 +54,22 @@ histoRoot = df.Histo2D(histoRootMdl, 'b_ownpv_ndof', 'ntracks', 'wt')
 mcNumpyBrsN = ['b_ownpv_ndof', 'ntracks', 'wpid', 'wtrk', 'wjk_occ']
 mcNumpyBrs = uproot.concatenate(f'{mcNtpN}:{mcTreeN}', mcNumpyBrsN, library='np')
 
+
+def getWeights(branches, histoRaw):
+    histo, *binSpecs = histoRaw
+    histoPadded = np.pad(histo, tuple((1, 1) for _ in range(histo.ndim)))
+    binIdx = tuple(np.digitize(br, spec)
+                   for br, spec in zip(branches, binSpecs))
+
+    return histoPadded[binIdx]
+
+histoWtNp = uproot.open(histoNtpN)[histoN].to_numpy()
+wtJkOcc = getWeights(
+    (mcNumpyBrs['b_ownpv_ndof'], mcNumpyBrs['ntracks']), histoWtNp)
 histoNumpy, *_ = np.histogram2d(
     mcNumpyBrs['b_ownpv_ndof'], mcNumpyBrs['ntracks'],
     (20, 20), ((1, 200), (0, 450)),
-    weights=mcNumpyBrs['wpid']*mcNumpyBrs['wtrk']
-    #  weights=mcNumpyBrs['wpid']*mcNumpyBrs['wtrk']*mcNumpyBrs['wjk_occ']
+    weights=mcNumpyBrs['wpid']*mcNumpyBrs['wtrk']*wtJkOcc
 )
 
 tabData = []
