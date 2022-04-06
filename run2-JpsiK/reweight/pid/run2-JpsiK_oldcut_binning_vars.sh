0c819fe8b0c71bb791fffa82e64c3e6f07a277c5
Yipeng Sun <syp@umd.edu>
2022-04-06 20:23:15 +0200

    Fix: cli flag.

--- run2-JpsiK/reweight/pid/run2-JpsiK_oldcut_binning_vars.sh
+++ run2-JpsiK/reweight/pid/run2-JpsiK_oldcut_binning_vars.sh
@@ -2,7 +2,8 @@
 
 lb-conda pidcalib pidcalib2.plot_calib_distributions \
     --sample Turbo16 --magnet down --particle K \
-    --bin-var Brunel_P --binning ./binning.json
+    --bin-var Brunel_P --binning-file ./binning.json
+
 lb-conda pidcalib pidcalib2.plot_calib_distributions \
     --sample Turbo16 --magnet down --particle Mu_nopt \
-    --bin-var Brunel_P --binning ./binning.json
+    --bin-var Brunel_P --binning-file ./binning.json
