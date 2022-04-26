2f3b683bf67ae011075fd8c21b00dcde38320991
Yipeng Sun <syp@umd.edu>
2022-04-26 14:53:00 -0400

    WIP #111: Try out Greg's suggested MC samples.

---
+++ run2-rdx/conds/cond-ghost-2016-md-sim09b.py
@@ -0,0 +1,27 @@
+from Configurables import DaVinci
+
+DaVinci().DataType = '2016'
+DaVinci().Simulation = True
+
+DaVinci().TupleFile = 'ghost.root'
+
+# Additional global flags
+DaVinci().MoniSequence += ['GHOST']
+
+
+from Configurables import LHCbApp
+
+LHCbApp().CondDBtag = "sim-20161124-2-vc-md100"
+LHCbApp().DDDBtag = "dddb-20150724"
+
+
+from GaudiConf import IOHelper
+from glob import glob
+
+# 12875420 (Bu_D0Xmunu,D0=cocktail,LHCbAcceptance)
+#  IOHelper().inputFiles(glob('./data/Bu2D0XMuNu_D0_cocktail-2016-md-py8-sim09b/*.dst'),
+#                        clear=True)
+
+# 11876060 (Bd_D0Xmunu,D0=cocktail,LHCbAcceptance_buggy)
+IOHelper().inputFiles(glob('./data/Bd2D0XMuNu_D0_cocktail-2016-md-py8-sim09b/*.dst'),
+                      clear=True)
