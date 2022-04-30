e3e805b08ea21ca1526c2868449411937397eef9
Yipeng Sun <syp@umd.edu>
2022-04-30 01:24:35 -0400

    Add a sim09k norm. MC sample for debugging.

--- studies/ntuple-RDX_hammer_sample/gen_hammer_sample.sh
+++ studies/ntuple-RDX_hammer_sample/gen_hammer_sample.sh
@@ -9,6 +9,7 @@ declare -A NTPS=(
     ["rdx-run2-Bd2DstMuNu.root"]="../../run2-rdx/samples/Dst_D0--21_07_30--mc--Bd2DstMuNu--2016--md--py8-sim09j-dv45-subset.root"
     ["rdx-run2-Bd2DststTauNu.root"]="../../run2-rdx/samples/Dst_D0--21_07_30--mc--Bd2DststTauNu--2016--md--py8-sim09j-dv45-subset.root"
     ["rdx-run2-Bd2D0DX_MuNu.root"]="../../run2-rdx/samples/Dst_D0--21_09_25--mc--Bd2D0DX_MuNu--2016--md--py8-sim09j-dv45-subset.root"
+    ["rdx-run2-Bd2DstMuNu-sim09k.root"]="../../ntuples/0.9.6-2016_production/Dst_D0-mc-tracker_only/Dst_D0--21_10_16--mc--tracker_only--MC_2016_Beam6500GeV-2016-MagDown-TrackerOnly-Nu1.6-25ns-Pythia8_Sim09k_Reco16_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST/Dst_D0--21_10_16--mc--tracker_only--MC_2016_Beam6500GeV-2016-MagDown-TrackerOnly-Nu1.6-25ns-Pythia8_Sim09k_Reco16_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST--000-dv.root"
 )
 
 for key in "${!NTPS[@]}"; do
