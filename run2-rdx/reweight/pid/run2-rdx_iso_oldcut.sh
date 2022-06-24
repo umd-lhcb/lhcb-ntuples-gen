afaf0c5d7b8f06bb8f4bd691554b78b5fa117105
Yipeng Sun <syp@umd.edu>
2022-06-24 18:44:33 -0400

    WIP #118: Update pidcalib efficiency generation script.

--- run2-rdx/reweight/pid/run2-rdx_iso_oldcut.sh
+++ run2-rdx/reweight/pid/run2-rdx_iso_oldcut.sh
@@ -2,19 +2,37 @@
 #
 # Note: Run this on lxplus!
 
-declare -a K_CUTS=(
-    "MC15TuneV1_ProbNNk > 0.2"
-    "MC15TuneV1_ProbNNk < 0.2"
-)
+SAMPLES=("K" "Pi" "P" "Mu_nopt" "e_B_Jpsi")
 
-for year in 15 16 17 18; do
+declare -A CUTS
+CUTS[nnk_gt]="Brunel_MC15TuneV1_ProbNNk > 0.2"
+
+declare -A POLARITY
+POLARITY[up]="mu"
+POLARITY[down]="md"
+
+for year in 16; do
     for polarity in "up" "down"; do
-        for cut in "${K_CUTS[@]}"; do
-            lb-conda pidcalib pidcalib2.make_eff_hists \
-                --output-dir pidcalib_output \
-                --sample "Turbo${year}" --magnet ${polarity} \
-                --particle K --pid-cut "${cut}" \
-                --bin-var Brunel_P --bin-var Brunel_ETA --bin-var nTracks_Brunel
+        for part in "${SAMPLES[@]}"; do
+            for name in "${!CUTS[@]}"; do
+                folder_name="run2-rdx-20${year}-${POLARITY[${polarity}]}-${part}_${name}-p_eta_ntracks_all_offline"
+                echo "Output folder: ${folder_name}"
+                lb-conda pidcalib pidcalib2.make_eff_hists \
+                    --output-dir ${folder_name} \
+                    --sample "Turbo${year}" --magnet ${polarity} \
+                    --particle ${part} --pid-cut "${CUTS[${name}]}" \
+                    --bin-var Brunel_P --bin-var Brunel_ETA --bin-var nTracks_Brunel \
+                    --max-files 3
+            done
         done
     done
 done
+
+# now rename the pkls
+rm -rf pkl-run2-rdx_iso_oldcut
+mkdir -p pkl-run2-rdx_iso_oldcut
+
+for pkl in ./*/*.pkl; do
+    new_name="$(basename $(dirname $pkl)).pkl"
+    echo "Renaming $pkl to pkl-run2-rdx_iso_oldcut/${new_name}..."
+    cp $pkl pkl-run2-rdx_iso_oldcut/${new_name}
