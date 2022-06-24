5045e1172b516dac086dbb65299d9b080ec95945
Yipeng Sun <syp@umd.edu>
2022-06-25 01:03:26 +0200

    WIP #118: Various small improvements on the PID efficiency script.

--- run2-rdx/reweight/pid/run2-rdx_iso_oldcut.sh
+++ run2-rdx/reweight/pid/run2-rdx_iso_oldcut.sh
@@ -11,18 +11,32 @@ declare -A POLARITY
 POLARITY[up]="mu"
 POLARITY[down]="md"
 
+declare -A PREFIX
+PREFIX[K]="Turbo"
+PREFIX[Pi]="Turbo"
+PREFIX[P]="Turbo"
+PREFIX[Mu_nopt]="Turbo"
+PREFIX[e_B_Jpsi]="Electron"
+
+declare -A NTRACKS_ALIAS
+NTRACKS_ALIAS[K]="nTracks_Brunel"
+NTRACKS_ALIAS[Pi]="nTracks_Brunel"
+NTRACKS_ALIAS[P]="nTracks_Brunel"
+NTRACKS_ALIAS[Mu_nopt]="nTracks_Brunel"
+NTRACKS_ALIAS[e_B_Jpsi]="nTracks"
+
+rm -rf pidcalib_output
 for year in 16; do
     for polarity in "up" "down"; do
         for part in "${SAMPLES[@]}"; do
             for name in "${!CUTS[@]}"; do
-                folder_name="run2-rdx-20${year}-${POLARITY[${polarity}]}-${part}_${name}-p_eta_ntracks_all_offline"
+                folder_name="pidcalib_output/run2-rdx-20${year}-${POLARITY[${polarity}]}-${part}_${name}-p_eta_ntracks"
                 echo "Output folder: ${folder_name}"
                 lb-conda pidcalib pidcalib2.make_eff_hists \
                     --output-dir ${folder_name} \
-                    --sample "Turbo${year}" --magnet ${polarity} \
+                    --sample "${PREFIX[${part}]}${year}" --magnet ${polarity} \
                     --particle ${part} --pid-cut "${CUTS[${name}]}" \
-                    --bin-var Brunel_P --bin-var Brunel_ETA --bin-var nTracks_Brunel \
-                    --max-files 3
+                    --bin-var Brunel_P --bin-var Brunel_ETA --bin-var "${NTRACKS_ALIAS[${part}]}"
             done
         done
     done
@@ -32,7 +46,8 @@ done
 rm -rf pkl-run2-rdx_iso_oldcut
 mkdir -p pkl-run2-rdx_iso_oldcut
 
-for pkl in ./*/*.pkl; do
-    new_name="$(basename $(dirname $pkl)).pkl"
+for pkl in ./pidcalib_output/*/*.pkl; do
+    new_name="$(basename $(dirname ${pkl})).pkl"
     echo "Renaming $pkl to pkl-run2-rdx_iso_oldcut/${new_name}..."
-    cp $pkl pkl-run2-rdx_iso_oldcut/${new_name}
+    cp ${pkl} pkl-run2-rdx_iso_oldcut/${new_name}
+done
