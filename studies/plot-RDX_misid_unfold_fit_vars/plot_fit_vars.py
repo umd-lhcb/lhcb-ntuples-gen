575d98701d61ecd64a133f0b8cb9114495da9f7a
Yipeng Sun <syp@umd.edu>
2022-09-20 12:54:15 -0400

    Simplify the plot routine.

--- studies/plot-RDX_misid_unfold_fit_vars/plot_fit_vars.py
+++ studies/plot-RDX_misid_unfold_fit_vars/plot_fit_vars.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Tue Sep 20, 2022 at 11:25 AM -0400
+# Last Change: Tue Sep 20, 2022 at 12:51 PM -0400
 #
 # Description: plot fit variables w/ w/o decay-in-flight smearing
 
 import mplhep
-import uproot
 import numpy as np
 
 from argparse import ArgumentParser
-from glob import glob
 
 from pyTuplingUtils.utils import gen_histo_stacked_baseline, gen_histo
 from pyTuplingUtils.plot import (
@@ -20,7 +18,7 @@ from pyTuplingUtils.plot import (
     plot_step,
     ax_add_args_step,
 )
-from pyTuplingUtils.io import read_branches_dict
+from pyTuplingUtils.boolean.eval import BooleanEvaluator
 
 
 ################
@@ -31,7 +29,6 @@ DEFAULT_COLORS = ["#00429d", "#5585b7", "#8bd189", "#d15d5f", "#93003a"]
 DEFAULT_OVERALL_COLORS = ["black", "red"]
 LEGEND_LOC = {"q2": "upper left", "mm2": "upper left", "el": "upper right"}
 
-MISID_WTS = ["wmis"]
 MISID_TAGS = {
     "is_misid_pi": r"$\pi$ tag",
     "is_misid_k": r"$K$ tag",
@@ -44,8 +41,23 @@ PLOT_VARS = {
     "q2": r"$q^2$ [GeV$^2$]",
     "el": r"$E_l$ [GeV]",
 }
+PLOT_VARS_SMR_SUF = ['_smr_pi', '_smr_k', '_no_smr']
+
 PLOT_RANGE = {"q2": [-0.4, 12.6], "mm2": [-2.0, 10.9], "el": [0.1, 2.65]}
 SMR_WTS = [["", "_smr_pi", "_smr_k"], ["_no_smr", "_smr_pi", "_smr_k"]]
+
+MISID_WTS = {
+    'q2': 'wmis*skim_global_ok',
+    'q2_smr_pi': 'wmis_smr_pi*skim_global_ok_smr_pi',
+    'q2_smr_k': 'wmis_smr_k*skim_global_ok_smr_k',
+    'q2_no_smr': 'wmis_no_smr*skim_global_ok_no_smr'
+}
+
+for br in ['mm2', 'el']:
+    for s in [''] + PLOT_VARS_SMR_SUF:
+        name = f'{br}{s}' if s != '' else br
+        name_ref = f'q2{s}' if s != '' else 'q2'
+        MISID_WTS[name] = MISID_WTS[name_ref]
 
 
 #######################
@@ -63,14 +75,6 @@ def parse_input():
     parser.add_argument("-t", "--tree", help="specify tree name.", default="tree")
 
     parser.add_argument("--bins", help="specify number of bins.", type=int, default=120)
-
-    parser.add_argument(
-        "-c",
-        "--cuts",
-        nargs="+",
-        default=["b_m_ok", "dx_m_ok", "in_fit_range"],
-        help="specify global cuts. Use a whitespace to separate each cut.",
-    )
 
     parser.add_argument(
         "-p", "--prefix", help="specify plot filename prefix.", default="D0"
@@ -106,15 +110,6 @@ def parse_input():
 ###########
 # Helpers #
 ###########
-
-
-def load_vars(ntps, tree, variables):
-    variables_to_load = [
-        v for v in variables for n in [uproot.open(x) for x in ntps] if v in n[tree]
-    ]
-    for v in variables_to_load:
-        print(f"  Loading {v}")
-    return read_branches_dict(ntps, tree, variables_to_load)
 
 
 def plot_comp(
@@ -209,138 +204,103 @@ def plot_overall(
 if __name__ == "__main__":
     mplhep.style.use("LHCb2")
     args = parse_input()
-    ntp_main = args.input
+    reader = BooleanEvaluator(args.input, args.tree)
 
-    all_vars = (
-        list(PLOT_VARS.keys())
-        + MISID_WTS
-        + list(MISID_TAGS.keys())
-        + [v + s for v in PLOT_VARS for s in SMR_WTS[0]]
-        + [w + s for w in MISID_WTS for s in SMR_WTS[1]]
-        + args.cuts
-    )
 
-    brs = load_vars(ntp_main, args.tree, all_vars)
-    global_cuts = np.multiply.reduce([brs[i] for i in args.cuts]).astype(float)
+    for br_name, show_title, show_legend in zip(
+        PLOT_VARS, args.show_title, args.show_legend
+    ):
+        histos_misid = []
+        histos_smr = []
+        data_range = PLOT_RANGE[br_name]
 
-    for misid_wt in MISID_WTS:
-        wt_tags = []
-        wt_misid = []
-        for tag_cut in MISID_TAGS:
-            wt_tags.append(brs[tag_cut].astype(float))
-            wt_misid.append(brs[misid_wt] * brs[tag_cut])
+        for cut_tag in MISID_TAGS:
+            # unsmeared:
+            expr_wt_unsmr = f'{cut_tag}*{MISID_WTS[br_name]}'
+            br = reader.eval(br_name)
+            wt_unsmr = reader.eval(expr_wt_unsmr)
 
-        wt_smr = []
-        for smr_suf in SMR_WTS[1]:
-            wt_smr.append(brs[misid_wt + smr_suf])
+            histos_misid.append(
+                gen_histo(
+                    br,
+                    args.bins,
+                    data_range=data_range,
+                    weights=wt_unsmr,
+                )
+            )
 
-        for v, show_title, show_legend in zip(
-            PLOT_VARS, args.show_title, args.show_legend
-        ):
-            histos_tags = []
-            histos_misid = []
-            histos_smr = []
-            data_range = PLOT_RANGE[v]
+            # smeared
+            tmp_histos = []
+            for br_name_suf in PLOT_VARS_SMR_SUF:
+                br_name_smr = f'{br_name}{br_name_suf}'
+                if 'no_smr' in br_name_smr:
+                    br_name_smr = br_name
+                expr_wt_smr = f'{cut_tag}*{MISID_WTS[br_name_smr]}'
 
-            for w in wt_tags:
-                histos_tags.append(
+                br_smr = reader.eval(br_name_smr)
+                wt_smr = reader.eval(expr_wt_smr)
+                tmp_histos.append(
                     gen_histo(
-                        brs[v],
+                        br_smr,
                         args.bins,
                         data_range=data_range,
-                        weights=w * global_cuts,
+                        weights=wt_smr,
                     )
                 )
+            merged_tmp_histo = np.add.reduce([h[0] for h in tmp_histos])
+            histos_smr.append((merged_tmp_histo, tmp_histos[0][1]))
 
-            for w in wt_misid:
-                # unsmeared
-                histos_misid.append(
-                    gen_histo(
-                        brs[v],
-                        args.bins,
-                        data_range=data_range,
-                        weights=w * global_cuts,
-                    )
-                )
+        xlabel = PLOT_VARS[br_name]
 
-                # smeared
-                tmp_histos = []
-                for v_suffix, w_smr in zip(SMR_WTS[0], wt_smr):
-                    tmp_histos.append(
-                        gen_histo(
-                            brs[v + v_suffix],
-                            args.bins,
-                            data_range=data_range,
-                            weights=w_smr * w * global_cuts,
-                        )
-                    )
-                merged_tmp_histo = np.add.reduce([h[0] for h in tmp_histos])
-                histos_smr.append((merged_tmp_histo, tmp_histos[0][1]))
+        # unsmeared, w/ misID weights
+        plot_comp(
+            histos_misid,
+            MISID_TAGS.values(),
+            f"{args.title} (misID weighted)",
+            xlabel,
+            args.ylabel,
+            args.output,
+            f"{args.prefix}_{br_name}",
+            legend_loc=LEGEND_LOC[br_name],
+            show_title=show_title,
+            show_legend=show_legend,
+        )
 
-            xlabel = PLOT_VARS[v]
-            # raw data, just taggged
-            plot_comp(
-                histos_tags,
-                MISID_TAGS.values(),
-                f"{args.title} (tags)",
-                xlabel,
-                args.ylabel,
-                args.output,
-                f"{args.prefix}_{misid_wt}_{v}_tags",
-                legend_loc=LEGEND_LOC[v],
-                show_title=show_title,
-                show_legend=show_legend,
-            )
+        # smeared, w/ misID weights
+        plot_comp(
+            histos_smr,
+            MISID_TAGS.values(),
+            f"{args.title} (misID weighted smeared)",
+            xlabel,
+            args.ylabel,
+            args.output,
+            f"{args.prefix}_{br_name}_smr",
+            legend_loc=LEGEND_LOC[br_name],
+            show_title=show_title,
+            show_legend=show_legend,
+        )
 
-            # unsmeared, w/ misID weights
-            plot_comp(
-                histos_misid,
-                MISID_TAGS.values(),
-                f"{args.title} (misID weighted)",
-                xlabel,
-                args.ylabel,
-                args.output,
-                f"{args.prefix}_{misid_wt}_{v}_misid",
-                legend_loc=LEGEND_LOC[v],
-                show_title=show_title,
-                show_legend=show_legend,
-            )
-
-            # smeared, w/ misID weights
-            plot_comp(
-                histos_smr,
-                MISID_TAGS.values(),
-                f"{args.title} (misID weighted smeared)",
-                xlabel,
-                args.ylabel,
-                args.output,
-                f"{args.prefix}_{misid_wt}_{v}_smr",
-                legend_loc=LEGEND_LOC[v],
-                show_title=show_title,
-                show_legend=show_legend,
-            )
-
-            # shape comparison
-            # NOTE: The overall number of event is NOT guaranteed to conserve,
-            #       as after smearing, some of the events might fall out of the
-            #       fit variable acceptance cut
-            merged_histo_misid = (
-                np.add.reduce([h[0] for h in histos_misid]),
-                histos_misid[0][1],
-            )
-            merged_histo_smr = (
-                np.add.reduce([h[0] for h in histos_smr]),
-                histos_smr[0][1],
-            )
-            plot_overall(
-                [merged_histo_misid, merged_histo_smr],
-                ["unsmeared", "smeared"],
-                f"{args.title} (misID unsmeared/smeared comparison)",
-                xlabel,
-                args.ylabel,
-                args.output,
-                f"{args.prefix}_{misid_wt}_{v}_comp",
-                legend_loc=LEGEND_LOC[v],
-                show_title=show_title,
-                show_legend=show_legend,
-            )
+        # shape comparison
+        # NOTE: The overall number of event is NOT guaranteed to conserve,
+        #       as after smearing, some of the events might fall out of the
+        #       fit variable acceptance cut
+        merged_histo_misid = (
+            np.add.reduce([h[0] for h in histos_misid]),
+            histos_misid[0][1],
+        )
+        merged_histo_smr = (
+            np.add.reduce([h[0] for h in histos_smr]),
+            histos_smr[0][1],
+        )
+        plot_overall(
+            [merged_histo_misid, merged_histo_smr],
+            ["unsmeared", "smeared"],
+            f"{args.title} (misID unsmeared/smeared comparison)",
+            xlabel,
+            args.ylabel,
+            args.output,
+            f"{args.prefix}_{br_name}_comp",
+            legend_loc=LEGEND_LOC[br_name],
+            show_title=show_title,
+            show_legend=show_legend,
+        )
