57ada80aab46c6d6a38e9dc7e3aa9f6b48fe04db
Alex Fernez <afernez@umd.edu>
2023-10-03 00:11:17 -0400

    for sig/norm, also calculate FF variation weights with rescaling not removed (w FFs updated)

--- workflows/rdx.py
+++ workflows/rdx.py
@@ -484,6 +484,7 @@ JOBS = {
             '../ntuples/0.9.6-2016_production/Dst_D0-mc-tracker_only/*12773400*.DST', # sig, D*0
         ],
         '../postprocess/rdx-run2/rdx-run2_oldcut.yml',
+        use_hammer_alt=True,
         num_of_workers=20
     ),
     'rdx-ntuple-run2-mc-to-ddx': partial(
