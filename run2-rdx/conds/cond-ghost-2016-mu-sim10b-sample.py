3440bd572d5082ed09d54f3635aad7b9edb0372b
alexfernez <afernez@umd.edu>
2023-12-29 19:03:30 -0500

    updated DaVinci Docker image to have ref to needed db tags for sim10; update corresponding conds, delete unneeded 2017 mc, and prep for missing DDX grid submission

--- run2-rdx/conds/cond-ghost-2016-mu-sim10b-sample.py
+++ run2-rdx/conds/cond-ghost-2016-mu-sim10b-sample.py
@@ -6,13 +6,14 @@ DaVinci().Simulation = True
 DaVinci().TupleFile = 'mc.root'
 # DaVinci().HistogramFile = 'mc-histo.root'
 
+# Additional global flags
+DaVinci().MoniSequence += ['GHOST']
+
+
 from Configurables import LHCbApp
 
 LHCbApp().CondDBtag = "sim-20201113-6-vc-mu100-Sim10"
 LHCbApp().DDDBtag = "dddb-20220927-2016"
-
-# Additional global flags
-DaVinci().MoniSequence += ['GHOST']
 
 from GaudiConf import IOHelper
 
--- run2-rdx/conds/cond-mc-2016-md-sim10b-tracker_only.py
+++ run2-rdx/conds/cond-mc-2016-md-sim10b-tracker_only.py
@@ -9,15 +9,16 @@ DaVinci().TupleFile = 'mc.root'
 # Additional global flags
 DaVinci().MoniSequence += ['TRACKER_ONLY']
 
+
 from Configurables import LHCbApp
 
 LHCbApp().CondDBtag = "sim-20201113-6-vc-md100-Sim10"
 LHCbApp().DDDBtag = "dddb-20220927-2016"
 
 
-from GaudiConf import IOHelper
+# from GaudiConf import IOHelper
 
-# Missing DDX modes
-IOHelper().inputFiles([
-    './data/Bd2D0DX_MuNu-2016-md-py8-sim10b-tracker_only/00202746_00000063_1.d0taunu.safestriptrig.dst',
-], clear=True)
+# # Missing DDX modes
+# IOHelper().inputFiles([
+#     './data/Bd2D0DX_MuNu-2016-md-py8-sim10b-tracker_only/00202746_00000063_1.d0taunu.safestriptrig.dst',
+# ], clear=True)
