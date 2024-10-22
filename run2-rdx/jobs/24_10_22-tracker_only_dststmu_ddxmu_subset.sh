e39778e4f680e8100afac75a8512b5973e5497d3
Alex Fernez <afernez@umd.edu>
2024-10-22 14:29:14 +0200

    add job scripts for lucas + manuel to run 2016 DaVinci jobs

---
+++ run2-rdx/jobs/24_10_22-tracker_only_dststmu_ddxmu_subset.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+export PATH=$DIR/../../ganga:$PATH
+#EXE=ganga_sample_jobs_parser.py
+EXE=ganga_jobs.py
+
+for i in 11874430 12873450 11894600; do
+    for p in md mu; do
+        $EXE \
+            ../reco_Dst_D0.py \
+            ../conds/cond-mc-2016-$p-sim09k-tracker_only.py \
+            -p $p -d $i
+    done
+done
