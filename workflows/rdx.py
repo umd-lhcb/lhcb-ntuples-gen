69e0ce9ff93ccbcc5dc218b2d72307bf2b5b0b2c
Alex Fernez <afernez@umd.edu>
2024-12-27 20:40:07 -0500

    missing DDspi included in main DDX workflow

--- workflows/rdx.py
+++ workflows/rdx.py
@@ -555,13 +555,13 @@ JOBS = {
     #     use_hammer=False,
     #     num_of_workers=20
     # ),
-    'rdx-ntuple-run2-mc-to-missing-ddx-DstDspi': partial(
-        workflow_split,
-        [f'../ntuples/0.9.10-DstDspi/Dst_D0-mc-tracker_only/*{i}*.DST' for i in [11894400, 12895410]],
-        '../postprocess/rdx-run2/rdx-run2_oldcut.yml',
-        use_hammer=False,
-        num_of_workers=20
-    ),
+    # 'rdx-ntuple-run2-mc-to-missing-ddx-DstDspi': partial(
+    #     workflow_split,
+    #     [f'../ntuples/0.9.10-DstDspi/Dst_D0-mc-tracker_only/*{i}*.DST' for i in [11894400, 12895410]],
+    #     '../postprocess/rdx-run2/rdx-run2_oldcut.yml',
+    #     use_hammer=False,
+    #     num_of_workers=20
+    # ),
     'Dst_D0-mc-tracker_only-Dstst': partial(
         workflow_split,
         [
