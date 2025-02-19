fc501cf1616b1026dda7bd4ab448aa98f9e05201
Alex Fernez <afernez@umd.edu>
2025-02-19 18:13:58 +0100

    add the JpsiK 2017/18 cond files too

---
+++ run2-JpsiK/conds/cond-std-2018.py
@@ -0,0 +1,14 @@
+from Configurables import DaVinci
+
+DaVinci().DataType = '2018'
+DaVinci().Simulation = False
+
+DaVinci().TupleFile = 'std.root'
+# DaVinci().HistogramFile = 'std-histo.root'
+
+
+# from GaudiConf import IOHelper
+# from glob import glob
+
+# # Stripping v28r2
+# IOHelper().inputFiles(glob('./data/data-2016-md/*.dst'), clear=True)
