7d8c836bc8fc55ca87e3f5a2d61115a8b9760b74
lmeyerga <lucas.meyer.garcia@cern.ch>
2025-04-30 09:58:16 +0200

    Add tuple for ghost tracks and rename objects consistently

--- run2-rdx/reco_Dst_D0-incl_b.py
+++ run2-rdx/reco_Dst_D0-incl_b.py
@@ -1,7 +1,7 @@
-from Configurables import DecayTreeTuple, CombineParticles, FilterDesktop
+from Configurables import DecayTreeTuple, CombineParticles, FilterDesktop, BackgroundCategory
 from DecayTreeTuple.Configuration import *
 from PhysSelPython.Wrappers import Selection, SelectionSequence
-from StandardParticles import StdAllNoPIDsKaons, StdAllNoPIDsPions
+from StandardParticles import StdAllNoPIDsKaons, StdAllNoPIDsPions, StdAllNoPIDsMuons
 
 
 from Configurables import DaVinci, TrackSmearState
@@ -27,6 +27,10 @@ fromPi     = 'MCSELMATCH( MCINANCESTORS( MCABSID == "pi+" ) )'
 fromK      = 'MCSELMATCH( MCINANCESTORS( MCABSID == "K+" ) )'
 isK        = 'mcMatch("[K+]cc")'
 isPi       = 'mcMatch("[pi+]cc")'
+isP        = 'mcMatch("[p+]cc")'
+isE        = 'mcMatch("[e-]cc")'
+isMu       = 'mcMatch("[mu-]cc")'
+isGhost    = f'(NINTREE( {isK} | {isPi} | {isP} | {isE} | {isMu} ) < 1)'
 isKLoose   = f'({isK} | {fromK})'
 isPiLoose  = f'({isPi} | {fromPi})'
 
@@ -35,90 +39,82 @@ isPiLoose  = f'({isPi} | {fromPi})'
 # D* -> D0(-> K pi) pi ntuples for study K/pi misid corrections #
 #################################################################
 
-filter_k_dst  = FilterDesktop('filter_k_dst',  Code=f'ISLONG & {fromSignal} & {fromDst}', Preambulo=mc_match_preambulo)
-filter_pi_dst = FilterDesktop('filter_pi_dst', Code=f'ISLONG & {fromSignal} & {fromDst}', Preambulo=mc_match_preambulo)
+filterK  = FilterDesktop('FilterK',  Code=f'ISLONG & {fromSignal} & {fromDst}', Preambulo=mc_match_preambulo)
+filterPi = FilterDesktop('FilterPi', Code=f'ISLONG & {fromSignal} & {fromDst}', Preambulo=mc_match_preambulo)
 
-filtered_k_dst = Selection('filtered_k_dst', Algorithm=filter_k_dst, RequiredSelections=[StdAllNoPIDsKaons])
-filtered_pi_dst = Selection('filtered_pi_dst', Algorithm=filter_pi_dst, RequiredSelections=[StdAllNoPIDsPions])
+selectionK  = Selection('SelK',  Algorithm=filterK, RequiredSelections=[StdAllNoPIDsKaons])
+selectionPi = Selection('SelPi', Algorithm=filterPi, RequiredSelections=[StdAllNoPIDsPions])
 
 # define D0 -> K pi decay
-nameD0 = 'D02KPi'
-D02KPiDauCuts = {'K+':  fromD0,
-                 'K-':  fromD0,
-                 'pi+': fromD0,
-                 'pi-': fromD0
-}
-D02KPiComCut = 'AALL'
+D02KPiDauCuts = {'K-':  fromD0, 'pi+': fromD0}
+D02KPiComCuts = 'AALL'
 D02KPiMotCuts = 'ALL'
 D02KPiCombination = CombineParticles(
-    nameD0,
+    'CombD02KPi',
     DecayDescriptor='[D0 -> K- pi+]cc',
     MotherCut=D02KPiMotCuts,
     DaughtersCuts=D02KPiDauCuts,
-    CombinationCut=D02KPiComCut)
+    CombinationCut=D02KPiComCuts)
 D02KPiCombination.Preambulo += mc_match_preambulo
 D02KPi = Selection(
-    'Sel' + nameD0,
+    'SelD02KPi',
     Algorithm=D02KPiCombination,
-    RequiredSelections=[filtered_k_dst, filtered_pi_dst])
+    RequiredSelections=[selectionK, selectionPi])
 
 # define D* -> D0 pi decay
-nameDst = 'Dst2D0Pi'
-Dst2D0PiDauCuts = {'D0':  'ALL',
-                   'D~0': 'ALL',
-                   'pi+': 'ALL',
-                   'pi-': 'ALL'
-                  }
+Dst2D0PiDauCuts = {'D0':  'ALL', 'pi+': 'ALL'}
 Dst2D0PiComCuts = 'AALL'
 Dst2D0PiMotCuts = 'ALL'
 Dst2D0PiCombination = CombineParticles(
-    nameDst,
+    'CombDst2D0Pi',
     DecayDescriptor='[D*(2010)+ -> D0 pi+]cc',
     MotherCut=Dst2D0PiMotCuts,
     DaughtersCuts=Dst2D0PiDauCuts,
     CombinationCut=Dst2D0PiComCuts)
 # Dst2D0PiCombination.Preambulo += mc_match_preambulo
 Dst2D0Pi = Selection(
-    'Sel' + nameDst,
+    'SelDst2D0Pi',
     Algorithm=Dst2D0PiCombination,
-    RequiredSelections=[D02KPi, filtered_pi_dst])
+    RequiredSelections=[D02KPi, selectionPi])
 
 # Define decay tree tuple with uBDT input for K candidate
-dtt_k = DecayTreeTuple('Dst_D0Pi_ANNK')
-dtt_k.setDescriptorTemplate('${dst}[(D*(2010)+ -> ${d0}(D0 -> ${k}K- ${pi}pi+) ${spi}pi+)]CC')
-dtt_k.Inputs = [Dst2D0Pi.outputLocation()]
-dtt_k.addTupleTool('TupleToolTISTOS')
-dtt_k.addTupleTool('TupleToolRecoStats')
-dtt_k.addTupleTool('TupleToolMCBackgroundInfo')
-dtt_k.addTupleTool('TupleToolMCTruth')
-dtt_k.TupleToolMCTruth.ToolList = [
+dttDstK = DecayTreeTuple('TupleDstANNK')
+dttDstK.setDescriptorTemplate('${dst}[(D*(2010)+ -> ${d0}(D0 -> ${k}K- ${pi}pi+) ${spi}pi+)]CC')
+dttDstK.Inputs = [Dst2D0Pi.outputLocation()]
+dttDstK.addTool(BackgroundCategory)
+dttDstK.addTupleTool('TupleToolTISTOS')
+dttDstK.addTupleTool('TupleToolRecoStats')
+dttDstK.addTupleTool('TupleToolMCBackgroundInfo')
+dttDstK.addTupleTool('TupleToolMCTruth')
+dttDstK.TupleToolMCTruth.ToolList = [
     'MCTupleToolKinematic',
     'MCTupleToolPID',
     'MCTupleToolHierarchy'
 ]
-dtt_k.k.addTupleTool('TupleToolANNPIDTraining')
+dttDstK.k.addTupleTool('TupleToolANNPIDTraining')
 
 # Now define decay tree tuple with uBDT input for pi candidate
-dtt_pi = DecayTreeTuple('Dst_D0Pi_ANNPi')
-dtt_pi.setDescriptorTemplate('${dst}[(D*(2010)+ -> ${d0}(D0 -> ${k}K- ${pi}pi+) ${spi}pi+)]CC')
-dtt_pi.Inputs = [Dst2D0Pi.outputLocation()]
-dtt_pi.addTupleTool('TupleToolTISTOS')
-dtt_pi.addTupleTool('TupleToolRecoStats')
-dtt_pi.addTupleTool('TupleToolMCBackgroundInfo')
-dtt_pi.addTupleTool('TupleToolMCTruth')
-dtt_pi.TupleToolMCTruth.ToolList = [
+dttDstPi = DecayTreeTuple('TupleDstANNPi')
+dttDstPi.setDescriptorTemplate('${dst}[(D*(2010)+ -> ${d0}(D0 -> ${k}K- ${pi}pi+) ${spi}pi+)]CC')
+dttDstPi.Inputs = [Dst2D0Pi.outputLocation()]
+dttDstPi.addTool(BackgroundCategory)
+dttDstPi.addTupleTool('TupleToolTISTOS')
+dttDstPi.addTupleTool('TupleToolRecoStats')
+dttDstPi.addTupleTool('TupleToolMCBackgroundInfo')
+dttDstPi.addTupleTool('TupleToolMCTruth')
+dttDstPi.TupleToolMCTruth.ToolList = [
     'MCTupleToolKinematic',
     'MCTupleToolPID',
     'MCTupleToolHierarchy'
 ]
-dtt_pi.pi.addTupleTool('TupleToolANNPIDTraining') # This is the only difference between the two tuples
+dttDstPi.pi.addTupleTool('TupleToolANNPIDTraining') # This is the only difference between the two tuples
 
-sequenceDstD0Pi_k = SelectionSequence(
-    'Seq' + nameDst + 'K', TopSelection=Dst2D0Pi, PostSelectionAlgs=[dtt_k])
-sequenceDstD0Pi_pi = SelectionSequence(
-    'Seq' + nameDst + 'Pi', TopSelection=Dst2D0Pi, PostSelectionAlgs=[dtt_pi])
+sequenceDstK = SelectionSequence(
+    'SeqDstK', TopSelection=Dst2D0Pi, PostSelectionAlgs=[dttDstK])
+sequenceDstPi = SelectionSequence(
+    'SeqDstPi', TopSelection=Dst2D0Pi, PostSelectionAlgs=[dttDstPi])
 
-DaVinci().UserAlgorithms += [sequenceDstD0Pi_k.sequence(), sequenceDstD0Pi_pi.sequence()]
+DaVinci().UserAlgorithms += [sequenceDstK.sequence(), sequenceDstPi.sequence()]
 
 
 #########################################
@@ -126,41 +122,67 @@ DaVinci().UserAlgorithms += [sequenceDstD0Pi_k.sequence(), sequenceDstD0Pi_pi.se
 #########################################
 
 # Loosely select true Kaons satisfying ISMUON
-filter_k_dif  = FilterDesktop('filter_k_dif',  Code=f'ISLONG & ISMUON & {isKLoose}', Preambulo=mc_match_preambulo)
+filterKDiF  = FilterDesktop('FilterKDiF',  Code=f'ISLONG & ISMUON & {isKLoose}', Preambulo=mc_match_preambulo)
 
-filtered_k_dif = Selection('filtered_k_dif', Algorithm=filter_k_dif, RequiredSelections=[StdAllNoPIDsKaons])
+selectionKDiF = Selection('SelKDiF', Algorithm=filterKDiF, RequiredSelections=[StdAllNoPIDsKaons])
 
-dtt_k_dif = DecayTreeTuple('K_DiF')
-dtt_k_dif.setDescriptorTemplate('${k}[K+]CC')
-dtt_k_dif.Inputs = [filtered_k_dif.outputLocation()]
-dtt_k_dif.addTupleTool('TupleToolMCTruth')
-dtt_k_dif.TupleToolMCTruth.ToolList = [
+dttKDiF = DecayTreeTuple('TupleKDiF')
+dttKDiF.setDescriptorTemplate('${k}[K+]CC')
+dttKDiF.Inputs = [selectionKDiF.outputLocation()]
+dttKDiF.addTupleTool('TupleToolMCTruth')
+dttKDiF.TupleToolMCTruth.ToolList = [
     'MCTupleToolKinematic',
     'MCTupleToolPID',
     'MCTupleToolHierarchy'
 ]
-dtt_k_dif.k.addTupleTool('TupleToolANNPIDTraining')
+dttKDiF.k.addTupleTool('TupleToolANNPIDTraining')
 
 sequenceK_dif = SelectionSequence(
-    'SeqKDiF', TopSelection=filtered_k_dif, PostSelectionAlgs=[dtt_k_dif])
+    'SeqKDiF', TopSelection=selectionKDiF, PostSelectionAlgs=[dttKDiF])
 
 # Loosely select true Pions satisfying ISMUON
-filter_pi_dif = FilterDesktop('filter_pi_dif', Code=f'ISLONG & ISMUON & {isPiLoose}', Preambulo=mc_match_preambulo)
+filterPiDiF = FilterDesktop('FilterPiDiF', Code=f'ISLONG & ISMUON & {isPiLoose}', Preambulo=mc_match_preambulo)
 
-filtered_pi_dif = Selection('filtered_pi_dif', Algorithm=filter_pi_dif, RequiredSelections=[StdAllNoPIDsPions])
+selectionPiDiF = Selection('SelPiDiF', Algorithm=filterPiDiF, RequiredSelections=[StdAllNoPIDsPions])
 
-dtt_pi_dif = DecayTreeTuple('Pi_DiF')
-dtt_pi_dif.setDescriptorTemplate('${pi}[pi+]CC')
-dtt_pi_dif.Inputs = [filtered_pi_dif.outputLocation()]
-dtt_pi_dif.addTupleTool('TupleToolMCTruth')
-dtt_pi_dif.TupleToolMCTruth.ToolList = [
+dttPiDiF = DecayTreeTuple('TuplePiDiF')
+dttPiDiF.setDescriptorTemplate('${pi}[pi+]CC')
+dttPiDiF.Inputs = [selectionPiDiF.outputLocation()]
+dttPiDiF.addTupleTool('TupleToolMCTruth')
+dttPiDiF.TupleToolMCTruth.ToolList = [
     'MCTupleToolKinematic',
     'MCTupleToolPID',
     'MCTupleToolHierarchy'
 ]
-dtt_pi_dif.pi.addTupleTool('TupleToolANNPIDTraining')
+dttPiDiF.pi.addTupleTool('TupleToolANNPIDTraining')
 
 sequencePi_dif = SelectionSequence(
-    'SeqPiDiF', TopSelection=filtered_pi_dif, PostSelectionAlgs=[dtt_pi_dif])
+    'SeqPiDiF', TopSelection=selectionPiDiF, PostSelectionAlgs=[dttPiDiF])
 
 DaVinci().UserAlgorithms += [sequenceK_dif.sequence(), sequencePi_dif.sequence()]
+
+
+################
+# ghost tracks #
+################
+
+# Select true ghost tracks
+filterGhost = FilterDesktop('FilterGhost',  Code=f'ISLONG & {isGhost}', Preambulo=mc_match_preambulo)
+
+selectionGhost = Selection('SelGhost', Algorithm=filterGhost, RequiredSelections=[StdAllNoPIDsMuons])
+
+dttGhost = DecayTreeTuple('Ghost')
+dttGhost.setDescriptorTemplate('${mu}[mu+]CC')
+dttGhost.Inputs = [selectionGhost.outputLocation()]
+dttGhost.addTupleTool('TupleToolMCTruth')
+dttGhost.TupleToolMCTruth.ToolList = [
+    'MCTupleToolKinematic',
+    'MCTupleToolPID',
+    'MCTupleToolHierarchy'
+]
+dttGhost.mu.addTupleTool('TupleToolANNPIDTraining')
+
+sequence_g = SelectionSequence(
+    'SeqGhost', TopSelection=selectionGhost, PostSelectionAlgs=[dttGhost])
+
+DaVinci().UserAlgorithms += [sequence_g.sequence()]
